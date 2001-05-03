ffa4c6ff809f84d2877c437206d2c9b242a33f73
Paul Julius <pauljulius@users.sourceforge.net>
2001-05-03 18:50:01 +0000

    Added license & copyright snippet.

--- main/setclasspath.bat
+++ main/setclasspath.bat
@@ -1,3 +1,24 @@
+REM /********************************************************************************
+REM  * CruiseControl, a Continuous Integration Toolkit                              *
+REM  * Copyright (C) 2001  ThoughtWorks, Inc.                                       *
+REM  * 651 W Washington Ave. Suite 500                                              *
+REM  * Chicago, IL 60661 USA                                                        *
+REM  *                                                                              *
+REM  * This program is free software; you can redistribute it and/or                *
+REM  * modify it under the terms of the GNU General Public License                  *
+REM  * as published by the Free Software Foundation; either version 2               *
+REM  * of the License, or (at your option) any later version.                       *
+REM  *                                                                              *
+REM  * This program is distributed in the hope that it will be useful,              *
+REM  * but WITHOUT ANY WARRANTY; without even the implied warranty of               *
+REM  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                *
+REM  * GNU General Public License for more details.                                 *
+REM  *                                                                              *
+REM  * You should have received a copy of the GNU General Public License            *
+REM  * along with this program; if not, write to the Free Software                  *
+REM  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.  *
+REM  ********************************************************************************/
+ 
 set CCDIR=d:\projects\cruisecontrol\cruisecontrol
 set LIBDIR=%CCDIR%\lib
 
--- main/src/CruiseControl.java
+++ main/src/CruiseControl.java
@@ -1,3 +1,24 @@
+/********************************************************************************
+ * CruiseControl, a Continuous Integration Toolkit                              *
+ * Copyright (C) 2001  ThoughtWorks, Inc.                                       *
+ * 651 W Washington Ave. Suite 500                                              *
+ * Chicago, IL 60661 USA                                                        *
+ *                                                                              *
+ * This program is free software; you can redistribute it and/or                *
+ * modify it under the terms of the GNU General Public License                  *
+ * as published by the Free Software Foundation; either version 2               *
+ * of the License, or (at your option) any later version.                       *
+ *                                                                              *
+ * This program is distributed in the hope that it will be useful,              *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                *
+ * GNU General Public License for more details.                                 *
+ *                                                                              *
+ * You should have received a copy of the GNU General Public License            *
+ * along with this program; if not, write to the Free Software                  *
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.  *
+ ********************************************************************************/
+ 
 import net.sourceforge.cruisecontrol.*;
 
 /**
