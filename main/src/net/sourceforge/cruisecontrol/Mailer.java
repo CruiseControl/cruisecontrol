c2820a4c67145e6eeeb1833563740fc1349ae796
Jason Yip <jchyip@users.sourceforge.net>
2001-05-22 14:55:31 +0000

    Use Ant mailer vs javax.mail to remove activation jar dependency
    git-svn-id: https://cruisecontrol.svn.sourceforge.net/svnroot/cruisecontrol/trunk/cruisecontrol@76 878de816-9c12-0410-b5fe-b34e1928e20d

--- main/src/net/sourceforge/cruisecontrol/Mailer.java
+++ main/src/net/sourceforge/cruisecontrol/Mailer.java
@@ -18,19 +18,16 @@
  * along with this program; if not, write to the Free Software                  *
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.  *
  ********************************************************************************/
- 
 package net.sourceforge.cruisecontrol;
 
 import java.io.*;
-import java.net.InetAddress;
 import java.util.*;
-import javax.mail.*;
-import javax.mail.internet.*;
+import org.apache.tools.mail.MailMessage;
 
 /**
  * The Mailer can handle sending simple character based mail messages.
- * 
- * @author Alden Almagro, Jack Bolles, Paul Julius, ThoughtWorks, Inc.
+ *
+ * @author Alden Almagro, Jack Bolles, Paul Julius, ThoughtWorks, Inc., neillr
  *
  * @since November 28, 2000
  */
@@ -47,11 +44,11 @@ public class Mailer {
      * Stores the address of the mail(SMTP) server.
      */
     private String mailhost;
-
+    
     /**
      * Construct a mailer for the given recipient(s) and
      * sender.
-     * 
+     *
      * @param to     Either a single address, or a comma delimited list
      *               of addresses, to which mail should be sent.
      * @param from   The sender's address.
@@ -61,10 +58,10 @@ public class Mailer {
         this.to = to;
         this.from = from;
     }
-
+    
     /**
      * Creates a Mailer for a Collection of "to" addresses.
-     * 
+     *
      * @param to     Collection of email address to which mail should be sent.
      * @param from   Address from which the mail should be sent.
      */
@@ -82,16 +79,16 @@ public class Mailer {
         this.to = buf.toString();
         this.from = from;
     }
-
+    
     /**
      * Creates a Mailer for an array of "to" addresses.
-     * 
+     *
      * @param to     Array of email addresses to which mail should be sent.
      * @param from   Address from which the mail is sent.
      */
     public Mailer(String mailhost, String[] to, String from) {
         this.mailhost = mailhost;
-
+        
         StringBuffer buf = new StringBuffer();
         for (int i=0; i<to.length; i++ ) {
             String nextName = to[i];
@@ -103,46 +100,28 @@ public class Mailer {
         this.to = buf.toString();
         this.from = from;
     }
-
+    
     /**
      * Sends a message to the recipient(s) managed by this
      * Mailer instance.
-     * 
+     *
      * @param subject Subject of the mail message.
      * @param message Message body.
-     * @exception MessagingException
+     * @throws IOException
      */
-    public void sendMessage(String subject, String message) throws MessagingException {
-        sendMessage(subject,message,false);
-    }
-
-    /**
-     * Sends a message to the recipient(s) managed by this
-     * Mailer instance.
-     * 
-     * @param subject Subject of the mail message.
-     * @param message Message body.
-     * @param debug   true to output standard debug information from the
-     *                JavaMail Transport Provider.
-     * @exception MessagingException
-     */
-    public void sendMessage(String subject, String message, boolean debug) throws MessagingException  {
-        Properties props = System.getProperties();
-        props.put("mail.smtp.host", mailhost);
-        Session session = Session.getDefaultInstance(props, null);
-        session.setDebug(debug);
-
-        Message msg = new MimeMessage(session);
-        msg.setFrom(new InternetAddress(from));
-        msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to, false));
+    public void sendMessage(String subject, String message) 
+      throws IOException {
+        MailMessage msg = new MailMessage(mailhost);
+        msg.from(from);
+        msg.to(to);
         msg.setSubject(subject);
-        msg.setText(message);
+        
+        PrintStream msg_out = msg.getPrintStream();
+        msg_out.println(message);
         msg.setHeader("X-Mailer", "MasterBuild.Mailer");
-        msg.setSentDate(new Date());
-
-        // send the message off
-        Transport.send(msg);
-
+        msg.sendAndClose();
+        
         System.out.println("\nMail was sent successfully.");
     }
+    
 }
