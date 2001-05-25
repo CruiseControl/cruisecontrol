b9600842614bb7e10575db5ea19479745dad2067
Jason Yip <jchyip@users.sourceforge.net>
2001-05-25 15:24:24 +0000

    Include JUnit task
    git-svn-id: https://cruisecontrol.svn.sourceforge.net/svnroot/cruisecontrol/trunk/cruisecontrol@103 878de816-9c12-0410-b5fe-b34e1928e20d

--- main/docs/helloWorld/src/hello/HelloWorld.java
+++ main/docs/helloWorld/src/hello/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
     public String sayHello() {
         return "Hello world";
     }
-    
+
     public static void main(String[] args) {
         System.out.println(new HelloWorld().sayHello());
     }
