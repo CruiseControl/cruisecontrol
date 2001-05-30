09bfbde23bc84a8de17ddda3ef15eb313d3c4ceb
Jason Yip <jchyip@users.sourceforge.net>
2001-05-30 20:00:11 +0000

    Duplicates
    git-svn-id: https://cruisecontrol.svn.sourceforge.net/svnroot/cruisecontrol/trunk/cruisecontrol@114 878de816-9c12-0410-b5fe-b34e1928e20d

--- main/cruiseControl.sh
+++ main/cruiseControl.sh
@@ -25,7 +25,7 @@ CCDIR=`dirname $0`
 LIBDIR=$CCDIR/lib
 CRUISE_PATH=$JAVA_HOME/lib/tools.jar:$CCDIR/dist/cruisecontrol.jar:$LIBDIR/ant.jar:$LIBDIR/xerces.jar:$LIBDIR/mail.jar:$LIBDIR/optional.jar:$LIBDIR/junit.jar:$LIBDIR/activation.jar:.
 
-EXEC=java -cp %CRUISE_PATH% net.sourceforge.cruisecontrol.MasterBuild $@
+EXEC=java -cp $CRUISE_PATH net.sourceforge.cruisecontrol.MasterBuild $@
 echo $EXEC
 $EXEC
 
