c8ee41fc83d06367d8850cf16a85be600c7dd3b6
Jason Yip <jchyip@users.sourceforge.net>
2001-06-11 15:50:55 +0000

    Repackage source control elements to net.sourceforge.cruisecontrol.element
    git-svn-id: https://cruisecontrol.svn.sourceforge.net/svnroot/cruisecontrol/trunk/cruisecontrol@153 878de816-9c12-0410-b5fe-b34e1928e20d

---
+++ main/src/net/sourceforge/cruisecontrol/element/ClearCaseElement.java
@@ -0,0 +1,277 @@
+package net.sourceforge.cruisecontrol.element;
+import java.io.*;
+
+import java.text.*;
+import java.util.*;
+import net.sourceforge.cruisecontrol.Modification;
+
+import org.apache.tools.ant.Task;
+
+/**
+ *  This class implements the SourceControlElement methods for a Clear Case
+ *  repository.
+ *
+ *@author  Thomas Leseney, jchyip
+ *@created  June 11, 2001
+ *@version  May 25, 2001
+ */
+
+public class ClearCaseElement extends SourceControlElement {
+
+	/**
+	 *  Set of the authors that modified files. With Clear Case, it correspond the
+	 *  user names.
+	 */
+	private Set _emailNames = new HashSet();
+
+	/**
+	 *  This date indicates the most recent modification time.
+	 */
+	private Date _lastModified;
+
+	/**
+	 *  The path of the clear case view
+	 */
+	private String _viewPath;
+
+	/**
+	 *  The branch to check for modifications
+	 */
+	private String _branch = null;
+	private boolean _recursive = true;
+
+	/**
+	 *  Date format required by commands passed to Clear Case
+	 */
+	final static SimpleDateFormat IN_DATE_FORMAT = new SimpleDateFormat("dd-MMMM-yyyy.HH:mm:ss");
+
+	/**
+	 *  Date format returned in the output of Clear Case commands.
+	 */
+	final static SimpleDateFormat OUT_DATE_FORMAT = new SimpleDateFormat("yyyyMMdd.HHmmss");
+
+	/**
+	 *  Sets the local working copy to use when making queries.
+	 *
+	 *@param  path
+	 */
+	public void setViewpath(String path) {
+		_viewPath = getAntTask().getProject().resolveFile(path).getAbsolutePath();
+	}
+
+	/**
+	 *  Sets the branch that we're concerned about checking files into.
+	 *
+	 *@param  branch
+	 */
+	public void setBranch(String branch) {
+		_branch = branch;
+	}
+
+	public void setRecursive(boolean b) {
+		_recursive = b;
+	}
+
+	/**
+	 *  Returns a Set of email addresses. since Clear Case doesn't track actual
+	 *  email addresse, we just return the usernames, which may correspond to
+	 *  emails ids.
+	 *
+	 *@return
+	 */
+	public Set getEmails() {
+		return _emailNames;
+	}
+
+	/**
+	 *  Gets the last modified time for the set of files queried in the {@link
+	 *  #getHistory} method.
+	 *
+	 *@return  the lastest revision time.
+	 */
+	public long getLastModified() {
+		if (_lastModified == null) {
+			return 0;
+		}
+		return _lastModified.getTime();
+	}
+
+	/**
+	 *  Returns an {@link java.util.ArrayList ArrayList} of {@link Modification}
+	 *  detailing all the changes between now and the last build.
+	 *
+	 *@param  lastBuild the last build time
+	 *@param  now time now, or time to check, NOT USED
+	 *@param  quietPeriod NOT USED
+	 *@return  the list of modifications, an empty (not null) list if no
+	 *      modifications.
+	 */
+	public ArrayList getHistory(Date lastBuild, Date now, long quietPeriod) {
+		ArrayList modifications = null;
+		String lastBuildDate = IN_DATE_FORMAT.format(lastBuild);
+		/*
+		 *  let's try a different clearcase command--this one just takes waaaaaaaay too long.
+		 *  String command = "cleartool find " + _viewPath +
+		 *  " -type f -exec \"cleartool lshistory" +
+		 *  " -since " + lastBuildDate;
+		 *  if(_branch != null)
+		 *  command += " -branch " + _branch;
+		 *  command += " -nco" + // exclude check out events
+		 *  " -fmt \\\" %u;%Nd;%n;%o \\n \\\" \\\"%CLEARCASE_XPN%\\\" \"";
+		 */
+		String command = "cleartool lshistory";
+		if (_branch != null) {
+			command += " -branch " + _branch;
+		}
+		if (_recursive == true) {
+			command += " -r ";
+		}
+		command += " -nco -since " + lastBuildDate;
+		command += " -fmt \"%u;%Nd;%n;%o\\n\" " + _viewPath;
+
+		log("Command to execute : " + command);
+		try {
+			Process p = Runtime.getRuntime().exec(command);
+
+			StreamPumper errorPumper = new StreamPumper(p.getErrorStream());
+			new Thread(errorPumper).start();
+
+			InputStream input = p.getInputStream();
+			modifications = parseStream(input);
+
+			p.waitFor();
+		}
+		catch (Exception e) {
+			log("Error in executing the Clear Case command : " + e);
+			e.printStackTrace();
+		}
+		if (modifications == null) {
+			modifications = new ArrayList();
+		}
+		return modifications;
+	}
+
+	/**
+	 *  Parses the input stream to construct the modifications list.
+	 *
+	 *@param  input the stream to parse
+	 *@return  a list of modification elements
+	 *@exception  IOException
+	 */
+	private ArrayList parseStream(InputStream input)
+			 throws IOException {
+		ArrayList modifications = new ArrayList();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+		String line;
+
+		while ((line = reader.readLine()) != null) {
+			Modification mod = parseEntry(line);
+			if (mod != null) {
+				modifications.add(mod);
+			}
+		}
+		return modifications;
+	}
+
+	/**
+	 *  Parses a single line from the reader. Each line contains a signe revision
+	 *  with the format : <br>
+	 *  username;date_of_revision;element_name;operation_type <br>
+	 *
+	 *
+	 *@param  line the line to parse
+	 *@return  a modification element corresponding to the given line
+	 */
+	private Modification parseEntry(String line) {
+		System.out.println("parsing entry: " + line);
+		StringTokenizer st = new StringTokenizer(line, ";");
+		if (st.countTokens() != 4) {
+			return null;
+		}
+		String username = st.nextToken().trim();
+		String timeStamp = st.nextToken().trim();
+		String elementName = st.nextToken().trim();
+		String operationType = st.nextToken().trim();
+
+		/*
+		 *  a branch event shouldn't trigger a build
+		 */
+		if (operationType.equals("mkbranch")) {
+			return null;
+		}
+
+		Modification mod = new Modification();
+
+		mod.userName = username;
+		_emailNames.add(mod.userName);
+
+		elementName = elementName.substring(elementName.indexOf(":\\") + 1);
+		String fileName = elementName.substring(0, elementName.indexOf("@@"));
+
+		mod.fileName = fileName.substring(fileName.lastIndexOf("\\"));
+		mod.folderName = fileName.substring(0, fileName.lastIndexOf("\\"));
+
+		try {
+			mod.modifiedTime = OUT_DATE_FORMAT.parse(timeStamp);
+			updateLastModified(mod.modifiedTime);
+		}
+		catch (ParseException e) {
+			mod.modifiedTime = null;
+		}
+
+		mod.type = operationType;
+
+		mod.comment = "";
+
+		return mod;
+	}
+
+	/**
+	 *  Updates the lastModified date if necessary (new date is after the current
+	 *  lastModified date).
+	 *
+	 *@param  newPossible the new possible lastModified date
+	 */
+	private void updateLastModified(Date newPossible) {
+		if (_lastModified == null || _lastModified.before(newPossible)) {
+			_lastModified = newPossible;
+		}
+	}
+
+	/**
+	 *  Inner class to pump the error stream during Process's runtime. Copied from
+	 *  the Ant built-in task.
+	 *
+	 *@author  jcyip
+	 *@created  June 11, 2001
+	 */
+	class StreamPumper implements Runnable {
+
+		private InputStream _in;
+		private final static int SIZE = 128;
+		private final static int SLEEP = 5;
+
+		public StreamPumper(InputStream in) {
+			_in = in;
+		}
+
+		public void run() {
+			final byte[] buf = new byte[SIZE];
+			int length;
+
+			try {
+				while ((length = _in.read(buf)) > 0) {
+					System.err.write(buf, 0, length);
+					try {
+						Thread.sleep(SLEEP);
+					}
+					catch (InterruptedException e) {
+					}
+				}
+			}
+			catch (IOException e) {
+			}
+		}
+	}
+}
---
+++ main/src/net/sourceforge/cruisecontrol/element/P4Element.java
@@ -0,0 +1,282 @@
+package net.sourceforge.cruisecontrol.element;
+import java.io.*;
+
+import java.util.*;
+import net.sourceforge.cruisecontrol.Modification;
+
+import org.apache.oro.text.perl.*;
+
+import org.apache.tools.ant.*;
+import org.apache.tools.ant.taskdefs.*;
+
+import org.apache.tools.ant.taskdefs.optional.perforce.*;
+import org.apache.tools.ant.types.*;
+
+/**
+ *  This class implements the SourceControlElement methods for a P4 depot. The
+ *  call to CVS is assumed to work without any setup. This implies that if the
+ *  authentication type is pserver the call to cvs login should be done prior to
+ *  calling this class. <p>
+ *
+ *  P4Element depends on the optional P4 package delivered with Ant v1.3. But
+ *  since it probably doesn't make much sense using the P4Element without other
+ *  P4 support it shouldn't be a problem. <p>
+ *
+ *  P4Element sets the property ${p4element.change} with the current changelist
+ *  number. This should then be passed into p4sync or other p4 commands.
+ *
+ *@author  niclas.olofsson@ismobile.com, jchyip
+ *@created  den 23 april 2001
+ *@version  0.1
+ */
+public class P4Element extends SourceControlElement {
+
+	private Set _emailNames = new HashSet();
+	private Date _lastModified;
+
+	//P4 runtime directives
+
+	private String _P4Port;
+	private String _P4Client;
+	private String _P4User;
+	private String _P4View;
+	private int _P4lastChange;
+	private final static java.text.SimpleDateFormat p4Date = new java.text.SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
+	/**
+	 *  Constructor for P4Element. Doesn't do much.
+	 */
+	public P4Element() {
+	}
+
+	//Setters called by Ant
+	public void setPort(String P4Port) {
+		this._P4Port = P4Port;
+	}
+
+	public void setClient(String P4Client) {
+		this._P4Client = P4Client;
+	}
+
+	public void setUser(String P4User) {
+		this._P4User = P4User;
+	}
+
+	public void setView(String P4View) {
+		this._P4View = P4View;
+	}
+
+	/**
+	 *  Returns a Set of email addresses. P4 doesn't track actual email addresses,
+	 *  so we'll just return the usernames here, which may correspond to email ids.
+	 *  We'll tack on the suffix, i.e.
+	 *
+	 *@return  Set of author names; maybe empty, never null.
+	 *@apache.org,  in MasterBuild.java before mailing results of the build.
+	 */
+	public Set getEmails() {
+		if (_emailNames == null) {
+			_emailNames = new HashSet();
+		}
+		return _emailNames;
+	}
+
+	/**
+	 *  Gets the last modified time for this set of files queried in the
+	 *  getHistory() method.
+	 *
+	 *@return  Latest revision time.
+	 */
+	public long getLastModified() {
+		if (_lastModified == null) {
+			return 0;
+		}
+		return _lastModified.getTime();
+	}
+
+	public ArrayList getHistory(Date lastBuild, Date now, long quietPeriod) {
+
+		ArrayList mods = null;
+		final Perl5Util util = new Perl5Util();
+
+		//Init last modified to last build date.
+		_lastModified = lastBuild;
+
+		// next line is a trick to get the variable usable within the adhoc handler.
+		final StringBuffer sbChangenumber = new StringBuffer();
+		final StringBuffer sbModifiedTime = new StringBuffer();
+		//if this._P4lastChange != p4 changes -m 1 -s submitted <depotpath>
+		execP4Command("changes -m 1 -s submitted " + _P4View,
+			new P4HandlerAdapter() {
+				public void process(String line) {
+					if (util.match("/Change/", line)) {
+						//Parse out the change number
+						sbChangenumber.append(util.substitute("s/Change\\s([0-9]*?)\\son\\s.*/$1/gx", line));
+						log("Latest change is " + sbChangenumber, Project.MSG_INFO);
+					}
+					else if (util.match("/error/", line)) {
+						throw new BuildException("Perforce Error, check client settings and/or server");
+					}
+				}
+			});
+
+		// and collect info for this change
+
+		final StringBuffer sbDescription = new StringBuffer();
+		execP4Command("describe -s " + sbChangenumber.toString(),
+			new P4HandlerAdapter() {
+				public void process(String line) {
+					if (util.match("/error/", line)) {
+						throw new BuildException("Perforce Error, check client settings and/or server");
+					}
+					sbDescription.append(line);
+					sbDescription.append("\n");
+				}
+			});
+		// now, lets parse the data out
+		String userName = util.substitute("s/Change\\s([0-9]*?)\\sby\\s(.*?)\\@.*?\\son\\s(.*?\\s.*?)\\n\\n(.*)\\n\\nAffected\\sfiles.*/$2/s", sbDescription.toString());
+		String sModifiedTime = util.substitute("s/Change\\s([0-9]*?)\\sby\\s(.*?)\\@.*?\\son\\s(.*?\\s.*?)\\n\\n(.*)\\n\\nAffected\\sfiles.*/$3/s", sbDescription.toString());
+		String comment = util.substitute("s/Change\\s([0-9]*?)\\sby\\s(.*?)\\@.*?\\son\\s(.*?\\s.*?)\\n\\n(.*)\\n\\nAffected\\sfiles.*/$4/s", sbDescription.toString());
+		comment = util.substitute("s/\\t//g", comment);
+
+		java.util.Date modifiedTime;
+		try {
+			modifiedTime = p4Date.parse(sModifiedTime);
+		}
+		catch (Exception ex) {
+			log("Wrong date format exception caught. Using lastModified date from project instead.");
+			modifiedTime = _lastModified;
+		}
+
+		if (modifiedTime.compareTo(lastBuild) > 0) {
+			// if it differs, we build,
+			_P4lastChange = Integer.parseInt(sbChangenumber.toString());
+			getAntTask().getProject().setProperty("p4element.change", sbChangenumber.toString());
+
+			// the rest should be a list of the files affected and the resp action
+			String affectedFiles = util.substitute("s/Change\\s([0-9]*?)\\sby\\s(.*?)\\@.*?\\son\\s(.*?\\s.*?)\\n\\n(.*)\\n\\nAffected\\sfiles.*?\\n\\n(.*)\\n\\n/$5/s", sbDescription.toString());
+			java.util.Vector files = new java.util.Vector();
+			util.split(files, "/\\n/s", affectedFiles);
+			java.util.Iterator iter = files.iterator();
+			while (iter.hasNext()) {
+				String file = (String) iter.next();
+				String folderName = util.substitute("s/\\.\\.\\.\\s(\\/\\/.*\\/)(.*?)\\s(.*)/$1/s", file);
+				String fileName = util.substitute("s/\\.\\.\\.\\s(\\/\\/.*\\/)(.*?)\\s(.*)/$2/s", file);
+				String action = util.substitute("s/\\.\\.\\.\\s(\\/\\/.*\\/)(.*?)\\s(.*)/$3/s", file);
+				Modification mod = new Modification();
+				mod.comment = comment;
+				mod.fileName = fileName;
+				mod.folderName = folderName;
+				mod.modifiedTime = modifiedTime;
+				mod.type = action;
+				mod.userName = userName;
+
+				if (mods == null) {
+					mods = new ArrayList();
+				}
+				mods.add(mod);
+			}
+		}
+		else {
+			// otherwise we don't build
+		}
+
+		if (mods == null) {
+			mods = new ArrayList();
+		}
+
+		return mods;
+	}
+
+	/**
+	 *@param  command
+	 *@throws  BuildException
+	 */
+	protected void execP4Command(String command) throws BuildException {
+		execP4Command(command, null);
+	}
+
+	/**
+	 *  Execute P4 commands. Giv a P4Handler to process the events.
+	 *
+	 *@param  command The command to run
+	 *@param  handler A P4Handler to process any input and output
+	 *@throws  BuildException Description of Exception
+	 */
+	protected void execP4Command(String command, P4Handler handler) throws BuildException {
+
+		final Perl5Util util = new Perl5Util();
+
+		try {
+
+			String cCmd = "";
+			if (_P4Client != null) {
+				cCmd = "-c" + _P4Client;
+			}
+			String pCmd = "";
+			if (_P4Port != null) {
+				pCmd = "-p" + _P4Port;
+			}
+			String uCmd = "";
+			if (_P4User != null) {
+				uCmd = "-u" + _P4User;
+			}
+
+			Commandline commandline = new Commandline();
+			commandline.setExecutable("p4");
+
+			commandline.createArgument().setValue(pCmd);
+			commandline.createArgument().setValue(uCmd);
+			commandline.createArgument().setValue(cCmd);
+			commandline.createArgument().setLine(command);
+
+			log("Execing " + commandline);
+
+			// Just a simple handler to record the events in question.
+			//handler = null;
+			if (handler == null) {
+				handler =
+					new P4HandlerAdapter() {
+						public void process(String line) {
+							if (util.match("/^exit/", line)) {
+								return;
+							}
+							if (util.match("/error:/", line) && !util.match("/up-to-date/", line)) {
+								throw new BuildException(line);
+							}
+							log(util.substitute("s/^.*: //", line));
+						}
+					};
+
+			}
+
+			Execute exe = new Execute(handler);
+
+			if (getAntTask() != null) {
+				exe.setAntRun(getAntTask().getProject());
+			}
+
+			exe.setCommandline(commandline.getCommandline());
+
+			try {
+				exe.execute();
+			}
+			catch (IOException e) {
+				throw new BuildException(e);
+			}
+			finally {
+				try {
+					handler.stop();
+				}
+				catch (Exception e) {
+				}
+			}
+
+		}
+		catch (Exception e) {
+			throw new BuildException("Problem exec'ing P4 command: " + e.getMessage());
+		}
+	}
+
+}
+// P4Element
