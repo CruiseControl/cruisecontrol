d2c0ad525c47a0f73a55be02adf514d0965be89b
Jason Yip <jchyip@users.sourceforge.net>
2001-06-25 19:26:45 +0000

    Update build.sh to do more checks, works on cygwin
    git-svn-id: https://cruisecontrol.svn.sourceforge.net/svnroot/cruisecontrol/trunk/cruisecontrol@195 878de816-9c12-0410-b5fe-b34e1928e20d

--- main/build.sh
+++ main/build.sh
@@ -21,12 +21,69 @@
 #  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.  *
 #  ********************************************************************************/
 
-OLDCLASSPATH=$CLASSPATH
+#--------------------------------------------
+# You may modify the default values below.
+#--------------------------------------------
 
-CLASSPATH=lib/ant.jar:lib/optional.jar:lib/junit.jar
-export CLASSPATH
+# The name of the build file to use
+BUILDFILE=build.xml
 
-ant
+# Root directory for the project
+PROJECTDIR=.
 
-CLASSPATH=$OLDCLASSPATH
-export CLASSPATH
+# Directory where necessary build Java libraries are found
+LIBDIR=${PROJECTDIR}/lib
+
+#--------------------------------------------
+# No need to edit anything past here
+#--------------------------------------------
+
+# Try to find Java Home directory, from JAVA_HOME environment 
+# or java executable found in PATH
+
+if test -z "${JAVA_HOME}" ; then
+    # JAVA_HOME is not set, try to set it if java is in PATH
+    JAVABIN=`type java`
+    if [ $? -eq 0 ]
+    then
+        # We found something, clean the path to get a valid JAVA_HOME
+        JAVA_HOME=`echo $javart | sed -e 's/\/bin\/java.*//' `
+    else
+        echo "ERROR: JAVA_HOME not found in your environment."
+        echo "Please, set the JAVA_HOME variable in your environment to match the"
+        echo "location of the Java Virtual Machine you want to use."
+        exit
+    fi
+fi
+
+# convert the existing path to unix
+if [ "$OSTYPE" = "cygwin32" ] || [ "$OSTYPE" = "cygwin" ] ; then
+   CLASSPATH=`cygpath --path --unix "$CLASSPATH"`
+   JAVA_HOME=`cygpath --path --unix "$JAVA_HOME"`
+fi
+
+# Define the java executable path
+if [ "$JAVABIN" = "" ] ; then
+  JAVABIN=${JAVA_HOME}/bin/java
+fi
+
+# Find all jars defined in LIBDIR and add them to the classpath
+for lib in ${LIBDIR:-.}/*.jar
+do
+    CLASSPATH=${CLASSPATH}:${lib}
+done
+
+# Try to include tools.jar for compilation
+if test -f ${JAVA_HOME}/lib/tools.jar ; then
+    CLASSPATH=${CLASSPATH}:${JAVA_HOME}/lib/tools.jar
+fi
+
+# convert the unix path to windows
+if [ "$OSTYPE" = "cygwin32" ] || [ "$OSTYPE" = "cygwin" ] ; then
+   CLASSPATH=`cygpath --path --windows "$CLASSPATH"`
+   JAVA_HOME=`cygpath --path --windows "$JAVA_HOME"`
+fi
+
+# Call Ant
+${JAVABIN} -cp "${CLASSPATH}" org.apache.tools.ant.Main \
+           -buildfile "${BUILDFILE}" "$@"
