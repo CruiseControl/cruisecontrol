531b7e48a56d50fef3186ee7ad5710e9a432f64c
Paul Julius <pauljulius@users.sourceforge.net>
2001-06-28 03:59:02 +0000

    Changed Licensing from GPL to BSD.

--- main/build.bat
+++ main/build.bat
@@ -1,25 +1,41 @@
 @echo off
+REM ################################################################################
+REM # CruiseControl, a Continuous Integration Toolkit
+REM # Copyright (c) 2001, ThoughtWorks, Inc.
+REM # 651 W Washington Ave. Suite 500
+REM # Chicago, IL 60661 USA
+REM # All rights reserved.
+REM # 
+REM # Redistribution and use in source and binary forms, with or without 
+REM # modification, are permitted provided that the following conditions
+REM # are met:
+REM # 
+REM #     + Redistributions of source code must retain the above copyright 
+REM #       notice, this list of conditions and the following disclaimer. 
+REM #       
+REM #     + Redistributions in binary form must reproduce the above 
+REM #       copyright notice, this list of conditions and the following 
+REM #       disclaimer in the documentation and/or other materials provided 
+REM #       with the distribution. 
+REM #       
+REM #     + Neither the name of ThoughtWorks, Inc., CruiseControl, nor the 
+REM #       names of its contributors may be used to endorse or promote 
+REM #       products derived from this software without specific prior 
+REM #       written permission. 
+REM # 
+REM # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+REM # "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+REM # LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
+REM # A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR 
+REM # CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+REM # EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+REM # PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+REM # PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+REM # LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+REM # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+REM # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+REM ################################################################################
 
-REM /********************************************************************************
-REM  * CruiseControl, a Continuous Integration Toolkit                              *
-REM  * Copyright (C) 2001  ThoughtWorks, Inc.                                       *
-REM  * 651 W Washington Ave. Suite 500                                              *
-REM  * Chicago, IL 60661 USA                                                        *
-REM  *                                                                              *
-REM  * This program is free software; you can redistribute it and/or                *
-REM  * modify it under the terms of the GNU General Public License                  *
-REM  * as published by the Free Software Foundation; either version 2               *
-REM  * of the License, or (at your option) any later version.                       *
-REM  *                                                                              *
-REM  * This program is distributed in the hope that it will be useful,              *
-REM  * but WITHOUT ANY WARRANTY; without even the implied warranty of               *
-REM  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                *
-REM  * GNU General Public License for more details.                                 *
-REM  *                                                                              *
-REM  * You should have received a copy of the GNU General Public License            *
-REM  * along with this program; if not, write to the Free Software                  *
-REM  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.  *
-REM  ********************************************************************************/
 set OLDCLASSPATH=CLASSPATH
 
 set CLASSPATH=lib/ant.jar;lib/optional.jar;lib/junit.jar
