636070f354f6d097641aba8cbc2ce5e4861f8dff
Jason Yip <jchyip@users.sourceforge.net>
2001-08-11 18:05:16 +0000

    ClearCaseElement nows extract comments (currently replaced with an empty string) from the list of changes returned by the server.
    Patch by Eric Lefevre and Ralf Krakowski

--- main/src/net/sourceforge/cruisecontrol/element/ClearCaseElement.java
+++ main/src/net/sourceforge/cruisecontrol/element/ClearCaseElement.java
@@ -4,34 +4,34 @@
  * 651 W Washington Ave. Suite 500
  * Chicago, IL 60661 USA
  * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without 
+ *
+ * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- * 
- *     + Redistributions of source code must retain the above copyright 
- *       notice, this list of conditions and the following disclaimer. 
- *       
- *     + Redistributions in binary form must reproduce the above 
- *       copyright notice, this list of conditions and the following 
- *       disclaimer in the documentation and/or other materials provided 
- *       with the distribution. 
- *       
- *     + Neither the name of ThoughtWorks, Inc., CruiseControl, nor the 
- *       names of its contributors may be used to endorse or promote 
- *       products derived from this software without specific prior 
- *       written permission. 
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR 
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+ *
+ *     + Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     + Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *     + Neither the name of ThoughtWorks, Inc., CruiseControl, nor the
+ *       names of its contributors may be used to endorse or promote
+ *       products derived from this software without specific prior
+ *       written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  ********************************************************************************/
 package net.sourceforge.cruisecontrol.element;
@@ -47,265 +47,282 @@ import org.apache.tools.ant.Task;
 /**
  *  This class implements the SourceControlElement methods for a Clear Case
  *  repository.
- * 
+ *
  * @author Thomas Leseney
  * @author <a href="mailto:jcyip@thoughtworks.com">Jason Yip</a>
+ * @author Eric Lefevre
+ * @author Ralf Krakowski
  */
 public class ClearCaseElement extends SourceControlElement {
 
-	/**
-	 * Set of the authors that modified files. With Clear Case, it corresponds 
-     * to the user names.
-	 */
-	private Set _emailNames = new HashSet();
+  /**
+   * Set of the authors that modified files. With Clear Case, it corresponds
+   * to the user names.
+   */
+  private Set _emailNames = new HashSet();
 
-	/**
-	 *  This date indicates the most recent modification time.
-	 */
-	private Date _lastModified;
+  /**
+   *  This date indicates the most recent modification time.
+   */
+  private Date _lastModified;
 
-	/**
-	 *  The path of the clear case view
-	 */
-	private String _viewPath;
+  /**
+   *  The path of the clear case view
+   */
+  private String _viewPath;
 
-	/**
-	 *  The branch to check for modifications
-	 */
-	private String _branch = null;
-	private boolean _recursive = true;
+  /**
+   *  The branch to check for modifications
+   */
+  private String _branch = null;
+  private boolean _recursive = true;
 
-	/**
-	 *  Date format required by commands passed to Clear Case
-	 */
-	final static SimpleDateFormat IN_DATE_FORMAT = 
+  /**
+   *  Date format required by commands passed to Clear Case
+   */
+  final static SimpleDateFormat IN_DATE_FORMAT =
      new SimpleDateFormat("dd-MMMM-yyyy.HH:mm:ss");
 
-	/**
-	 *  Date format returned in the output of Clear Case commands.
-	 */
-	final static SimpleDateFormat OUT_DATE_FORMAT = 
+  /**
+   *  Date format returned in the output of Clear Case commands.
+   */
+  final static SimpleDateFormat OUT_DATE_FORMAT =
      new SimpleDateFormat("yyyyMMdd.HHmmss");
 
-	/**
-	 *  Sets the local working copy to use when making queries.
-	 *
-	 *@param  path
-	 */
-	public void setViewpath(String path) {
-		_viewPath = getAntTask().getProject().resolveFile(path).getAbsolutePath();
-	}
+  /**
+   *  Unlikely combinaison of 2 characters to create a String delimiter
+   */
+  final static String DELIMITER = "££";
 
-	/**
-	 *  Sets the branch that we're concerned about checking files into.
-	 *
-	 *@param  branch
-	 */
-	public void setBranch(String branch) {
-		_branch = branch;
-	}
+  /**
+   *  Sets the local working copy to use when making queries.
+   *
+   *@param  path
+   */
+  public void setViewpath(String path) {
+    _viewPath = getAntTask().getProject().resolveFile(path).getAbsolutePath();
+  }
 
-	public void setRecursive(boolean b) {
-		_recursive = b;
-	}
+  /**
+   *  Sets the branch that we're concerned about checking files into.
+   *
+   *@param  branch
+   */
+  public void setBranch(String branch) {
+    _branch = branch;
+  }
 
-	/**
-	 *  Returns a Set of email addresses. since Clear Case doesn't track actual
-	 *  email addresse, we just return the usernames, which may correspond to
-	 *  emails ids.
-	 *
-	 *@return
-	 */
-	public Set getEmails() {
-		return _emailNames;
-	}
+  public void setRecursive(boolean b) {
+    _recursive = b;
+  }
 
-	/**
-	 *  Gets the last modified time for the set of files queried in the {@link
-	 *  #getHistory} method.
-	 *
-	 *@return  the lastest revision time.
-	 */
-	public long getLastModified() {
-		if (_lastModified == null) {
-			return 0;
-		}
-		return _lastModified.getTime();
-	}
+  /**
+   *  Returns a Set of email addresses. since Clear Case doesn't track actual
+   *  email addresse, we just return the usernames, which may correspond to
+   *  emails ids.
+   *
+   *@return
+   */
+  public Set getEmails() {
+    return _emailNames;
+  }
 
-	/**
-	 *  Returns an {@link java.util.List List} of {@link Modification}
-	 *  detailing all the changes between now and the last build.
-	 *
-	 *@param  lastBuild the last build time
-	 *@param  now time now, or time to check, NOT USED
-	 *@param  quietPeriod NOT USED
-	 *@return  the list of modifications, an empty (not null) list if no
-	 *      modifications.
-	 */
-	public List getHistory(Date lastBuild, Date now, long quietPeriod) {
-		String lastBuildDate = IN_DATE_FORMAT.format(lastBuild);
-		/*
-		 * let's try a different clearcase command--this one just takes 
+  /**
+   *  Gets the last modified time for the set of files queried in the {@link
+   *  #getHistory} method.
+   *
+   *@return  the lastest revision time.
+   */
+  public long getLastModified() {
+    if (_lastModified == null) {
+      return 0;
+    }
+    return _lastModified.getTime();
+  }
+
+  /**
+   *  Returns an {@link java.util.List List} of {@link Modification}
+   *  detailing all the changes between now and the last build.
+   *
+   *@param  lastBuild the last build time
+   *@param  now time now, or time to check, NOT USED
+   *@param  quietPeriod NOT USED
+   *@return  the list of modifications, an empty (not null) list if no
+   *      modifications.
+   */
+  public List getHistory(Date lastBuild, Date now, long quietPeriod) {
+    String lastBuildDate = IN_DATE_FORMAT.format(lastBuild);
+    /*
+     * let's try a different clearcase command--this one just takes
          * waaaaaaaay too long.
-		 * String command = "cleartool find " + _viewPath +
-		 * " -type f -exec \"cleartool lshistory" +
-		 * " -since " + lastBuildDate;
-		 * if(_branch != null)
-		 * command += " -branch " + _branch;
-		 * command += " -nco" + // exclude check out events
-		 * " -fmt \\\" %u;%Nd;%n;%o \\n \\\" \\\"%CLEARCASE_XPN%\\\" \"";
-		 */
-		String command = "cleartool lshistory";
-	
+     * String command = "cleartool find " + _viewPath +
+     * " -type f -exec \"cleartool lshistory" +
+     * " -since " + lastBuildDate;
+     * if(_branch != null)
+     * command += " -branch " + _branch;
+     * command += " -nco" + // exclude check out events
+     * " -fmt \\\" %u;%Nd;%n;%o \\n \\\" \\\"%CLEARCASE_XPN%\\\" \"";
+     */
+    String command = "cleartool lshistory";
+
         if (_branch != null) {
-			command += " -branch " + _branch;
-		}
-		
+      command += " -branch " + _branch;
+    }
+
         if (_recursive == true) {
-			command += " -r ";
-		}
-		
+      command += " -r ";
+    }
+
         command += " -nco -since " + lastBuildDate;
-		command += " -fmt \"%u;%Nd;%n;%o\\n\" " + _viewPath;
+    command += " -fmt \"%u"+DELIMITER+"%Nd"+DELIMITER+"%n"+DELIMITER+"%o"+DELIMITER+"%Nc\\n\" " + _viewPath;
 
-		log("Command to execute : " + command);
+    log("Command to execute : " + command);
         List modifications = null;
-		try {
-			Process p = Runtime.getRuntime().exec(command);
+    try {
+      Process p = Runtime.getRuntime().exec(command);
 
-			StreamPumper errorPumper = new StreamPumper(p.getErrorStream());
-			new Thread(errorPumper).start();
+      StreamPumper errorPumper = new StreamPumper(p.getErrorStream());
+      new Thread(errorPumper).start();
 
-			InputStream input = p.getInputStream();
-			modifications = parseStream(input);
+      InputStream input = p.getInputStream();
+      modifications = parseStream(input);
 
-			p.waitFor();
-		} catch (Exception e) {
-			log("Error in executing the Clear Case command : " + e);
-			e.printStackTrace();
-		}
-        
-		if (modifications == null) {
-			modifications = new ArrayList();
-		}
-        
-		return modifications;
-	}
+      p.waitFor();
+    } catch (Exception e) {
+      log("Error in executing the Clear Case command : " + e);
+      e.printStackTrace();
+    }
 
-	/**
-	 *  Parses the input stream to construct the modifications list.
-	 *
-	 *@param  input the stream to parse
-	 *@return  a list of modification elements
-	 *@exception  IOException
-	 */
-	private List parseStream(InputStream input) throws IOException {
-		ArrayList modifications = new ArrayList();
-		BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+    if (modifications == null) {
+      modifications = new ArrayList();
+    }
 
-		String line;
+    return modifications;
+  }
 
-		while ((line = reader.readLine()) != null) {
-			Modification mod = parseEntry(line);
-			if (mod != null) {
-				modifications.add(mod);
-			}
-		}
-		return modifications;
-	}
+  /**
+   *  Parses the input stream to construct the modifications list.
+   *
+   *@param  input the stream to parse
+   *@return  a list of modification elements
+   *@exception  IOException
+   */
+  private List parseStream(InputStream input) throws IOException {
+    ArrayList modifications = new ArrayList();
+    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
 
-	/**
-	 *  Parses a single line from the reader. Each line contains a signe revision
-	 *  with the format : <br>
-	 *  username;date_of_revision;element_name;operation_type <br>
-	 *
-	 *
-	 *@param  line the line to parse
-	 *@return  a modification element corresponding to the given line
-	 */
-	private Modification parseEntry(String line) {
-		System.out.println("parsing entry: " + line);
-		StringTokenizer st = new StringTokenizer(line, ";");
-		if (st.countTokens() != 4) {
-			return null;
-		}
-		String username = st.nextToken().trim();
-		String timeStamp = st.nextToken().trim();
-		String elementName = st.nextToken().trim();
-		String operationType = st.nextToken().trim();
+    String line;
 
-		/*
-		 *  a branch event shouldn't trigger a build
-		 */
-		if (operationType.equals("mkbranch")) {
-			return null;
-		}
+    while ((line = reader.readLine()) != null) {
+      Modification mod = parseEntry(line);
+      if (mod != null) {
+        modifications.add(mod);
+      }
+    }
+    return modifications;
+  }
 
-		Modification mod = new Modification();
+  /**
+   *  Parses a single line from the reader. Each line contains a signe revision
+   *  with the format : <br>
+   *  username££date_of_revision££element_name££operation_type££comments <br>
+   *
+   *
+   *@param  line the line to parse
+   *@return  a modification element corresponding to the given line
+   */
+  private Modification parseEntry(String line) {
+    System.out.println("parsing entry: " + line);
+    StringTokenizer st = new StringTokenizer(line, DELIMITER);
 
-		mod.userName = username;
-		_emailNames.add(mod.userName);
+    // we should get either 4 (w/o comments) or 5 tokens (w/ comments)
+    if ( (st.countTokens()<4) || (st.countTokens()>5) ) {
+      return null;
+    }
+    String username = st.nextToken().trim();
+    String timeStamp = st.nextToken().trim();
+    String elementName = st.nextToken().trim();
+    String operationType = st.nextToken().trim();
 
-		elementName = elementName.substring(elementName.indexOf(":\\") + 1);
-		String fileName = elementName.substring(0, elementName.indexOf("@@"));
+    String comment;
+    if ( st.countTokens() > 4 ) {
+      comment = st.nextToken().trim();
+    }
+    else {
+      comment = "";
+    }
 
-		mod.fileName = fileName.substring(fileName.lastIndexOf("\\"));
-		mod.folderName = fileName.substring(0, fileName.lastIndexOf("\\"));
+    /*
+     *  a branch event shouldn't trigger a build
+     */
+    if (operationType.equals("mkbranch")) {
+      return null;
+    }
 
-		try {
-			mod.modifiedTime = OUT_DATE_FORMAT.parse(timeStamp);
-			updateLastModified(mod.modifiedTime);
-		} catch (ParseException e) {
-			mod.modifiedTime = null;
-		}
+    Modification mod = new Modification();
 
-		mod.type = operationType;
+    mod.userName = username;
+    _emailNames.add(mod.userName);
 
-		mod.comment = "";
+    elementName = elementName.substring(elementName.indexOf(File.separator));
+    String fileName = elementName.substring(0, elementName.indexOf("@@"));
 
-		return mod;
-	}
+    mod.fileName = fileName.substring(fileName.lastIndexOf(File.separator));
+    mod.folderName = fileName.substring(0, fileName.lastIndexOf(File.separator));
 
-	/**
-	 *  Updates the lastModified date if necessary (new date is after the current
-	 *  lastModified date).
-	 *
-	 *@param  newPossible the new possible lastModified date
-	 */
-	private void updateLastModified(Date newPossible) {
-		if (_lastModified == null || _lastModified.before(newPossible)) {
-			_lastModified = newPossible;
-		}
-	}
+    try {
+      mod.modifiedTime = OUT_DATE_FORMAT.parse(timeStamp);
+      updateLastModified(mod.modifiedTime);
+    } catch (ParseException e) {
+      mod.modifiedTime = null;
+    }
 
-	/**
-	 *  Inner class to pump the error stream during Process's runtime. Copied from
-	 *  the Ant built-in task.
-	 */
-	class StreamPumper implements Runnable {
+    mod.type = operationType;
 
-		private InputStream _in;
-		private final static int SIZE = 128;
-		private final static int SLEEP = 5;
+    mod.comment = comment;
 
-		public StreamPumper(InputStream in) {
-			_in = in;
-		}
+    return mod;
+  }
 
-		public void run() {
-			final byte[] buf = new byte[SIZE];
-			int length;
+  /**
+   *  Updates the lastModified date if necessary (new date is after the current
+   *  lastModified date).
+   *
+   *@param  newPossible the new possible lastModified date
+   */
+  private void updateLastModified(Date newPossible) {
+    if (_lastModified == null || _lastModified.before(newPossible)) {
+      _lastModified = newPossible;
+    }
+  }
 
-			try {
-				while ((length = _in.read(buf)) > 0) {
-					System.err.write(buf, 0, length);
-					try {
-						Thread.sleep(SLEEP);
-					}
-					catch (InterruptedException ignoredInterrupted) { }
-				}
-			} catch (IOException ignoredIOE) { }
-		}
-	}
+  /**
+   *  Inner class to pump the error stream during Process's runtime. Copied from
+   *  the Ant built-in task.
+   */
+  class StreamPumper implements Runnable {
+
+    private InputStream _in;
+    private final static int SIZE = 128;
+    private final static int SLEEP = 5;
+
+    public StreamPumper(InputStream in) {
+      _in = in;
+    }
+
+    public void run() {
+      final byte[] buf = new byte[SIZE];
+      int length;
+
+      try {
+        while ((length = _in.read(buf)) > 0) {
+          System.err.write(buf, 0, length);
+          try {
+            Thread.sleep(SLEEP);
+          }
+          catch (InterruptedException ignoredInterrupted) { }
+        }
+      } catch (IOException ignoredIOE) { }
+    }
+  }
 }
