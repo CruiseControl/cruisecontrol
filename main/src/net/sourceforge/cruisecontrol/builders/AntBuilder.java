710f4fa9255f7237c2ff24b5da50980426ae7627
Alden Almagro <alden@users.sourceforge.net>
2002-01-10 16:47:29 +0000

    extracts ant from the main build loop. also runs ant in a new process to help address memory leaks etc.
    git-svn-id: https://cruisecontrol.svn.sourceforge.net/svnroot/cruisecontrol/trunk/cruisecontrol@352 878de816-9c12-0410-b5fe-b34e1928e20d

---
+++ main/src/net/sourceforge/cruisecontrol/builders/AntBuilder.java
@@ -0,0 +1,166 @@
+/********************************************************************************
+ * CruiseControl, a Continuous Integration Toolkit
+ * Copyright (c) 2001, ThoughtWorks, Inc.
+ * 651 W Washington Ave. Suite 500
+ * Chicago, IL 60661 USA
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     + Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     + Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *     + Neither the name of ThoughtWorks, Inc., CruiseControl, nor the
+ *       names of its contributors may be used to endorse or promote
+ *       products derived from this software without specific prior
+ *       written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ********************************************************************************/
+
+package net.sourceforge.cruisecontrol.builders;
+
+import net.sourceforge.cruisecontrol.Builder;
+import net.sourceforge.cruisecontrol.util.StreamPumper;
+import org.apache.log4j.Category;
+import org.jdom.Element;
+import org.jdom.input.SAXBuilder;
+import org.jdom.output.XMLOutputter;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ *  we often see builds that fail because the previous build is still holding on to some resource.
+ *  we can avoid this by just building in a different process which will completely die after every
+ *  build.
+ */
+public class AntBuilder extends Builder {
+
+    /** enable logging for this class */
+    private static Category log = Category.getInstance(AntBuilder.class.getName());
+
+    private String _buildFile;
+    private String _target;
+
+    /**
+     *  build and return the results via xml.  debug status can be determined from log4j category once we
+     *  get all the logging in place.
+     */
+    public Element build(Map buildProperties) {
+
+        try {
+            Process p = Runtime.getRuntime().exec(getCommandLineArgs(buildProperties));
+            StreamPumper errorPumper = new StreamPumper(p.getErrorStream());
+            new Thread(errorPumper).start();
+            InputStream input = p.getInputStream();
+            p.waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //read in log file as element, return it
+        File log = new File("log.xml");
+        Element buildLogElement = getAntLogAsElement(log);
+        log.delete();
+        return buildLogElement;
+    }
+
+    public void setTarget(String target) {
+        _target = target;
+    }
+
+    public void setBuildFile(String buildFile) {
+        _buildFile = buildFile;
+    }
+
+    /**
+     *  construct the command that we're going to execute.
+     *  @param buildProperties Map holding key/value pairs of arguments to the build process
+     *  @return String[] holding command to be executed
+     */
+    protected String[] getCommandLineArgs(Map buildProperties) {
+        List al = new ArrayList();
+        al.add("java");
+        al.add("org.apache.tools.ant.Main");
+        al.add("-listener");
+        al.add("org.apache.tools.ant.XmlLogger");
+
+        Iterator propertiesIterator = buildProperties.keySet().iterator();
+        while (propertiesIterator.hasNext()) {
+            String key = (String) propertiesIterator.next();
+            al.add("-D" + key + "=" + buildProperties.get(key));
+        }
+
+        if (log.isDebugEnabled()) {
+            al.add("-debug");
+            al.add("-verbose");
+        }
+
+        al.add("-buildfile");
+        al.add(_buildFile);
+        al.add(_target);
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("Executing Command: ");
+        Iterator argIterator = al.iterator();
+        while (argIterator.hasNext()) {
+            String arg = (String) argIterator.next();
+            sb.append(arg);
+            sb.append(" ");
+        }
+        log.debug(sb.toString());
+
+        return (String[]) al.toArray(new String[0]);
+    }
+
+    /**
+     *  JDOM doesn't like the <?xml:stylesheet ?> tag.  we don't need it, so we'll skip it.
+     *  TO DO: make sure that we are only skipping this string and not something else
+     */
+    protected static Element getAntLogAsElement(File f) {
+        try {
+            BufferedInputStream bufferedStream = new BufferedInputStream(new FileInputStream(f));
+            bufferedStream.skip(49);
+            SAXBuilder builder = new SAXBuilder("org.apache.xerces.parsers.SAXParser");
+            return builder.build(bufferedStream).getRootElement();
+        } catch (Exception ee) {
+            ee.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     *  temporarily for testing
+     */
+    public static void main(String[] args) {
+        AntBuilder ab = new AntBuilder();
+        ab.setTarget("init");
+        ab.setBuildFile("build.xml");
+
+        Element log = null;//ab.build();
+        try {
+            XMLOutputter output = new XMLOutputter();
+            output.output(log, System.out);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+}
---
+++ main/test/net/sourceforge/cruisecontrol/builders/AntBuilderTest.java
@@ -0,0 +1,92 @@
+/********************************************************************************
+ * CruiseControl, a Continuous Integration Toolkit
+ * Copyright (c) 2001, ThoughtWorks, Inc.
+ * 651 W Washington Ave. Suite 500
+ * Chicago, IL 60661 USA
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     + Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     + Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *     + Neither the name of ThoughtWorks, Inc., CruiseControl, nor the
+ *       names of its contributors may be used to endorse or promote
+ *       products derived from this software without specific prior
+ *       written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ********************************************************************************/
+
+package net.sourceforge.cruisecontrol.builders;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+import org.apache.log4j.*;
+import org.jdom.Element;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Hashtable;
+
+public class AntBuilderTest extends TestCase {
+
+    /** enable logging for this class */
+    private static Category log = Category.getInstance(AntBuilderTest.class.getName());
+
+    public AntBuilderTest(String name) {
+        super(name);
+    }
+
+    public void testGetCommandLineArgs() {
+        AntBuilder builder = new AntBuilder();
+        builder.setTarget("target");
+        builder.setBuildFile("buildfile");
+        Hashtable properties = new Hashtable();
+        properties.put("label", "200.1.23");
+        String[] resultDebug = { "java","org.apache.tools.ant.Main","-listener","org.apache.tools.ant.XmlLogger","-Dlabel=200.1.23","-debug","-verbose","-buildfile","buildfile","target"};
+        String[] resultInfo = { "java","org.apache.tools.ant.Main","-listener","org.apache.tools.ant.XmlLogger","-Dlabel=200.1.23","-buildfile","buildfile","target"};
+        BasicConfigurator.configure(new ConsoleAppender(new PatternLayout("%m%n")));
+
+        log.getRoot().setPriority(Priority.INFO);
+        Assert.assertTrue(Arrays.equals(resultInfo, builder.getCommandLineArgs(properties)));
+
+        log.getRoot().setPriority(Priority.DEBUG);
+        Assert.assertTrue(Arrays.equals(resultDebug, builder.getCommandLineArgs(properties)));
+    }
+
+    public void testGetAntLogAsElement() {
+        try {
+            Element buildLogElement = new Element("build");
+            File logFile = new File("_tempAntLog.xml");
+            BufferedWriter bw1 = new BufferedWriter(new FileWriter(logFile));
+            bw1.write("<?xml:stylesheet type=\"text/xsl\" href=\"log.xsl\"?><build></build>");
+            bw1.flush();
+            bw1.close();
+            AntBuilder builder = new AntBuilder();
+            Assert.assertEquals(buildLogElement.toString(), builder.getAntLogAsElement(logFile).toString());
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+}
