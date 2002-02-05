a22aa75874da189caf0dbd80b71178681616c509
Alden Almagro <alden@users.sourceforge.net>
2002-02-05 23:35:08 +0000

    updated to use JDOM for handling xml properly, including escaping special characters.  idea suggested by Dan Corneanu.

--- main/src/net/sourceforge/cruisecontrol/Modification.java
+++ main/src/net/sourceforge/cruisecontrol/Modification.java
@@ -4,79 +4,109 @@
  * 651 W Washington Ave. Suite 500
  * Chicago, IL 60661 USA
  * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without 
+ *
+ * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- * 
- *     + Redistributions of source code must retain the above copyright 
- *       notice, this list of conditions and the following disclaimer. 
- *       
- *     + Redistributions in binary form must reproduce the above 
- *       copyright notice, this list of conditions and the following 
- *       disclaimer in the documentation and/or other materials provided 
- *       with the distribution. 
- *       
- *     + Neither the name of ThoughtWorks, Inc., CruiseControl, nor the 
- *       names of its contributors may be used to endorse or promote 
- *       products derived from this software without specific prior 
- *       written permission. 
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR 
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+ *
+ *     + Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     + Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *     + Neither the name of ThoughtWorks, Inc., CruiseControl, nor the
+ *       names of its contributors may be used to endorse or promote
+ *       products derived from this software without specific prior
+ *       written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  ********************************************************************************/
- 
+
 package net.sourceforge.cruisecontrol;
 
-import java.util.Date;
+import org.jdom.CDATA;
+import org.jdom.Element;
+import org.jdom.output.XMLOutputter;
+
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * data structure for holding data about a single modification
  * to a source control tool.
- * 
+ *
  * @author <a href="mailto:alden@thoughtworks.com">alden almagro</a>
  */
 public class Modification {
 
-  public String type = "unknown";
-  public String fileName;
-  public String folderName;
-  public Date modifiedTime;
-  public String userName;
-  public String comment = "";
-  
-  public String toXml(DateFormat formatter) {
-     StringBuffer sb = new StringBuffer();
-     sb.append("   <modification type=\"" + type + "\">\n");
-     sb.append("     <filename>" + fileName + "</filename>\n");
-     sb.append("     <project>" + folderName + "</project>\n");
-     sb.append("     <date>" + formatter.format(modifiedTime) + "</date>\n");
-     sb.append("     <user>" + userName + "</user>\n");
-     sb.append("     <comment><![CDATA[" + comment + "]]></comment>\n");
-     sb.append("   </modification>\n");
+    public String type = "unknown";
+    public String fileName;
+    public String folderName;
+    public Date modifiedTime;
+    public String userName;
+    public String comment = "";
 
-     return sb.toString();
-  }
+    public Element toElement(DateFormat formatter) {
+        Element modificationElement = new Element("modification");
+        modificationElement.setAttribute("type", type);
+        Element filenameElement = new Element("filename");
+        filenameElement.addContent(fileName);
+        Element projectElement = new Element("project");
+        projectElement.addContent(folderName);
+        Element dateElement = new Element("date");
+        dateElement.addContent(formatter.format(modifiedTime));
+        Element userElement = new Element("user");
+        userElement.addContent(userName);
+        Element commentElement = new Element("comment");
+        commentElement.addContent(new CDATA(comment));
 
-  public String toString(DateFormat formatter) {
-     StringBuffer sb = new StringBuffer();
-     sb.append("FileName: " + fileName + "\n");
-     sb.append("FolderName: " + folderName + "\n");
-     sb.append("Last Modified: " + formatter.format(modifiedTime) + "\n");
-     sb.append("UserName: " + userName + "\n");
-     sb.append("Comment: " + comment + "\n\n");
+        modificationElement.addContent(filenameElement);
+        modificationElement.addContent(projectElement);
+        modificationElement.addContent(dateElement);
+        modificationElement.addContent(userElement);
+        modificationElement.addContent(commentElement);
 
-     return sb.toString();
-  }
+        return modificationElement;
+    }
 
+    public String toXml(DateFormat formatter) {
+        XMLOutputter outputter = new XMLOutputter();
+        return outputter.outputString(toElement(formatter));
+    }
+
+    public String toString(DateFormat formatter) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("FileName: " + fileName + "\n");
+        sb.append("FolderName: " + folderName + "\n");
+        sb.append("Last Modified: " + formatter.format(modifiedTime) + "\n");
+        sb.append("UserName: " + userName + "\n");
+        sb.append("Comment: " + comment + "\n\n");
+        return sb.toString();
+    }
+
+    //for brief testing only
+    public static void main(String args[]) {
+        Date now = new Date();
+        Modification mod = new Modification();
+        mod.fileName = "File\"Name&";
+        mod.folderName = "Folder'Name";
+        mod.modifiedTime = now;
+        mod.userName = "User<>Name";
+        mod.comment = "Comment";
+        System.out.println(mod.toXml(new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")));
+    }
 }
