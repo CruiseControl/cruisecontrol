e68eb3e715030e39cd6cfebae5f6c24646ebe313
Alden Almagro <alden@users.sourceforge.net>
2002-02-05 23:36:19 +0000

    add jdom.jar to classpath

--- main/dist/cruiseControl.sh
+++ main/dist/cruiseControl.sh
@@ -39,7 +39,7 @@
 
 CCDIR=..
 LIBDIR=$CCDIR/lib
-CRUISE_PATH=$JAVA_HOME/lib/tools.jar:cruisecontrol.jar:$LIBDIR/ant.jar:$LIBDIR/xerces.jar:$LIBDIR/mail.jar:$LIBDIR/optional.jar:$LIBDIR/junit.jar:$LIBDIR/activation.jar:.
+CRUISE_PATH=$JAVA_HOME/lib/tools.jar:cruisecontrol.jar:$LIBDIR/jdom.jar:$LIBDIR/ant.jar:$LIBDIR/xerces.jar:$LIBDIR/mail.jar:$LIBDIR/optional.jar:$LIBDIR/junit.jar:$LIBDIR/activation.jar:.
 
 EXEC="java -cp $CRUISE_PATH net.sourceforge.cruisecontrol.MasterBuild $@"
 echo $EXEC
