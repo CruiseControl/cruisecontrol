811ae6b84b263b5e7bca411b126825ce5046d707
Alden Almagro <alden@users.sourceforge.net>
2002-02-05 23:34:04 +0000

    testing that proper chars are escaped

---
+++ main/test/net/sourceforge/cruisecontrol/ModificationTest.java
@@ -0,0 +1,29 @@
+package net.sourceforge.cruisecontrol;
+
+import junit.framework.TestCase;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class ModificationTest extends TestCase {
+
+    public ModificationTest(String name) {
+        super(name);
+    }
+
+    public void testToXml() {
+        Date modifiedTime = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+        Modification mod = new Modification();
+        mod.fileName = "File\"Name&";
+        mod.folderName = "Folder'Name";
+        mod.modifiedTime = modifiedTime;
+        mod.userName = "User<>Name";
+        mod.comment = "Comment";
+
+        String expected = "<modification type=\"unknown\"><filename>File\"Name&amp;</filename><project>Folder'Name</project><date>" +
+        formatter.format(modifiedTime) + "</date><user>User&lt;&gt;Name</user><comment><![CDATA[Comment]]></comment></modification>";
+
+        assertEquals(mod.toXml(formatter), expected);
+    }
+}
