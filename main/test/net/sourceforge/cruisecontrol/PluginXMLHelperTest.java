8248b46a3c4bfc4d398498667cd2a831d44f32b0
Niclas Olofsson <gurun@users.sourceforge.net>
2002-03-18 21:44:12 +0000

    Rewrote some of the test to point out some of the stuff I think one should consider when writing unit-tests. A real //(POLICE) business. Alden says he is hard to insult, but I'm working on it :-)

--- main/test/net/sourceforge/cruisecontrol/PluginXMLHelperTest.java
+++ main/test/net/sourceforge/cruisecontrol/PluginXMLHelperTest.java
@@ -38,43 +38,58 @@
 package net.sourceforge.cruisecontrol;
 
 import junit.framework.TestCase;
-import org.jdom.Element;
 import net.sourceforge.cruisecontrol.publishers.MockPublisher;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Category;
+import org.jdom.Element;
+
+//(POLICE) Been here
 
 public class PluginXMLHelperTest extends TestCase {
+	private static Category log = Category.getInstance(PluginXMLHelperTest.class.getName());
 
-    public PluginXMLHelperTest(String name) {
-        super(name);
-    }
+	public PluginXMLHelperTest(String name) {
+		super(name);
+		//(POLICE) This is one way of making _really_ sure that log4j is keeping its mouth shut.
+		// This is probably defined in a log4j.config file somewhere. We usually have it in a
+		// JUnit suite (but CC doesn't have that right now).
+		BasicConfigurator.configure();
+		log.getHierarchy().disableAll();
+	}
 
-    public void testConfigure() {
-        Element testElement = new Element("test");
-        testElement.setAttribute("somestring", "expectedString");
-        testElement.setAttribute("someint", "15");
-        Element childElement = new Element("mockpluginchild");
-        childElement.setAttribute("somestring", "childString");
-        childElement.setAttribute("someint", "16");
-        testElement.addContent(childElement);
+	public void testConfigure() throws Exception {
+		Element testElement = new Element("test");
+		testElement.setAttribute("somestring", "expectedString");
+		testElement.setAttribute("someint", "15");
+		Element childElement = new Element("mockpluginchild");
+		childElement.setAttribute("somestring", "childString");
+		childElement.setAttribute("someint", "16");
+		testElement.addContent(childElement);
 
-        PluginXMLHelper helper = new PluginXMLHelper();
-        MockPublisher plugin = null;
-        try {
-            plugin = (MockPublisher) helper.configure(testElement, "net.sourceforge.cruisecontrol.publishers.MockPublisher");
-        } catch (CruiseControlException e) {
-            e.printStackTrace();
-            assertTrue(false);
-        }
-        assertEquals(plugin.getSomeString(), "expectedString");
-        assertEquals(plugin.getSomeInt(), 15);
-        assertEquals(plugin.getMockPluginChild().getSomeString(), "childString");
-        assertEquals(plugin.getMockPluginChild().getSomeInt(), 16);
+		PluginXMLHelper helper = new PluginXMLHelper();
+		MockPublisher plugin = null;
 
-        try {
-            plugin = (MockPublisher) helper.configure(testElement, "noclass");
-            assertTrue(false);
-        } catch(Exception e) {
-            e.printStackTrace();
-            assertEquals("Could not find class: noclass", e.getMessage());
-        }
-    }
+		//(POLICE) if this goes wrong, the method will throw an exception that will be handled
+		// by the JUnit framework. This is the recommended way of doing things like this.
+		// Notice that I added a throws clause to the method signature.
+		plugin = (MockPublisher) helper.configure(testElement, "net.sourceforge.cruisecontrol.publishers.MockPublisher");
+
+		//(POLICE) all asserts in JUnit is on the form
+		// assertXXX("description", expectedValue, actualValue);
+		assertEquals("expectedString", plugin.getSomeString());
+		assertEquals(15, plugin.getSomeInt());
+		assertEquals("childString", plugin.getMockPluginChild().getSomeString());
+		assertEquals(16, plugin.getMockPluginChild().getSomeInt());
+
+		//(POLICE) Slightly rewrote this method. Having assertTrue(false) is pretty
+		// much the same as having a if(true) then return true else return false :-)
+		try {
+			plugin = (MockPublisher) helper.configure(testElement, "noclass");
+			//(POLICE) a real overkill on the nextline would be to check so that noclass really
+			// doesn't exist. But .. hmm, don't think so.
+			fail("Expected an exception because noclass shouldn't exist");
+		} catch (CruiseControlException e) {
+			// this is as what we expect to happen
+		}
+	}
 }
