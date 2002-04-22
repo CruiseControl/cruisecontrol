87e6535ab9f8211d664043e22c06a8dfa8ef3d80
Alden Almagro <alden@users.sourceforge.net>
2002-04-22 22:04:58 +0000

    Tests for the Main class.
    git-svn-id: https://cruisecontrol.svn.sourceforge.net/svnroot/cruisecontrol/trunk/cruisecontrol@481 878de816-9c12-0410-b5fe-b34e1928e20d

---
+++ main/test/net/sourceforge/cruisecontrol/MainTest.java
@@ -0,0 +1,220 @@
+/********************************************************************************
+ * CruiseControl, a Continuous Integration Toolkit
+ * Copyright (c) 2001, ThoughtWorks, Inc.
+ * 651 W Washington Ave. Suite 500
+ * Chicago, IL 60661 USA
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     + Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     + Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *     + Neither the name of ThoughtWorks, Inc., CruiseControl, nor the
+ *       names of its contributors may be used to endorse or promote
+ *       products derived from this software without specific prior
+ *       written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ********************************************************************************/
+package net.sourceforge.cruisecontrol;
+
+import junit.framework.TestCase;
+
+public class MainTest extends TestCase {
+
+    public MainTest(String name) {
+        super(name);
+    }
+
+    public void testConfigureProject() {
+
+        String[] correctArgs = new String[] {"-lastbuild", "20020310120000", "-label", "1.2.2", "-projectname", "myproject", "-configfile", "config.xml"};
+
+        Main main = new Main();
+
+        try {
+            Project project = main.configureProject(correctArgs);
+            assertEquals(project.getConfigFileName(), "config.xml");
+            assertEquals(project.getLabel(), "1.2.2");
+            assertEquals(project.getLastBuild(), "20020310120000");
+            assertEquals(project.getName(), "myproject");
+        } catch (CruiseControlException e) {
+            assertTrue(false);
+        }
+
+        try {
+            Project project = new Project();
+            project.setConfigFileName("config.xml");
+            project.setLabel("1.2.2");
+            project.setLastBuild("20020310120000");
+            project.setName("myproject");
+            project.write();
+
+            Project newProject = main.configureProject(new String[]{"-projectname", "myproject"});
+            assertEquals(newProject.getConfigFileName(), "config.xml");
+            assertEquals(newProject.getLabel(), "1.2.2");
+            assertEquals(newProject.getLastBuild(), "20020310120000");
+            assertEquals(newProject.getName(), "myproject");
+        } catch (CruiseControlException e) {
+            assertTrue(false);
+        }
+
+        try {
+            Project project = new Project();
+            project.setConfigFileName("config.xml");
+            project.setLastBuild("20020310120000");
+            project.setName("myproject");
+            project.write();
+
+            Project newProject = main.configureProject(new String[]{"-projectname", "myproject"});
+            assertTrue(false);
+        } catch (CruiseControlException e) {
+            assertTrue(true);
+        }
+
+    }
+
+    public void testParseLastBuild() {
+        String[] correctArgs = new String[] {"-lastbuild", "20020310120000"};
+        String[] missingArgs = new String[] {""};
+        String[] incorrectArgs = new String[] {"-lastbuild"};
+        Main main = new Main();
+
+        try {
+            assertEquals(main.parseLastBuild(correctArgs, null), "20020310120000");
+        } catch(CruiseControlException e) {
+            assertTrue(false);
+        }
+
+        try {
+            assertEquals(main.parseLastBuild(missingArgs, "20020310000000"), "20020310000000");
+        } catch (CruiseControlException e) {
+            assertTrue(false);
+        }
+
+        try {
+            main.parseLastBuild(incorrectArgs, null);
+            assertTrue(false);
+        } catch(CruiseControlException e) {
+            assertTrue(true);
+        }
+
+        try {
+            main.parseLastBuild(missingArgs, null);
+            assertTrue(false);
+        } catch(CruiseControlException e) {
+            assertTrue(true);
+        }
+    }
+
+    public void testParseLabel() {
+        String[] correctArgs = new String[] {"-label", "1.2.3"};
+        String[] missingArgs = new String[] {""};
+        String[] incorrectArgs = new String[] {"-label"};
+        Main main = new Main();
+
+        try {
+            assertEquals(main.parseLabel(correctArgs, null), "1.2.3");
+        } catch (CruiseControlException e) {
+            assertTrue(false);
+        }
+
+        try {
+            assertEquals(main.parseLabel(missingArgs, "1.2.2"), "1.2.2");
+        } catch (CruiseControlException e) {
+            assertTrue(false);
+        }
+
+        try {
+            main.parseLabel(incorrectArgs, null);
+            assertTrue(false);
+        } catch (CruiseControlException e) {
+            assertTrue(true);
+        }
+
+        try {
+            main.parseLabel(missingArgs, null);
+            assertTrue(false);
+        } catch (CruiseControlException e) {
+            assertTrue(true);
+        }
+    }
+
+    public void testParseConfigurationFileName() {
+        String[] correctArgs = new String[] {"-configfile", "config.xml"};
+        String[] missingArgs = new String[] {""};
+        String[] incorrectArgs = new String[] {"-configfile"};
+        Main main = new Main();
+
+        try {
+            assertEquals(main.parseConfigFileName(correctArgs, null), "config.xml");
+        } catch (CruiseControlException e) {
+            assertTrue(false);
+        }
+
+        try {
+            assertEquals(main.parseConfigFileName(missingArgs, "config.xml"), "config.xml");
+        } catch (CruiseControlException e) {
+            assertTrue(false);
+        }
+
+        try {
+            main.parseConfigFileName(incorrectArgs, null);
+            assertTrue(false);
+        } catch (CruiseControlException e) {
+            assertTrue(true);
+        }
+
+        try {
+            main.parseConfigFileName(missingArgs, null);
+            assertTrue(false);
+        } catch (CruiseControlException e) {
+            assertTrue(true);
+        }
+    }
+
+    public void testParseProjectName() {
+        String[] correctArgs = new String[] {"-projectname", "myproject"};
+        String[] missingArgs = new String[] {""};
+        String[] incorrectArgs = new String[] {"-projectname"};
+        Main main = new Main();
+
+        try {
+            assertEquals(main.parseProjectName(correctArgs), "myproject");
+        } catch (CruiseControlException e) {
+            assertTrue(false);
+        }
+
+        try {
+            main.parseProjectName(missingArgs);
+            assertTrue(false);
+        } catch (CruiseControlException e) {
+            assertTrue(true);
+        }
+
+        try {
+            main.parseProjectName(incorrectArgs);
+            assertTrue(false);
+        } catch (CruiseControlException e) {
+            assertTrue(true);
+        }
+    }
+}
