19e3d99bbdde1b4b99ec397709f88cb8eb169065
Jason Yip <jchyip@users.sourceforge.net>
2002-07-05 20:10:11 +0000

    Upgrade to log4j-1.2.4

--- main/test/net/sourceforge/cruisecontrol/PluginXMLHelperTest.java
+++ main/test/net/sourceforge/cruisecontrol/PluginXMLHelperTest.java
@@ -1,4 +1,4 @@
-/********************************************************************************
+/******************************************************************************
  * CruiseControl, a Continuous Integration Toolkit
  * Copyright (c) 2001, ThoughtWorks, Inc.
  * 651 W Washington Ave. Suite 500
@@ -33,63 +33,60 @@
  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- ********************************************************************************/
+ ******************************************************************************/
 
 package net.sourceforge.cruisecontrol;
 
 import junit.framework.TestCase;
 import net.sourceforge.cruisecontrol.publishers.MockPublisher;
 import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.Category;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.jdom.Element;
 
-//(POLICE) Been here
-
 public class PluginXMLHelperTest extends TestCase {
-	private static Category log = Category.getInstance(PluginXMLHelperTest.class.getName());
+    private static Logger log = Logger.getLogger(PluginXMLHelperTest.class);
+    private PluginXMLHelper helper;
 
-	public PluginXMLHelperTest(String name) {
-		super(name);
-		//(POLICE) This is one way of making _really_ sure that log4j is keeping its mouth shut.
-		// This is probably defined in a log4j.config file somewhere. We usually have it in a
-		// JUnit suite (but CC doesn't have that right now).
-		BasicConfigurator.configure();
-		log.getHierarchy().disableAll();
-	}
+    private final int SOME_INT = 15;
+    private final int SOME_OTHER_INT = 16;
 
-	public void testConfigure() throws Exception {
-		Element testElement = new Element("test");
-		testElement.setAttribute("somestring", "expectedString");
-		testElement.setAttribute("someint", "15");
-		Element childElement = new Element("mockpluginchild");
-		childElement.setAttribute("somestring", "childString");
-		childElement.setAttribute("someint", "16");
-		testElement.addContent(childElement);
+    public PluginXMLHelperTest(String name) {
+        super(name);
 
-		PluginXMLHelper helper = new PluginXMLHelper();
-		MockPublisher plugin = null;
+        // Turn off logging
+        BasicConfigurator.configure();
+        log.getLoggerRepository().setThreshold(Level.OFF);
+    }
 
-		//(POLICE) if this goes wrong, the method will throw an exception that will be handled
-		// by the JUnit framework. This is the recommended way of doing things like this.
-		// Notice that I added a throws clause to the method signature.
-		plugin = (MockPublisher) helper.configure(testElement, "net.sourceforge.cruisecontrol.publishers.MockPublisher");
+    protected void setUp() {
+        helper = new PluginXMLHelper();
+    }
 
-		//(POLICE) all asserts in JUnit is on the form
-		// assertXXX("description", expectedValue, actualValue);
-		assertEquals("expectedString", plugin.getSomeString());
-		assertEquals(15, plugin.getSomeInt());
-		assertEquals("childString", plugin.getMockPluginChild().getSomeString());
-		assertEquals(16, plugin.getMockPluginChild().getSomeInt());
+    public void testConfigure() throws CruiseControlException {
+        Element testElement = new Element("test");
+        testElement.setAttribute("somestring", "expectedString");
+        testElement.setAttribute("someint", Integer.toString(SOME_INT));
+        Element childElement = new Element("mockpluginchild");
+        childElement.setAttribute("somestring", "childString");
+        childElement.setAttribute("someint", Integer.toString(SOME_OTHER_INT));
+        testElement.addContent(childElement);
 
-		//(POLICE) Slightly rewrote this method. Having assertTrue(false) is pretty
-		// much the same as having a if(true) then return true else return false :-)
-		try {
-			plugin = (MockPublisher) helper.configure(testElement, "noclass");
-			//(POLICE) a real overkill on the nextline would be to check so that noclass really
-			// doesn't exist. But .. hmm, don't think so.
-			fail("Expected an exception because noclass shouldn't exist");
-		} catch (CruiseControlException e) {
-			// this is as what we expect to happen
-		}
-	}
+        MockPublisher plugin = (MockPublisher) helper.configure(testElement,
+                "net.sourceforge.cruisecontrol.publishers.MockPublisher");
+
+        assertEquals("expectedString", plugin.getSomeString());
+        assertEquals(SOME_INT, plugin.getSomeInt());
+        assertEquals("childString", plugin.getMockPluginChild().getSomeString());
+        assertEquals(SOME_OTHER_INT, plugin.getMockPluginChild().getSomeInt());
+    }
+
+    public void testConfigureNoClass() {
+        try {
+            helper.configure(new Element("irrelevant"), "noclass");
+            fail("Expected an exception because noclass shouldn't exist");
+        } catch (CruiseControlException expected) {
+        }
+    }
+
 }
