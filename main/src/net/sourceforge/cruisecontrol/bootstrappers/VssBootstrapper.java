b7931edaf90a3d6473e72b2b42a527fa8c39a0a8
Alden Almagro <alden@users.sourceforge.net>
2002-07-11 23:25:14 +0000

    Vss bootstrapping functionality.
    Patch by Jeffrey Fredrick.

---
+++ main/src/net/sourceforge/cruisecontrol/bootstrappers/VssBootstrapper.java
@@ -0,0 +1,94 @@
+package net.sourceforge.cruisecontrol.bootstrappers;
+
+import net.sourceforge.cruisecontrol.Bootstrapper;
+import net.sourceforge.cruisecontrol.CruiseControlException;
+import net.sourceforge.cruisecontrol.util.StreamPumper;
+
+import org.apache.log4j.Logger;
+import java.io.File;
+import java.io.*;
+
+
+public class VssBootstrapper implements Bootstrapper {
+
+  /** enable logging for this class */
+  private static Logger log = Logger.getLogger(VssBootstrapper.class.getName());
+
+  private String vssPath;
+  private String localDirectory;
+  private String login;
+
+  public void bootstrap() throws CruiseControlException {
+    checkAttributes();
+    String commandLine = generateCommandLine();
+
+    try {
+      Process p = Runtime.getRuntime().exec(commandLine);
+      InputStream errorIn = p.getErrorStream();
+      PrintWriter errorOut = new PrintWriter(System.err, true);
+      StreamPumper errorPumper = new StreamPumper(errorIn, errorOut);
+      new Thread(errorPumper).start();
+      p.waitFor();
+    }
+    catch (IOException ex) {
+      log.debug("exception trying to exec ss.exe", ex);
+      throw new CruiseControlException(ex);
+    }
+    catch (InterruptedException ex) {
+      log.debug("interrupted during get", ex);
+      throw new CruiseControlException(ex);
+    }
+  }
+
+  void checkAttributes() throws CruiseControlException {
+    if (vssPath == null || localDirectory == null) throw new CruiseControlException("VssBootstrapper has required attributes vssPath and filePath");
+    File localDirForFile = new File(localDirectory);
+    boolean dirExists = localDirForFile.exists();
+    if (!dirExists) {
+      log.debug("local directory [" + localDirectory + "] does not exist");
+      throw new CruiseControlException("file path attribute value " + localDirectory + " must specify an existing directory.");
+    }
+    boolean isDir = localDirForFile.isDirectory();
+    if (!isDir) {
+      log.debug("local directory [" + localDirectory + "] is not a directory");
+      throw new CruiseControlException("file path attribute value " + localDirectory + " must specify an existing directory, not a file.");
+    }
+    setLocalDirectory(localDirForFile.getAbsolutePath());
+  }
+
+  String generateCommandLine() {
+    StringBuffer commandLine = new StringBuffer();
+    final String QUOTE = "\"";
+    commandLine.append("ss.exe get ");
+    commandLine.append(QUOTE+this.vssPath+QUOTE);
+    commandLine.append(" -GL");
+    commandLine.append(QUOTE + this.localDirectory + QUOTE);
+    commandLine.append(" -I-N");
+    if (login != null) commandLine.append(" -Y" + login);
+    return commandLine.toString();
+  }
+
+  /**
+   * Required.
+   * @param vssPath fully qualified VSS path to the file ($/Project/subproject/filename.ext)
+   */
+  public void setVssPath(String vssPath) {
+    this.vssPath = vssPath;
+  }
+
+  /**
+   * Required.
+   * @param localDirectory fully qualified path for the destination directory (c:\directory\subdirectory\)
+   */
+  public void setLocalDirectory(String localDirectory) {
+    this.localDirectory = localDirectory;
+  }
+
+  /**
+   * Optional.
+   * @param login vss login information in the form username,password\
+   */
+  public void setLogin(String login) {
+    this.login = login;
+  }
+}
---
+++ main/test/net/sourceforge/cruisecontrol/bootstrappers/VssBootstrapperTest.java
@@ -0,0 +1,67 @@
+package net.sourceforge.cruisecontrol.bootstrappers;
+
+import junit.framework.TestCase;
+import net.sourceforge.cruisecontrol.*;
+
+public class VssBootstrapperTest extends TestCase {
+
+  public VssBootstrapperTest(String name) { super(name); }
+
+  public void testCheckAttributes() {
+    VssBootstrapper bootstrapper = new VssBootstrapper();
+    try {
+      bootstrapper.checkAttributes();
+      fail("VssBootstrapper should throw exception if required attributes not set");
+    }
+    catch (CruiseControlException ex) {
+      String message = ex.getMessage();
+      assertEquals("exception message when required attributes not set", "VssBootstrapper has required attributes vssPath and filePath", message);
+    }
+
+    bootstrapper.setVssPath("$test/vss/path/file.ext");
+    try {
+      bootstrapper.checkAttributes();
+      fail("VssBootstrapper should throw exception if required attributes not set");
+    }
+    catch (CruiseControlException ex) {
+      String message = ex.getMessage();
+      assertEquals("exception message when required attributes not set", "VssBootstrapper has required attributes vssPath and filePath", message);
+    }
+
+    bootstrapper.setLocalDirectory(".");
+    try {
+      bootstrapper.checkAttributes();
+    }
+    catch (CruiseControlException ex) {
+      fail("checkAttributes() shouldn't fail when required attributes have been set");
+    }
+
+    bootstrapper.setLocalDirectory("c:/not/an/existing/directory");
+    try {
+      bootstrapper.checkAttributes();
+      fail("checkAttributes() should fail when given a file path that doesn't exist");
+    }
+    catch (CruiseControlException ex) {
+    }
+  }
+
+  public void testCommandLine() {
+    VssBootstrapper bootstrapper = new VssBootstrapper();
+    String commandLine = bootstrapper.generateCommandLine();
+    this.assertNotNull("command line should never be null", commandLine);
+
+    final String vssPath = "$Project/subproject/file.ext";
+    bootstrapper.setVssPath(vssPath);
+    final String localDirectory = "c:/foo";
+    bootstrapper.setLocalDirectory(localDirectory);
+
+    commandLine = bootstrapper.generateCommandLine();
+    String expectedCommandLine = "ss.exe get \"" + vssPath + "\" -GL\"" + localDirectory + "\" -I-N";
+    this.assertEquals(expectedCommandLine, commandLine);
+
+    bootstrapper.setLogin("bob,password");
+    commandLine = bootstrapper.generateCommandLine();
+    expectedCommandLine = expectedCommandLine + " -Ybob,password";
+    this.assertEquals(expectedCommandLine, commandLine);
+  }
+}
