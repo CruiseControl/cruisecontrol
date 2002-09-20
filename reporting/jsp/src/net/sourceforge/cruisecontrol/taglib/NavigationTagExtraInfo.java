7d1af08d463256fce8a2e2b06e5aa8d284b51b0e
Alden Almagro <alden@users.sourceforge.net>
2002-09-20 16:26:15 +0000

    Fixed bug when running webapp in Orion.
    Patch contributed by Mike Roberts.
    
    
    git-svn-id: https://cruisecontrol.svn.sourceforge.net/svnroot/cruisecontrol/trunk/cruisecontrol@761 878de816-9c12-0410-b5fe-b34e1928e20d

--- reporting/jsp/src/net/sourceforge/cruisecontrol/taglib/NavigationTagExtraInfo.java
+++ reporting/jsp/src/net/sourceforge/cruisecontrol/taglib/NavigationTagExtraInfo.java
@@ -43,11 +43,11 @@ public class NavigationTagExtraInfo extends TagExtraInfo {
     public VariableInfo[] getVariableInfo(TagData data) {
         return new VariableInfo[] {
             new VariableInfo("url",
-                             "String",
+                             "java.lang.String",
                              true,
                              VariableInfo.NESTED),
             new VariableInfo("linktext",
-                             "String",
+                             "java.lang.String",
                              true,
                              VariableInfo.NESTED)
         };
