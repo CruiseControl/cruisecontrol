d3053fb310d5ae6fc10e991dc269dbba6aba6126
Jeffrey Fredrick <jfredrick@users.sourceforge.net>
2003-02-10 05:18:04 +0000

    extracted some utility methods dealing with conversions between Date, int time ("HHmm"), and long time (milliseconds)

---
+++ main/src/net/sourceforge/cruisecontrol/util/Util.java
@@ -0,0 +1,78 @@
+/******************************************************************************
+ * CruiseControl, a Continuous Integration Toolkit
+ * Copyright (c) 2001, ThoughtWorks, Inc.
+ * 651 W Washington Ave. Suite 500
+ * Chicago, IL 60661 USA
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     + Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     + Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *     + Neither the name of ThoughtWorks, Inc., CruiseControl, nor the
+ *       names of its contributors may be used to endorse or promote
+ *       products derived from this software without specific prior
+ *       written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
+package net.sourceforge.cruisecontrol.util;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class Util {
+
+	/**
+	 *  Create an integer time from a <code>Date</code> object.
+	 *
+	 *  @param date The date to get the timestamp from.
+	 *  @return The time as an integer formatted as "HHmm".
+	 */
+	public static int getTimeFromDate(Date date) {
+	    Calendar calendar = Calendar.getInstance();
+	    calendar.setTime(date);
+	    int hour = calendar.get(Calendar.HOUR_OF_DAY) * 100;
+	    int minute = calendar.get(Calendar.MINUTE);
+	    return hour + minute;
+	}
+
+	/**
+	 * finds the difference in milliseconds between two integer time
+	 * values of the format "HHmm".
+	 * @param a integer time value of format "HHmm"
+	 * @param b integer time value of format "HHmm"
+	 * @return long millisecond time difference
+	 */
+	public static long milliTimeDiffernce(int a, int b) {
+		long aMillis = convertToMillis(a);
+		long bMillis = convertToMillis(b);
+		return bMillis-aMillis;
+	}
+
+	public static long convertToMillis(int HHmm) {
+		int minutes = HHmm % 100;
+		int hours = (HHmm - minutes)/100;
+		long milliseconds = (hours*60 + minutes) * 60 * 1000;
+		return milliseconds;
+	}
+
+}
