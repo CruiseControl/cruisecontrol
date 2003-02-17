05f4a1c5e7170477592986a3f4ed32a19aeb1b18
Jeffrey Fredrick <jfredrick@users.sourceforge.net>
2003-02-17 23:20:08 +0000

    No functional changes:   deleted unused local variable   ran Eclipse pretty printer over file
    git-svn-id: https://cruisecontrol.svn.sourceforge.net/svnroot/cruisecontrol/trunk/cruisecontrol@864 878de816-9c12-0410-b5fe-b34e1928e20d

--- main/test/net/sourceforge/cruisecontrol/MainTest.java
+++ main/test/net/sourceforge/cruisecontrol/MainTest.java
@@ -54,10 +54,16 @@ public class MainTest extends TestCase {
 
     public void testConfigureProject() throws Exception {
 
-        String[] correctArgs = new String[]{"-lastbuild", "20020310120000",
-                                            "-label", "1.2.2", "-projectname",
-                                            "myproject", "-configfile",
-                                            "config.xml"};
+        String[] correctArgs =
+            new String[] {
+                "-lastbuild",
+                "20020310120000",
+                "-label",
+                "1.2.2",
+                "-projectname",
+                "myproject",
+                "-configfile",
+                "config.xml" };
 
         File myProjFile = new File("myproject");
         if (myProjFile.exists()) {
@@ -80,13 +86,14 @@ public class MainTest extends TestCase {
             project.setName("myproject");
             project.serializeProject();
 
-            Project newProject = main.configureProject(
-                    new String[]{"-projectname", "myproject"});
+            Project newProject =
+                main.configureProject(
+                    new String[] { "-projectname", "myproject" });
             assertEquals(newProject.getConfigFileName(), "config.xml");
             assertEquals(newProject.getLabel(), "1.2.2");
             assertEquals(newProject.getLastBuild(), "20020310120000");
             assertEquals(newProject.getName(), "myproject");
-            
+
             try {
                 newProject.setPaused(false);
             }
@@ -102,150 +109,160 @@ public class MainTest extends TestCase {
             project.setName("myproject");
             project.serializeProject();
 
-            Project newProject = main.configureProject(
-                    new String[]{"-projectname", "myproject"});
+            main.configureProject(new String[] { "-projectname", "myproject" });
             fail("Expected exception");
-        } catch (CruiseControlException e) {
+        }
+        catch (CruiseControlException e) {
             // expected
         }
 
     }
 
     public void testParseLastBuild() throws Exception {
-        String[] correctArgs = new String[]{"-lastbuild", "20020310120000"};
-        String[] missingArgs = new String[]{""};
-        String[] incorrectArgs = new String[]{"-lastbuild"};
+        String[] correctArgs = new String[] { "-lastbuild", "20020310120000" };
+        String[] missingArgs = new String[] { "" };
+        String[] incorrectArgs = new String[] { "-lastbuild" };
 
         assertEquals(main.parseLastBuild(correctArgs, null), "20020310120000");
 
-        assertEquals(main.parseLastBuild(missingArgs, "20020310000000"),
-                "20020310000000");
+        assertEquals(
+            main.parseLastBuild(missingArgs, "20020310000000"),
+            "20020310000000");
 
         try {
             main.parseLastBuild(incorrectArgs, null);
             fail("Expected exception");
-        } catch (CruiseControlException e) {
+        }
+        catch (CruiseControlException e) {
             // expected
         }
 
         try {
             main.parseLastBuild(missingArgs, null);
             fail("Expected exception");
-        } catch (CruiseControlException e) {
+        }
+        catch (CruiseControlException e) {
             // expected
         }
     }
 
     public void testParseLabelCorrect() throws CruiseControlException {
         String correctLabel = "1.2.3";
-        String[] correctArgs = new String[]{"-label", correctLabel};
+        String[] correctArgs = new String[] { "-label", correctLabel };
 
-        assertEquals(
-                main.parseLabel(correctArgs, null),
-                correctLabel);
+        assertEquals(main.parseLabel(correctArgs, null), correctLabel);
     }
 
     public void testParseLabelNoArgs() throws CruiseControlException {
-        String[] noArgs = new String[]{""};
+        String[] noArgs = new String[] { "" };
         String previousLabel = "1.2.2";
 
-        assertEquals(main.parseLabel(noArgs, previousLabel),
-                previousLabel);
+        assertEquals(main.parseLabel(noArgs, previousLabel), previousLabel);
     }
 
     public void testParseLabelMissingLabelValue() {
-        String[] incorrectArgs = new String[]{"-label"};
+        String[] incorrectArgs = new String[] { "-label" };
 
         try {
             main.parseLabel(incorrectArgs, null);
             fail("Expected exception due to missing label value");
-        } catch (CruiseControlException expected) {
+        }
+        catch (CruiseControlException expected) {
         }
     }
 
     public void testParseLabelNoArgsNoPreviousLabel() {
-        String[] noArgs = new String[]{""};
+        String[] noArgs = new String[] { "" };
 
         try {
             main.parseLabel(noArgs, null);
             fail("Expected exception due to label not being set");
-        } catch (CruiseControlException expected) {
+        }
+        catch (CruiseControlException expected) {
         }
     }
 
     public void testParseConfigurationFileName() throws Exception {
-        String[] correctArgs = new String[]{"-configfile", "config.xml"};
-        String[] missingArgs = new String[]{""};
-        String[] incorrectArgs = new String[]{"-configfile"};
+        String[] correctArgs = new String[] { "-configfile", "config.xml" };
+        String[] missingArgs = new String[] { "" };
+        String[] incorrectArgs = new String[] { "-configfile" };
 
         assertEquals(main.parseConfigFileName(correctArgs, null), "config.xml");
 
-        assertEquals(main.parseConfigFileName(missingArgs, "config.xml"),
-                "config.xml");
+        assertEquals(
+            main.parseConfigFileName(missingArgs, "config.xml"),
+            "config.xml");
 
         try {
             main.parseConfigFileName(incorrectArgs, null);
             fail("Expected exception");
-        } catch (CruiseControlException e) {
+        }
+        catch (CruiseControlException e) {
             // expected
         }
 
         try {
             main.parseConfigFileName(missingArgs, null);
             fail("Expected exception");
-        } catch (CruiseControlException e) {
+        }
+        catch (CruiseControlException e) {
             // expected
         }
     }
 
     public void testParseProjectName() throws Exception {
-        String[] correctArgs = new String[]{"-projectname", "myproject"};
-        String[] missingArgs = new String[]{""};
-        String[] incorrectArgs = new String[]{"-projectname"};
+        String[] correctArgs = new String[] { "-projectname", "myproject" };
+        String[] missingArgs = new String[] { "" };
+        String[] incorrectArgs = new String[] { "-projectname" };
 
         assertEquals(main.parseProjectName(correctArgs), "myproject");
 
         try {
             main.parseProjectName(missingArgs);
             fail("Expected exception");
-        } catch (CruiseControlException e) {
+        }
+        catch (CruiseControlException e) {
             // expected
         }
 
         try {
             main.parseProjectName(incorrectArgs);
             fail("Expected exception");
-        } catch (CruiseControlException e) {
+        }
+        catch (CruiseControlException e) {
             // expected
         }
     }
 
     public void testParsePort() throws Exception {
-        String[] correctArgs = new String[]{"-port", "123"};
-        String[] missingArgs = new String[]{""};
-        String[] incorrectArgs = new String[]{"-port"};
-        String[] invalidArgs = new String[]{"-port", "ABC"};
+        String[] correctArgs = new String[] { "-port", "123" };
+        String[] missingArgs = new String[] { "" };
+        String[] incorrectArgs = new String[] { "-port" };
+        String[] invalidArgs = new String[] { "-port", "ABC" };
 
         assertEquals(Main.parsePort(correctArgs), 123);
 
         try {
             Main.parsePort(missingArgs);
             fail("Expected exception");
-        } catch (IllegalStateException e) {
+        }
+        catch (IllegalStateException e) {
             // expected
         }
 
         try {
             Main.parsePort(incorrectArgs);
             fail("Expected exception");
-        } catch (CruiseControlException e) {
+        }
+        catch (CruiseControlException e) {
             // expected
         }
 
         try {
             Main.parsePort(invalidArgs);
             fail("Expected exception");
-        } catch (IllegalArgumentException e) {
+        }
+        catch (IllegalArgumentException e) {
             // expected
         }
     }
@@ -255,34 +272,39 @@ public class MainTest extends TestCase {
         String defaultValue = "8080";
 
         //No args specified. Should get the default back.
-        String[] args = {};
+        String[] args = {
+        };
         String foundValue = Main.parseArgument(args, argName, defaultValue);
         assertEquals(defaultValue, foundValue);
 
         //One arg specified, should get the value specified, not the default.
         String setValue = "100";
-        args = new String[]{"-port", setValue};
+        args = new String[] { "-port", setValue };
         foundValue = Main.parseArgument(args, argName, defaultValue);
         assertEquals(setValue, foundValue);
 
         //More than one arg specified, should still get the value specified.
-        args = new String[]{"-port", setValue, "-throwAway", "value"};
+        args = new String[] { "-port", setValue, "-throwAway", "value" };
         foundValue = Main.parseArgument(args, argName, defaultValue);
         assertEquals(setValue, foundValue);
 
         //Switch the order around, should still get the value specified.
-        args = new String[]{"-throwAway", "value", "-port", setValue};
+        args = new String[] { "-throwAway", "value", "-port", setValue };
         foundValue = Main.parseArgument(args, argName, defaultValue);
         assertEquals(setValue, foundValue);
 
         //If arg name is included, but no arg, then should get an exception.
-        args = new String[]{"-port"};
+        args = new String[] { "-port" };
         try {
             foundValue = Main.parseArgument(args, argName, defaultValue);
-            fail("Expected to get an exception, because the user specified" +
-                    " an argument but didn't provide a value for the argument." +
-                    " Got the value '" + foundValue + "' instead.");
-        } catch (CruiseControlException e) {
+            fail(
+                "Expected to get an exception, because the user specified"
+                    + " an argument but didn't provide a value for the argument."
+                    + " Got the value '"
+                    + foundValue
+                    + "' instead.");
+        }
+        catch (CruiseControlException e) {
             assertTrue("Good, expected to get an exception.", true);
         }
     }
