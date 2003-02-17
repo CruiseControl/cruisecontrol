418617f962f4fd86ee8faa9efffc22a7fc37d7e2
Jeffrey Fredrick <jfredrick@users.sourceforge.net>
2003-02-17 23:41:24 +0000

    No functional changes:   removed unused imports   fixed issue where all lines were double spaced
    git-svn-id: https://cruisecontrol.svn.sourceforge.net/svnroot/cruisecontrol/trunk/cruisecontrol@873 878de816-9c12-0410-b5fe-b34e1928e20d

--- main/src/net/sourceforge/cruisecontrol/publishers/SCPPublisher.java
+++ main/src/net/sourceforge/cruisecontrol/publishers/SCPPublisher.java
@@ -5,10 +5,12 @@ import net.sourceforge.cruisecontrol.Publisher;
 import net.sourceforge.cruisecontrol.util.XMLLogHelper;
 import net.sourceforge.cruisecontrol.util.Commandline;
 import net.sourceforge.cruisecontrol.util.Commandline.Argument;
-import org.jdom.DataConversionException;
+
 import org.jdom.Element;
+
 import java.io.IOException;
 import java.io.File;
+
 import org.apache.log4j.Logger;
 
 /**
@@ -16,10 +18,12 @@ import org.apache.log4j.Logger;
  *
  * @author <a href="orenmnero@sourceforge.net">Oren Miller</a>
  */
+
 public class SCPPublisher implements Publisher {
+
     /** enable logging for this class */
-    private static Logger log = Logger.getLogger(
-                                    SCPPublisher.class);
+
+    private static Logger log = Logger.getLogger(SCPPublisher.class);
 
     private String _sourceuser = null;
     private String _sourcehost = null;
@@ -35,30 +39,39 @@ public class SCPPublisher implements Publisher {
     public void setSourceUser(String sourceuser) {
         _sourceuser = sourceuser;
     }
+
     public void setSourceHost(String sourcehost) {
         _sourcehost = sourcehost;
     }
+
     public void setSourceDir(String sourcedir) {
         _sourcedir = sourcedir;
     }
+
     public void setTargetUser(String targetuser) {
         _targetuser = targetuser;
     }
+
     public void setTargetHost(String targethost) {
         _targethost = targethost;
     }
+
     public void setTargetDir(String targetdir) {
         _targetdir = targetdir;
     }
+
     public void setSSH(String ssh) {
         _ssh = ssh;
     }
+
     public void setOptions(String options) {
         _options = options;
     }
+
     public void setFile(String file) {
         _file = file;
     }
+
     public void setTargetSeparator(String targetseparator) {
         _targetseparator = targetseparator;
     }
@@ -70,30 +83,35 @@ public class SCPPublisher implements Publisher {
      *  @throws CruiseControlException if there was a configuration error.
      */
     public void validate() throws CruiseControlException {
-        if(_sourceuser == null) {
-            if(_sourcehost != null) {
+        if (_sourceuser == null) {
+            if (_sourcehost != null) {
                 throw new CruiseControlException("'sourceuser' not specified in configuration file");
             }
         }
-        if(_sourcehost == null) {
-            if(_sourceuser != null) {
+
+        if (_sourcehost == null) {
+            if (_sourceuser != null) {
                 throw new CruiseControlException("'sourcehost' not specified in configuration file");
             }
         }
-        if(_targetuser == null) {
-            if(_targethost != null) {
+
+        if (_targetuser == null) {
+            if (_targethost != null) {
                 throw new CruiseControlException("'targetuser' not specified in configuration file");
             }
         }
-        if(_targethost == null) {
-            if(_targetuser != null) {
+
+        if (_targethost == null) {
+            if (_targetuser != null) {
                 throw new CruiseControlException("'targethost' not specified in configuration file");
             }
         }
     }
 
-    public void publish(Element cruisecontrolLog) throws CruiseControlException {
-        if(_file == null) {
+    public void publish(Element cruisecontrolLog)
+        throws CruiseControlException {
+
+        if (_file == null) {
             XMLLogHelper helper = new XMLLogHelper(cruisecontrolLog);
             _file = helper.getLogFileName().substring(1);
         }
@@ -101,8 +119,9 @@ public class SCPPublisher implements Publisher {
         Commandline command = createCommandline(_file);
         log.info("executing command: " + command);
         try {
-            Process p = Runtime.getRuntime().exec(command.getCommandline());
-        } catch( IOException e ) {
+            Runtime.getRuntime().exec(command.getCommandline());
+        }
+        catch (IOException e) {
             throw new CruiseControlException(e);
         }
     }
@@ -116,14 +135,35 @@ public class SCPPublisher implements Publisher {
         command.createArgument().setLine(_options);
         command.createArgument().setValue("-S");
         command.createArgument().setValue(_ssh);
-        createFileArgument(command.createArgument(),_sourceuser, _sourcehost, _sourcedir, sourcefile);
-        createFileArgument(command.createArgument(),_targetuser, _targethost, _targetdir, targetfile);
+
+        createFileArgument(
+            command.createArgument(),
+            _sourceuser,
+            _sourcehost,
+            _sourcedir,
+            sourcefile);
+
+        createFileArgument(
+            command.createArgument(),
+            _targetuser,
+            _targethost,
+            _targetdir,
+            targetfile);
+
         return command;
+
     }
 
-    public void createFileArgument(Argument arg, String user, String host, String dir, String file) {
+    public void createFileArgument(
+        Argument arg,
+        String user,
+        String host,
+        String dir,
+        String file) {
+
         String argValue = new String();
-        if(user != null && host != null) {
+
+        if (user != null && host != null) {
             argValue = user + "@" + host + ":";
         }
 
@@ -131,4 +171,5 @@ public class SCPPublisher implements Publisher {
         argValue += file;
         arg.setValue(argValue);
     }
+
 }
