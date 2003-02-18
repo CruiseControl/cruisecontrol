8d08bb5d3a6fa28213954cc6ae5aa058e8d95c01
Jeffrey Fredrick <jfredrick@users.sourceforge.net>
2003-02-18 03:10:20 +0000

    *** empty log message ***
    git-svn-id: https://cruisecontrol.svn.sourceforge.net/svnroot/cruisecontrol/trunk/cruisecontrol@876 878de816-9c12-0410-b5fe-b34e1928e20d

---
+++ main/test/net/sourceforge/cruisecontrol/MockProject.java
@@ -0,0 +1,99 @@
+/******************************************************************************
+ * CruiseControl, a Continuous Integration Toolkit
+ * Copyright (c) 2001, ThoughtWorks, Inc.
+ * 651 W Washington Ave. Suite 500
+ * Chicago, IL 60661 USA
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     + Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     + Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *     + Neither the name of ThoughtWorks, Inc., CruiseControl, nor the
+ *       names of its contributors may be used to endorse or promote
+ *       products derived from this software without specific prior
+ *       written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
+package net.sourceforge.cruisecontrol;
+
+import java.util.Date;
+
+/**
+ * @author jfredrick
+ */
+public class MockProject extends Project {
+
+    private int buildCount = 0;
+    private Date lastBuild;
+    private static final long ONE_SECOND = 1000;
+
+    public void execute() {
+        buildCount++;
+        lastBuild = new Date();
+        try {
+            Thread.sleep(ONE_SECOND);
+        }
+        catch (InterruptedException e) {
+            throw new RuntimeException(e);            
+        }
+    }
+    
+    public int getBuildCount() {
+        return buildCount;
+    }
+    
+    public Date getLastBuildDate() {
+        return lastBuild;
+    }
+
+
+
+    boolean keepLooping = false;
+    int loopCount = 0;
+    void loop() {
+        loopCount = 0;
+        keepLooping = true;
+        while(keepLooping) {
+            loopCount++;
+            try {
+                checkWait();
+                Thread.sleep(50);
+            }
+            catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    
+    void checkWait()  throws InterruptedException {
+    }
+    
+    void stopLooping() {
+        keepLooping = false;
+    }
+    
+    int getLoopCount() {
+        return loopCount;
+    }
+
+}
