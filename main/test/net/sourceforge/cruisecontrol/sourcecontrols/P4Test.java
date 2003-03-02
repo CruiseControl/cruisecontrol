1052ffb4c1d36df2dab0e37443d28df4f6dbbb1e
Jeffrey Fredrick <jfredrick@users.sourceforge.net>
2003-03-02 16:18:49 +0000

    Fixed test - expected values missing some spaces. reformated.

--- main/test/net/sourceforge/cruisecontrol/sourcecontrols/P4Test.java
+++ main/test/net/sourceforge/cruisecontrol/sourcecontrols/P4Test.java
@@ -47,7 +47,7 @@ import junit.framework.TestCase;
 import net.sourceforge.cruisecontrol.CruiseControlException;
 
 import org.jdom.Element;
-import org.jdom.output.XMLOutputter;
+// import org.jdom.output.XMLOutputter;
 
 /**
  *@author  Robert Watkins
@@ -79,35 +79,48 @@ public class P4Test extends TestCase {
     }
 
     public void testParseChangelists() throws IOException, ParseException {
-        File testLog = new File("test/net/sourceforge/cruisecontrol/sourcecontrols/p4_changes.txt");
-        BufferedInputStream input = new BufferedInputStream(new FileInputStream(testLog));
+        File testLog =
+            new File("test/net/sourceforge/cruisecontrol/sourcecontrols/p4_changes.txt");
+        BufferedInputStream input =
+            new BufferedInputStream(new FileInputStream(testLog));
 
         P4 p4 = new P4();
         String[] changelists = p4.parseChangelistNumbers(input);
         input.close();
         assertNotNull("No changelists returned", changelists);
-        assertEquals("Returned wrong number of changelists", 4, changelists.length);
-        String[] expectedChangelists = new String[]{"14", "12", "11"};
+        assertEquals(
+            "Returned wrong number of changelists",
+            4,
+            changelists.length);
+        String[] expectedChangelists = new String[] { "14", "12", "11" };
         for (int i = 0; i < expectedChangelists.length; i++) {
-            assertEquals("Returned wrong changelist number", expectedChangelists[i], changelists[i]);
+            assertEquals(
+                "Returned wrong changelist number",
+                expectedChangelists[i],
+                changelists[i]);
         }
     }
 
-    public void testParseChangeDescriptions() throws IOException, ParseException {
-        File testLog = new File("test/net/sourceforge/cruisecontrol/sourcecontrols/p4_describe.txt");
-        BufferedInputStream input = new BufferedInputStream(new FileInputStream(testLog));
+    public void testParseChangeDescriptions()
+        throws IOException, ParseException {
+        File testLog =
+            new File("test/net/sourceforge/cruisecontrol/sourcecontrols/p4_describe.txt");
+        BufferedInputStream input =
+            new BufferedInputStream(new FileInputStream(testLog));
 
         P4 p4 = new P4();
         List changelists = p4.parseChangeDescriptions(input);
         input.close();
-        assertEquals("Returned wrong number of changelists", 3, changelists.size());
-        XMLOutputter outputter = new XMLOutputter();
+        assertEquals(
+            "Returned wrong number of changelists",
+            3,
+            changelists.size());
         List changelistElements = p4.changeListsToElement(changelists);
         assertEquals(
             "Wrong description",
             "Fixed support for db2. This is now the default database shipped"
-                + "with HPDoc. For now that is. Still has to be tested on"
-                + "PostgreSQL to see that it is still working there. The search"
+                + " with HPDoc. For now that is. Still has to be tested on"
+                + " PostgreSQL to see that it is still working there. The sea rch"
                 + " mechanism is also upgraded to now ALMOST support AND/OR"
                 + " expressions. There are thoughtsabout this, but not yet implemented (however prepared for)",
             ((Element) changelistElements.get(0))
@@ -116,7 +129,7 @@ public class P4Test extends TestCase {
         assertEquals(
             "Wrong description",
             "ok, tests running smooth. Checking in mostly for backup. Not"
-                + "finished yet. CIMD is comming on great and I'm starting to see a framework developing.",
+                + " finished yet. CIMD is comming on great and I'm starting to see a framework developing.",
             ((Element) changelistElements.get(1))
                 .getChild("description")
                 .getText());
@@ -126,25 +139,26 @@ public class P4Test extends TestCase {
             ((Element) changelistElements.get(2))
                 .getChild("description")
                 .getText());
-//        for (Iterator iterator = changelistElements.iterator(); iterator.hasNext();) {
-//            Element element = (Element) iterator.next();
-//  Use next lines if you want to see the output of the run. This is what is inserted into the logs.
-//            outputter.setNewlines(true);
-//            outputter.setIndent(true);
-//            System.out.println(outputter.outputString(element));
-//        }
+        //        XMLOutputter outputter = new XMLOutputter();
+        //        for (Iterator iterator = changelistElements.iterator(); iterator.hasNext();) {
+        //            Element element = (Element) iterator.next();
+        //  Use next lines if you want to see the output of the run. This is what is inserted into the logs.
+        //            outputter.setNewlines(true);
+        //            outputter.setIndent(true);
+        //            System.out.println(outputter.outputString(element));
+        //        }
     }
 
-//    public void testGetModifications() throws Exception {
-//        
-//        // REAL TEST IF YOU NEED IT
-//        P4 p4 = new P4();
-//        p4.setView("//depot/...");
-//        List changelists = p4.getModifications(new Date(0), new Date(), 0);
-//        assertEquals("Returned wrong number of changelists", 3, changelists.size());
-//        
-//    }
-        
+    //    public void testGetModifications() throws Exception {
+    //        
+    //        // REAL TEST IF YOU NEED IT
+    //        P4 p4 = new P4();
+    //        p4.setView("//depot/...");
+    //        List changelists = p4.getModifications(new Date(0), new Date(), 0);
+    //        assertEquals("Returned wrong number of changelists", 3, changelists.size());
+    //        
+    //    }
+
     public static void main(String[] args) {
         junit.textui.TestRunner.run(P4Test.class);
     }
