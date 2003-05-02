ff33cd5f133347c322571e45260474c2ad4ac977
Robert Watkins <robertdw@users.sourceforge.net>
2003-05-02 02:12:14 +0000

    Added multi-project JMX
    git-svn-id: https://cruisecontrol.svn.sourceforge.net/svnroot/cruisecontrol/trunk/cruisecontrol@1011 878de816-9c12-0410-b5fe-b34e1928e20d

---
+++ main/src/net/sourceforge/cruisecontrol/CruiseControlController.java
@@ -0,0 +1,189 @@
+/********************************************************************************
+ * CruiseControl, a Continuous Integration Toolkit
+ * Copyright (c) 2001-2003, ThoughtWorks, Inc.
+ * 651 W Washington Ave. Suite 500
+ * Chicago, IL 60661 USA
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     + Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     + Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *     + Neither the name of ThoughtWorks, Inc., CruiseControl, nor the
+ *       names of its contributors may be used to endorse or promote
+ *       products derived from this software without specific prior
+ *       written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ********************************************************************************/
+package net.sourceforge.cruisecontrol;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.EventListener;
+import java.util.Iterator;
+import java.util.List;
+
+import net.sourceforge.cruisecontrol.util.Util;
+import org.apache.log4j.Logger;
+import org.jdom.Element;
+
+/**
+ *
+ * @author <a href="mailto:robertdw@users.sourceforge.net">Robert Watkins</a>
+ */
+public class CruiseControlController {
+    private static final Logger LOG = Logger.getLogger(CruiseControlController.class);
+    public static final String DEFAULT_CONFIG_FILE_NAME = "config.xml";
+    private File configFile;
+    private List projects = new ArrayList();
+    private BuildQueue buildQueue = new BuildQueue();
+
+    private List listeners = new ArrayList();
+
+    public File getConfigFile() {
+        return configFile;
+    }
+
+    public void setConfigFile(File configFile) throws CruiseControlException {
+        this.configFile = configFile;
+        if (configFile == null || !configFile.exists()) {
+            throw new CruiseControlException("No config file");
+        }
+        parseConfigFile();
+    }
+
+    private void parseConfigFile() throws CruiseControlException {
+        List projectList = getAllProjects();
+        for (Iterator iterator = projectList.iterator(); iterator.hasNext();) {
+            Project project = (Project) iterator.next();
+            addProject(project);
+        }
+    }
+
+    private void addProject(Project project) {
+        projects.add(project);
+        for (Iterator listenIter = listeners.iterator(); listenIter.hasNext();) {
+            LOG.debug("Informing listener of new project");
+            Listener listener = (Listener) listenIter.next();
+            listener.projectAdded(project);
+        }
+    }
+
+    public void start() {
+        buildQueue.start();
+        for (Iterator iterator = projects.iterator(); iterator.hasNext();) {
+            Project currentProject = (Project) iterator.next();
+            currentProject.setBuildQueue(buildQueue);
+            currentProject.start();
+        }
+    }
+
+    public void stop() {
+        buildQueue.stop();
+        for (Iterator iterator = projects.iterator(); iterator.hasNext(); ) {
+            Project currentProject = (Project) iterator.next();
+            currentProject.stop();
+        }
+    }
+
+    public List getProjects() {
+        return Collections.unmodifiableList(projects);
+    }
+
+    List getAllProjects() throws CruiseControlException {
+        Element configRoot = Util.loadConfigFile(configFile);
+        String[] projectNames = getProjectNames(configRoot);
+        ArrayList allProjects = new ArrayList(projectNames.length);
+        for (int i = 0; i < projectNames.length; i++) {
+            String projectName = projectNames[i];
+            LOG.info("projectName = [" + projectName + "]");
+            Project project = configureProject(projectName);
+            project.init();
+            allProjects.add(project);
+        }
+        return allProjects;
+    }
+
+    Project configureProject(String projectName)
+        throws CruiseControlException {
+        Project project = readProject(projectName);
+        project.setName(projectName);
+        project.setConfigFile(configFile);
+        return project;
+    }
+
+    /**
+     * Reads project configuration from a previously serialized Project.  The
+     * name of the serialized project file is equivalent to the name of the
+     * project.
+     *
+     * @param fileName name of the serialized project file
+     * @return Deserialized Project or a new Project if there are any problems
+     * reading the serialized Project; should never return null
+     */
+    private Project readProject(String fileName) {
+        File serializedProjectFile = new File(fileName);
+        LOG.debug("Reading serialized project from: " + serializedProjectFile.getAbsolutePath());
+        if (!serializedProjectFile.exists() || !serializedProjectFile.canRead()) {
+            LOG.warn("No previously serialized project found: " + serializedProjectFile.getAbsolutePath());
+            return new Project();
+        } else {
+            try {
+                ObjectInputStream s = new ObjectInputStream(new FileInputStream(serializedProjectFile));
+                Project project = (Project) s.readObject();
+                return project;
+            } catch (Exception e) {
+                LOG.warn("Error deserializing project file from " + serializedProjectFile.getAbsolutePath(), e);
+                return new Project();
+            }
+        }
+    }
+
+    String[] getProjectNames(Element rootElement) {
+        ArrayList projectNames = new ArrayList();
+        Iterator projectIterator = rootElement.getChildren("project").iterator();
+        while (projectIterator.hasNext()) {
+            Element projectElement = (Element) projectIterator.next();
+            String projectName = projectElement.getAttributeValue("name");
+            if (projectName == null) {
+                LOG.warn("configuration file contains project element with no name");
+            } else {
+                projectNames.add(projectName);
+            }
+        }
+
+        return (String[]) projectNames.toArray(new String[] {
+        });
+    }
+
+    public void addListener(Listener listener) {
+        LOG.debug("Listener added");
+        listeners.add(listener);
+    }
+
+    public static interface Listener extends EventListener {
+        void projectAdded(Project project);
+    }
+}
---
+++ main/src/net/sourceforge/cruisecontrol/jmx/CruiseControlControllerJMXAdaptor.java
@@ -0,0 +1,117 @@
+/********************************************************************************
+ * CruiseControl, a Continuous Integration Toolkit
+ * Copyright (c) 2001-2003, ThoughtWorks, Inc.
+ * 651 W Washington Ave. Suite 500
+ * Chicago, IL 60661 USA
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     + Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     + Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *     + Neither the name of ThoughtWorks, Inc., CruiseControl, nor the
+ *       names of its contributors may be used to endorse or promote
+ *       products derived from this software without specific prior
+ *       written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ********************************************************************************/
+package net.sourceforge.cruisecontrol.jmx;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+import javax.management.InvalidAttributeValueException;
+import javax.management.JMException;
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+
+import net.sourceforge.cruisecontrol.CruiseControlController;
+import net.sourceforge.cruisecontrol.CruiseControlException;
+import net.sourceforge.cruisecontrol.Project;
+import org.apache.log4j.Category;
+
+/**
+ *
+ * @author <a href="mailto:robertdw@users.sourceforge.net">Robert Watkins</a>
+ */
+public class CruiseControlControllerJMXAdaptor implements CruiseControlControllerJMXAdaptorMBean,
+        CruiseControlController.Listener {
+    private static final Category LOG = Category.getInstance(CruiseControlControllerJMXAdaptor.class);
+    private final CruiseControlController controller;
+    private MBeanServer server;
+
+    public CruiseControlControllerJMXAdaptor(CruiseControlController controlController) {
+        controller = controlController;
+        controller.addListener(this);
+    }
+
+    public String getConfigFileName() {
+        return controller.getConfigFile() != null ? controller.getConfigFile().getAbsolutePath() : "";
+    }
+
+    public void setConfigFileName(String fileName) throws InvalidAttributeValueException {
+        try {
+            controller.setConfigFile(fileName != null ? new File(fileName) : null);
+        } catch (CruiseControlException e) {
+            throw new InvalidAttributeValueException(e.getMessage());
+        }
+    }
+
+    public List getProjects() {
+        return controller.getProjects();
+    }
+
+    public void start() {
+        controller.start();
+    }
+
+    public void stop() {
+        controller.stop();
+    }
+
+    public void register(MBeanServer server) throws JMException {
+        this.server = server;
+        ObjectName controllerName = new ObjectName("CruiseControl Manager:id=unique");
+        server.registerMBean(this, controllerName);
+        updateProjectMBeans();
+    }
+
+    private void updateProjectMBeans() {
+        LOG.debug("Updating project mbeans");
+        if (server != null) {
+            for (Iterator iterator = controller.getProjects().iterator(); iterator.hasNext();) {
+                Project project = (Project) iterator.next();
+                projectAdded(project);
+            }
+        }
+    }
+
+    public void projectAdded(Project project) {
+        try {
+            LOG.debug("Registering project mbean");
+            ProjectController projectController = new ProjectController(project);
+            projectController.register(server);
+        } catch (JMException e) {
+            LOG.error("Could not register project " + project.getName(), e);
+        }
+    }
+}
