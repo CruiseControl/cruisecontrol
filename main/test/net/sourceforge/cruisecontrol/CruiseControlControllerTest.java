85b68cd3d158101e489613359db8a51794267fe2
Robert Watkins <robertdw@users.sourceforge.net>
2003-05-09 01:58:44 +0000

    Added some initial tests for CruiseControlController

---
+++ main/test/net/sourceforge/cruisecontrol/CruiseControlControllerTest.java
@@ -0,0 +1,111 @@
+/********************************************************************************
+ * CruiseControl, a Continuous Integration Toolkit
+ * Copyright (c) 2001, ThoughtWorks, Inc.
+ * 651 W Washington Ave. Suite 500
+ * Chicago, IL 60661 USA
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     + Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     + Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *     + Neither the name of ThoughtWorks, Inc., CruiseControl, nor the
+ *       names of its contributors may be used to endorse or promote
+ *       products derived from this software without specific prior
+ *       written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ********************************************************************************/
+package net.sourceforge.cruisecontrol;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author <a href="mailto:robertdw@users.sourceforge.net">Robert Watkins</a>
+ */
+public class CruiseControlControllerTest extends TestCase {
+    File configFile = new File("_tempConfigFile");
+
+    public void tearDown() {
+        if (configFile.exists()) {
+            configFile.delete();
+        }
+    }
+
+    public void testSetNoFile() {
+        CruiseControlController test = new CruiseControlController();
+        try {
+            test.setConfigFile(null);
+            fail("Allowed to not set a config file");
+        } catch (CruiseControlException expected) {
+            assertEquals("No config file", expected.getMessage());
+        }
+        try {
+            test.setConfigFile(configFile);
+            fail("Allowed to not set a config file");
+        } catch (CruiseControlException expected) {
+            assertEquals("No config file", expected.getMessage());
+        }
+    }
+
+    public void testLoadEmptyProjects() throws IOException, CruiseControlException {
+        CruiseControlController test = new CruiseControlController();
+        FileWriter configOut = new FileWriter(configFile);
+        configOut.write("<?xml version=\"1.0\" ?>\n");
+        configOut.write("<cruisecontrol>\n");
+        configOut.write("</cruisecontrol>\n");
+        configOut.close();
+
+        test.setConfigFile(configFile);
+        assertEquals(configFile, test.getConfigFile());
+        assertTrue(test.getProjects().isEmpty());
+    }
+
+    public void testLoadSomeProjects() throws IOException, CruiseControlException {
+        CruiseControlController test = new CruiseControlController() {
+            protected Project configureProject(String projectName) {
+                final Project project = new Project();
+                project.setName(projectName);
+                return project;
+            }
+        };
+        FileWriter configOut = new FileWriter(configFile);
+        configOut.write("<?xml version=\"1.0\" ?>\n");
+        configOut.write("<cruisecontrol>\n");
+        writeProjectDetails(configOut, "testProject1");
+        writeProjectDetails(configOut, "testProject2");
+        configOut.write("</cruisecontrol>\n");
+        configOut.close();
+
+        test.setConfigFile(configFile);
+        assertEquals(configFile, test.getConfigFile());
+        assertEquals(2, test.getProjects().size());
+    }
+
+    private void writeProjectDetails(FileWriter configOut, final String projectName) throws IOException {
+        configOut.write("<project name=\"" + projectName + "\" />\n");
+    }
+}
