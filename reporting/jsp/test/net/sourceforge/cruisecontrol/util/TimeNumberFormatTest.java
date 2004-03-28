0dcf387262b66a943da794afcc8e378b29488b20
Robert Watkins <robertdw@users.sourceforge.net>
2004-03-28 06:18:20 +0000

    Added a custom number formatter that will format numbers as their equivalent times.

---
+++ reporting/jsp/test/net/sourceforge/cruisecontrol/util/TimeNumberFormatTest.java
@@ -0,0 +1,69 @@
+/********************************************************************************
+ * CruiseControl, a Continuous Integration Toolkit
+ * Copyright (c) 2004, ThoughtWorks, Inc.
+ * 651 W Washington Ave. Suite 600
+ * Chicago, IL 60661 USA
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     + Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     + Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *     + Neither the name of ThoughtWorks, Inc., CruiseControl, nor the
+ *       names of its contributors may be used to endorse or promote
+ *       products derived from this software without specific prior
+ *       written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ********************************************************************************/
+package net.sourceforge.cruisecontrol.util;
+
+import java.text.NumberFormat;
+
+import junit.framework.TestCase;
+
+/**
+ * Test that we can format a number as a time.
+ * @author <a href="mailto:robertdw@users.sourceforge.net">Robert Watkins</a>
+ */
+public class TimeNumberFormatTest extends TestCase {
+    private NumberFormat test;
+    
+    protected void setUp() {
+        test = new TimeNumberFormat();
+    }
+
+    public void testMidnight() {
+        assertEquals("00:00", test.format(0));
+    }
+    
+    public void testOneMinutePastMidnight() {
+        assertEquals("00:01", test.format(60 * 1000));
+    }
+    
+    public void testOneInTheAfternoon() {
+        assertEquals("13:00", test.format(13 * 60 * 60 * 1000));
+    }
+    
+    public void testOneInTheMorningTheNextDay() {
+        assertEquals("01:00", test.format(25 * 60 * 60 * 1000));
+    }
+}
