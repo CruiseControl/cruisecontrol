60dabf40238f72b025147397bfac56955abad941
Jeffrey Fredrick <jfredrick@users.sourceforge.net>
2004-06-02 07:23:40 +0000

    cruisecontrol.sh: Support relative sym links. Patch by Jerome Lacoste

--- main/bin/cruisecontrol.sh
+++ main/bin/cruisecontrol.sh
@@ -42,7 +42,20 @@
 
 # Inspired by Ant's wrapper script
 if [ -z "$CCDIR" ] ; then
-  PRG=$0
+  # resolve links - $0 may be a softlink
+  PRG="$0"
+
+  # need this for relative symlinks
+  while [ -h "$PRG" ] ; do
+    ls=`ls -ld "$PRG"`
+    link=`expr "$ls" : '.*-> \(.*\)$'`
+    if expr "$link" : '/.*' > /dev/null; then
+      PRG="$link"
+    else
+      PRG=`dirname "$PRG"`"/$link"
+    fi
+  done
+
   saveddir=`pwd`
 
   CCDIR=`dirname "$PRG"`/..
