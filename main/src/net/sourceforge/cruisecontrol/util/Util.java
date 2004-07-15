02b4a3d0c287b598d6bd921423ac3f870f8bbcee
Joris Kuipers <jkuipers@users.sourceforge.net>
2004-07-15 15:39:40 +0000

    getMidnight could return noon; removed the test since it was a copy of the implementation
    git-svn-id: https://cruisecontrol.svn.sourceforge.net/svnroot/cruisecontrol/trunk/cruisecontrol@1448 878de816-9c12-0410-b5fe-b34e1928e20d

--- main/src/net/sourceforge/cruisecontrol/util/Util.java
+++ main/src/net/sourceforge/cruisecontrol/util/Util.java
@@ -143,7 +143,7 @@ public final class Util {
      */
     public static Date getMidnight() {
         Calendar midnight = Calendar.getInstance();
-        midnight.set(Calendar.HOUR, 0);
+        midnight.set(Calendar.HOUR_OF_DAY, 0);
         midnight.set(Calendar.MINUTE, 0);
         midnight.set(Calendar.SECOND, 0);
         midnight.set(Calendar.MILLISECOND, 0);
