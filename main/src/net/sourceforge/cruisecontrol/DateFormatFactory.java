4e4d6e93f3779cbfa015711a3dc7d51e9a2539dd
Jason Yip <jchyip@users.sourceforge.net>
2004-08-14 07:54:28 +0000

    Clean up of XMLLogHelperTest.  Patch by Shane Duan.

--- main/src/net/sourceforge/cruisecontrol/DateFormatFactory.java
+++ main/src/net/sourceforge/cruisecontrol/DateFormatFactory.java
@@ -43,6 +43,7 @@ public final class DateFormatFactory {
     private DateFormatFactory() {
     }
 
+    // TODO: return new SimpleDateFormat(dateFormatString)
     public static String getFormat() {
         return format;
     }
