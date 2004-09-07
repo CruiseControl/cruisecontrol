d146e6f84591b14d0e2e1a2b85ed2990747047ad
Jeffrey Fredrick <jfredrick@users.sourceforge.net>
2004-09-07 16:42:36 +0000

    add some debug output
    git-svn-id: https://cruisecontrol.svn.sourceforge.net/svnroot/cruisecontrol/trunk/cruisecontrol@1551 878de816-9c12-0410-b5fe-b34e1928e20d

--- main/src/net/sourceforge/cruisecontrol/listeners/CurrentBuildStatusListener.java
+++ main/src/net/sourceforge/cruisecontrol/listeners/CurrentBuildStatusListener.java
@@ -1,10 +1,11 @@
 package net.sourceforge.cruisecontrol.listeners;
 
+import net.sourceforge.cruisecontrol.CruiseControlException;
 import net.sourceforge.cruisecontrol.Listener;
 import net.sourceforge.cruisecontrol.ProjectEvent;
-import net.sourceforge.cruisecontrol.CruiseControlException;
 import net.sourceforge.cruisecontrol.ProjectState;
 import net.sourceforge.cruisecontrol.util.CurrentBuildFileWriter;
+import org.apache.log4j.Logger;
 
 import java.util.Date;
 
@@ -15,9 +16,12 @@ import java.util.Date;
  * Time: 10:54:33 PM
  */
 public class CurrentBuildStatusListener implements Listener {
+    private static final Logger LOG = Logger.getLogger(CurrentBuildStatusListener.class);
     private String fileName;
 
     public void handleEvent(final ProjectStateChangedEvent stateChanged) throws CruiseControlException {
+        LOG.debug("updating status to " + stateChanged.getNewState().getName()
+                + " for project " + stateChanged.getProjectName());
         final ProjectState newState = stateChanged.getNewState();
         final String text = "<span class=\"link\">" + newState.getDescription() + " as of:<br>";
         CurrentBuildFileWriter.writefile(text, new Date(), fileName);
@@ -25,6 +29,7 @@ public class CurrentBuildStatusListener implements Listener {
 
     public void handleEvent(ProjectEvent event) throws CruiseControlException {
         // ignore other ProjectEvents
+        LOG.debug("ignoring event " + event.getClass().getName() + " for project " + event.getProjectName());
     }
 
     public void validate() throws CruiseControlException {
@@ -36,5 +41,6 @@ public class CurrentBuildStatusListener implements Listener {
 
     public void setFile(String fileName) {
         this.fileName = fileName.trim();
+        LOG.debug("set fileName = " + fileName);
     }
 }
