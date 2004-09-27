827dcabdf81371b4d061a5f5d26f45736c4d69c2
Joris Kuipers <jkuipers@users.sourceforge.net>
2004-09-27 21:17:20 +0000

    XmlLoggerWithStatus to keep track of progress during the build by using the buildstatus file
    git-svn-id: https://cruisecontrol.svn.sourceforge.net/svnroot/cruisecontrol/trunk/cruisecontrol@1573 878de816-9c12-0410-b5fe-b34e1928e20d

---
+++ contrib/XmlLoggerWithStatus/XmlLoggerWithStatus.java
@@ -0,0 +1,104 @@
+package net.sourceforge.cruisecontrol.util;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.tools.ant.BuildEvent;
+import org.apache.tools.ant.XmlLogger;
+
+/**
+ * Extends XmlLogger by adding information about currently running target to a
+ * file specified by system property <br>
+ * XmlLoggerWithStatus.file
+ * 
+ * @author IgorSemenko (igor@semenko.com)
+ */
+public class XmlLoggerWithStatus extends XmlLogger {
+
+    private Writer out;
+    private boolean inited;
+
+    private static final SimpleDateFormat FORMATTER = new SimpleDateFormat("HH:mm:ss");
+
+    /**
+     * Reads current content of file defined by system property
+     * XmlLoggerWithStatus.file and creates a writer to it.
+     * 
+     * NOTE: this code MAY NOT be placed into buildStarted event because
+     * properties are not available at that point yet.
+     */
+    public void init(BuildEvent event) {
+        if (!inited) {
+            String statusFileName = event.getProject().getProperty("XmlLoggerWithStatus.file");
+            if (statusFileName == null) {
+                statusFileName = "buildstatus.txt";
+            }
+
+            //check directory
+            File parentDir = new File(statusFileName).getParentFile();
+            if (parentDir != null && !parentDir.exists()) {
+                parentDir.mkdirs();
+            }
+
+            // create a writer
+            try {
+                out = new FileWriter(statusFileName, true);
+            } catch (Exception e) {
+                System.err.println("Error opening file " + statusFileName + " for appending");
+                e.printStackTrace();
+            }
+            inited = true;
+        }
+    }
+
+    /**
+     * Closes writer to build status.
+     */
+    public void buildFinished(BuildEvent event) {
+        super.buildFinished(event);
+        if (out != null) {
+            try {
+                out.close();
+            } catch (IOException e) {
+                ;// no actions
+            }
+        }
+    }
+
+    /**
+     * Adds a line with started target and timestamp.
+     */
+    public void targetStarted(BuildEvent event) {
+        super.targetStarted(event);
+        init(event);
+        if (out != null) {
+            String name = event.getTarget().getName();
+            StringBuffer content = new StringBuffer();
+            content.append("<br>");
+            content.append("<span class=\"link\">");
+            content.append(FORMATTER.format(new Date()));
+            content.append("&nbsp;[");
+            content.append(name);
+            content.append("]&nbsp;</span>");
+            writeStatus(content);
+        }
+    }
+
+    /**
+     * Writes a line and flushes.
+     */
+    private void writeStatus(StringBuffer content) {
+        try {
+            out.write(content.toString());
+            out.flush();
+        } catch (IOException e) {
+            System.err.println("Error writing statusline to writer");
+            e.printStackTrace();
+        }
+    }
+
+}
