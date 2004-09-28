7c781ac1161e83d717d147e0d8ba9ca3d05fa7eb
Joris Kuipers <jkuipers@users.sourceforge.net>
2004-09-28 22:11:25 +0000

    replaced check for cygwin

--- release.sh
+++ release.sh
@@ -65,8 +65,7 @@ if test -z "${JAVA_HOME}" ; then
 fi
 
 # convert the existing path to unix
-if [ "$OSTYPE" = "cygwin32" ] || [ "$OSTYPE" = "cygwin" ] ; then
-   CLASSPATH=`cygpath --path --unix "$CLASSPATH"`
+if [ `uname | grep -n CYGWIN` ]; then
    JAVA_HOME=`cygpath --path --unix "$JAVA_HOME"`
 fi
 
@@ -87,9 +86,8 @@ if test -f ${JAVA_HOME}/lib/tools.jar ; then
 fi
 
 # convert the unix path to windows
-if [ "$OSTYPE" = "cygwin32" ] || [ "$OSTYPE" = "cygwin" ] ; then
+if [ `uname | grep -n CYGWIN` ]; then
    CLASSPATH=`cygpath --path --windows "$CLASSPATH"`
-   JAVA_HOME=`cygpath --path --windows "$JAVA_HOME"`
 fi
 
 # Call Ant
