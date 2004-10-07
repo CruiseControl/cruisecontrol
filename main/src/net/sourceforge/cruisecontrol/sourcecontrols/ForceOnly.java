d1de59f55569894a70a6866f54d43e65354d9d77
Joris Kuipers <jkuipers@users.sourceforge.net>
2004-10-07 19:29:16 +0000

    SourceControl that can be used to define a project that only builds when forced through JMX
    git-svn-id: https://cruisecontrol.svn.sourceforge.net/svnroot/cruisecontrol/trunk/cruisecontrol@1588 878de816-9c12-0410-b5fe-b34e1928e20d

---
+++ main/src/net/sourceforge/cruisecontrol/sourcecontrols/ForceOnly.java
@@ -0,0 +1,81 @@
+/********************************************************************************
+ * CruiseControl, a Continuous Integration Toolkit
+ * Copyright (c) 2001, ThoughtWorks, Inc.
+ * 651 W Washington Ave. Suite 600
+ * Chicago, IL 60661 USA
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     + Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     + Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *     + Neither the name of ThoughtWorks, Inc., CruiseControl, nor the
+ *       names of its contributors may be used to endorse or promote
+ *       products derived from this software without specific prior
+ *       written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ********************************************************************************/
+
+package net.sourceforge.cruisecontrol.sourcecontrols;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.Hashtable;
+import java.util.List;
+
+import net.sourceforge.cruisecontrol.CruiseControlException;
+import net.sourceforge.cruisecontrol.SourceControl;
+
+/**
+ * 
+ * Used to define a project that only builds when forced through the JMX interface,
+ * since no Modifications are ever returned. To report modifications when forcing a
+ * build, use this SourceControl as the Trigger part of a Compound SourceControl.
+ * 
+ * @author <a href="mailto:joriskuipers@xs4all.nl">Joris Kuipers</a>
+ *
+ */
+public class ForceOnly implements SourceControl {
+
+    private static final Hashtable EMPTY_HASHTABLE = new Hashtable();
+    
+    public List getModifications(Date lastBuild, Date now) {
+        return Collections.EMPTY_LIST;
+    }
+
+    public void validate() throws CruiseControlException {
+        // nothing to validate
+    }
+
+    public Hashtable getProperties() {
+        return EMPTY_HASHTABLE;
+    }
+
+    public void setProperty(String property) {
+        throw new UnsupportedOperationException("attribute 'property' is not supported");
+    }
+
+    public void setPropertyOnDelete(String property) {
+        throw new UnsupportedOperationException("attribute 'propertyOnDelete' is not supported");
+    }
+
+}
