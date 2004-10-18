ed823c34f9a241e938c01be4d4dc64842c687b9c
Joris Kuipers <jkuipers@users.sourceforge.net>
2004-10-18 20:05:02 +0000

    added copyright notice

--- main/src/net/sourceforge/cruisecontrol/bootstrappers/SnapshotCMBootstrapper.java
+++ main/src/net/sourceforge/cruisecontrol/bootstrappers/SnapshotCMBootstrapper.java
@@ -1,12 +1,49 @@
+/********************************************************************************
+ * CruiseControl, a Continuous Integration Toolkit
+ * Copyright (c) 2004, ThoughtWorks, Inc.
+ * 651 W Washington Ave. Suite 600
+ * Chicago, IL 60661 USA
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     + Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     + Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *     + Neither the name of ThoughtWorks, Inc., CruiseControl, nor the
+ *       names of its contributors may be used to endorse or promote
+ *       products derived from this software without specific prior
+ *       written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ********************************************************************************/
 package net.sourceforge.cruisecontrol.bootstrappers;
+
+import java.io.PrintWriter;
 
 import net.sourceforge.cruisecontrol.Bootstrapper;
 import net.sourceforge.cruisecontrol.CruiseControlException;
 import net.sourceforge.cruisecontrol.util.Commandline;
 import net.sourceforge.cruisecontrol.util.StreamPumper;
-import org.apache.log4j.Logger;
 
-import java.io.PrintWriter;
+import org.apache.log4j.Logger;
 
 /**
  * Since we rely on our build.xml to handle updating our source code, there has
@@ -27,7 +64,7 @@ import java.io.PrintWriter;
 public class SnapshotCMBootstrapper implements Bootstrapper {
 
     /** enable logging for this class */
-    private static Logger log = Logger.getLogger(SnapshotCMBootstrapper.class);
+    private static final Logger LOG = Logger.getLogger(SnapshotCMBootstrapper.class);
 
     /**
      *  Reference to the file to bootstrap.
@@ -46,8 +83,8 @@ public class SnapshotCMBootstrapper implements Bootstrapper {
         Commandline commandLine = buildUpdateCommand();
         Process p = null;
 
-        if (log.isDebugEnabled()) {
-            log.debug("Executing: " + commandLine.toString());
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Executing: " + commandLine.toString());
         }
         try {
             p = Runtime.getRuntime().exec(commandLine.getCommandline());
@@ -59,7 +96,7 @@ public class SnapshotCMBootstrapper implements Bootstrapper {
             p.getOutputStream().close();
             p.getErrorStream().close();
         } catch (Exception e) {
-            log.error("Error executing SnapshotCM update command", e);
+            LOG.error("Error executing SnapshotCM update command", e);
         }
     }
 
@@ -74,13 +111,9 @@ public class SnapshotCMBootstrapper implements Bootstrapper {
         commandLine.setExecutable("wco");
 
         commandLine.createArgument().setValue("-fR");
-        commandLine.createArgument().setValue(getFullPathFileName());
+        commandLine.createArgument().setValue(filename);
 
         return commandLine;
-    }
-
-    private String getFullPathFileName() {
-        return filename;
     }
 
 }
