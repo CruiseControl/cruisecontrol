32d5b8fa1c87c9d7c97223cfa9b620eebf52f36a
Jeffrey Fredrick <jfredrick@users.sourceforge.net>
2005-03-16 22:50:43 +0000

    AntPublisher will now pass the property "thisbuildsuccessful" to ant

---
+++ main/test/net/sourceforge/cruisecontrol/publishers/AntPublisherTest.java
@@ -0,0 +1,33 @@
+/*
+ * Created on Mar 16, 2005
+ */
+package net.sourceforge.cruisecontrol.publishers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import junit.framework.TestCase;
+import net.sourceforge.cruisecontrol.testutil.TestUtil;
+
+import org.jdom.Element;
+
+
+/**
+ * @author Jeffrey Fredrick
+ */
+public class AntPublisherTest extends TestCase {
+    
+    public void testPopulatePropertiesForAntBuilder() {
+        Element successfulBuild = TestUtil.createElement(true, true);
+        Element failedBuild = TestUtil.createElement(false, false);
+        Map properties = new HashMap();
+        AntPublisher publisher = new AntPublisher();
+        
+        publisher.populatePropertesForAntBuilder(successfulBuild, properties);
+        assertTrue("true".equals(properties.get("thisbuildsuccessful")));
+
+        publisher.populatePropertesForAntBuilder(failedBuild, properties);
+        assertTrue("false".equals(properties.get("thisbuildsuccessful")));
+    }
+    
+}
