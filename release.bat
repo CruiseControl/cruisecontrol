66681a3598817d693a6e31b22bfd04101875dadb
Jeffrey Fredrick <jfredrick@users.sourceforge.net>
2005-09-29 07:18:45 +0000

    add missing xml-apis-2.7.0.jar
    git-svn-id: https://cruisecontrol.svn.sourceforge.net/svnroot/cruisecontrol/trunk/cruisecontrol@2110 878de816-9c12-0410-b5fe-b34e1928e20d

--- release.bat
+++ release.bat
@@ -50,7 +50,7 @@ REM #--------------------------------------------
 REM # No need to edit anything past here
 REM #--------------------------------------------
 
-set ANT_CLASSPATH=%LIBDIR%\ant\ant-launcher.jar;%LIBDIR%\junit.jar;%LIBDIR%\xercesImpl-2.7.0.jar
+set ANT_CLASSPATH=%LIBDIR%\ant\ant-launcher.jar;%LIBDIR%\junit.jar;%LIBDIR%\xercesImpl-2.7.0.jar;%LIBDIR%\xml-apis-2.7.0.jar
 echo %ANT_CLASSPATH%
 
 if "%JAVA_HOME%" == "" goto noJavaFound
