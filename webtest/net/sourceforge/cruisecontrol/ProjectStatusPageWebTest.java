ee33d05d6786b2baf1af08e191fd80edee3759aa
Paul Julius <pauljulius@users.sourceforge.net>
2005-10-19 23:17:53 +0000

    Added "web" tests for the binary distribution. They are executed by the "webtest" target in release.xml.

---
+++ webtest/net/sourceforge/cruisecontrol/ProjectStatusPageWebTest.java
@@ -0,0 +1,26 @@
+package net.sourceforge.cruisecontrol;
+
+import net.sourceforge.jwebunit.WebTestCase;
+import com.meterware.httpunit.HttpUnitOptions;
+
+public class ProjectStatusPageWebTest extends WebTestCase {
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        getTestContext().setBaseUrl("http://localhost:7854");
+    }
+
+    public void testForceBuild() {
+        //HttpUnitOptions.setExceptionsThrownOnScriptError(false);
+        beginAt("/cruisecontrol");
+        assertTextPresent("CruiseControl Status Page");
+        clickLinkWithImage("play_white_bkg.png");
+        assertTextPresent("CruiseControl Status Page");
+
+        //Make sure the build actually started running.
+        clickLinkWithText("commons-math");
+        clickLinkWithText("Control Panel");
+        assertTextNotPresent("waiting for next time to build");
+
+    }
+}
