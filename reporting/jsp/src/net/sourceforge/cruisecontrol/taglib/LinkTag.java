96c10d6aa06d413bbeb12f7fe0718f44f67c3d15
Hack Kampbjørn <hack@users.sourceforge.net>
2005-10-21 11:15:44 +0000

    Add some javadoc: Creates a link to this page excluding one request parameter. By default the parameter excluded is log.
    git-svn-id: https://cruisecontrol.svn.sourceforge.net/svnroot/cruisecontrol/trunk/cruisecontrol@2175 878de816-9c12-0410-b5fe-b34e1928e20d

--- reporting/jsp/src/net/sourceforge/cruisecontrol/taglib/LinkTag.java
+++ reporting/jsp/src/net/sourceforge/cruisecontrol/taglib/LinkTag.java
@@ -40,7 +40,8 @@ import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.Tag;
 
 /**
- *
+ * Creates a link to this page excluding one request parameter.
+ * By default the parameter excluded is log.
  * @author <a href="mailto:robertdw@users.sourceforge.net">Robert Watkins</a>
  */
 public class LinkTag extends CruiseControlTagSupport {
