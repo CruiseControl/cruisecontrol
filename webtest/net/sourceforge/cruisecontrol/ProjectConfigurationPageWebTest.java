adf402c10ac5761a14a2b9439b8ee47fcd563aaf
Paul Julius <pauljulius@users.sourceforge.net>
2005-10-31 22:16:21 +0000

    Webtest for basic configuration editor functionality.
    Patch by Kent Spillner.
    
    
    git-svn-id: https://cruisecontrol.svn.sourceforge.net/svnroot/cruisecontrol/trunk/cruisecontrol@2221 878de816-9c12-0410-b5fe-b34e1928e20d

--- webtest/net/sourceforge/cruisecontrol/ProjectConfigurationPageWebTest.java
+++ webtest/net/sourceforge/cruisecontrol/ProjectConfigurationPageWebTest.java
@@ -3,11 +3,23 @@ package net.sourceforge.cruisecontrol;
 import net.sourceforge.jwebunit.WebTestCase;
 
 public class ProjectConfigurationPageWebTest extends WebTestCase {
-
+    private Configuration configuration;
+    private String configurationContents;
+    
     protected void setUp() throws Exception {
         super.setUp();
         getTestContext().setBaseUrl("http://localhost:7854");
+        
+        configuration = new Configuration("localhost", 7856);
+        configurationContents = configuration.getConfiguration();
     }
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+
+        configuration.setConfiguration(configurationContents);
+    }
+
 
     public void testShouldLoadRawXMLConfigurationData() {
         beginAt("/cruisecontrol/config/commons-math?tab=config");
@@ -17,5 +29,19 @@ public class ProjectConfigurationPageWebTest extends WebTestCase {
         assertFormElementPresent("configuration");
         assertTextPresent("<cruisecontrol>");
         assertTextPresent("</cruisecontrol>");
+    }
+    
+    public void testShouldSaveChangesToXMLConfigurationData() {
+        beginAt("/cruisecontrol/config/commons-math?tab=config");
+        assertFormPresent("commons-math-config");
+        assertSubmitButtonPresent("configure");
+        setWorkingForm("commons-math-config");
+        setFormElement("configuration", "<?xml version=\"1.0\" encoding=\"UTF-8\"?><!-- Empty config --><cruisecontrol />");
+        submit();
+        assertFormPresent("commons-math-config");
+        assertSubmitButtonPresent("configure");
+        setWorkingForm("commons-math-config");
+        assertFormElementPresent("configuration");
+        assertTextPresent("<!-- Empty config -->");
     }
 }
