356adbb12174a6da25d17437c14686a7b5481192
Hack Kampbjørn <hack@users.sourceforge.net>
2005-11-01 11:41:53 +0000

    Test that configuration files with a thread count can be loaded

--- main/test/net/sourceforge/cruisecontrol/CruiseControlControllerTest.java
+++ main/test/net/sourceforge/cruisecontrol/CruiseControlControllerTest.java
@@ -129,6 +129,19 @@ public class CruiseControlControllerTest extends TestCase {
         assertTrue(ccController.getProjects().isEmpty());
     }
 
+    public void testLoadThreadCount() throws IOException, CruiseControlException {
+        FileWriter configOut = new FileWriter(configFile);
+        writeHeader(configOut);
+        configOut.write("<system><configuration>"
+                + "<threads count=\"2\"/>"
+                + "</configuration></system>");
+        writeFooterAndClose(configOut);
+
+        ccController.setConfigFile(configFile);
+        assertEquals(configFile, ccController.getConfigFile());
+        assertTrue(ccController.getProjects().isEmpty());
+    }
+
     public void testLoadSomeProjects() throws IOException, CruiseControlException {
         ccController = new MockCruiseControlController();
 
