a6e5967c73ac2181a4134f0e15b9b37070494471
Paul Julius <pauljulius@users.sourceforge.net>
2005-11-29 16:43:17 +0000

    No longer extends AntBuilder, but delegates to an AntBuilder instance. Patch by Kent Spillner.
    git-svn-id: https://cruisecontrol.svn.sourceforge.net/svnroot/cruisecontrol/trunk/cruisecontrol@2285 878de816-9c12-0410-b5fe-b34e1928e20d

--- main/src/net/sourceforge/cruisecontrol/bootstrappers/AccurevBootstrapper.java
+++ main/src/net/sourceforge/cruisecontrol/bootstrappers/AccurevBootstrapper.java
@@ -39,6 +39,7 @@ package net.sourceforge.cruisecontrol.bootstrappers;
 import net.sourceforge.cruisecontrol.Bootstrapper;
 import net.sourceforge.cruisecontrol.CruiseControlException;
 import net.sourceforge.cruisecontrol.builders.AntBuilder;
+import net.sourceforge.cruisecontrol.builders.Property;
 import net.sourceforge.cruisecontrol.sourcecontrols.accurev.AccurevCommand;
 import net.sourceforge.cruisecontrol.sourcecontrols.accurev.AccurevCommandline;
 import net.sourceforge.cruisecontrol.sourcecontrols.accurev.Runner;
@@ -50,12 +51,14 @@ import net.sourceforge.cruisecontrol.sourcecontrols.accurev.Runner;
  * @author <a href="mailto:jason_chown@scee.net">Jason Chown</a>
  * @author <a href="mailto:Nicola_Orru@scee.net">Nicola Orru'</a>
  */
-public class AccurevBootstrapper extends AntBuilder implements Bootstrapper {
+public class AccurevBootstrapper implements Bootstrapper {
   private boolean verbose;
   private boolean keep;
   private boolean synctime;
   private String  workspace;
   private Runner  runner;
+  private AntBuilder delegate = new AntBuilder();
+
   /**
    * Enables/disables verbose logging
    * 
@@ -120,5 +123,114 @@ public class AccurevBootstrapper extends AntBuilder implements Bootstrapper {
   }
   public void setRunner(Runner runner) {
     this.runner = runner;
+  }
+
+  public void validate() throws CruiseControlException {
+      delegate.validate();
+  }
+
+  /**
+   * @see net.sourceforge.cruisecontrol.builders.AntBuilder#setSaveLogDir(String)
+   */
+  public void setSaveLogDir(String dir) {
+      delegate.setSaveLogDir(dir);
+  }
+
+  /**
+   * @see net.sourceforge.cruisecontrol.builders.AntBuilder#setAntWorkingDir(String)
+   */
+  public void setAntWorkingDir(String dir) {
+      delegate.setAntWorkingDir(dir);
+  }
+
+  /**
+   * @see net.sourceforge.cruisecontrol.builders.AntBuilder#setAntScript(String)
+   */
+  public void setAntScript(String antScript) {
+      delegate.setAntScript(antScript);
+  }
+
+  /**
+   * @see net.sourceforge.cruisecontrol.builders.AntBuilder#setAntHome(String)
+   */
+  public void setAntHome(String antHome) {
+      delegate.setAntHome(antHome);
+  }
+
+  /**
+   * @see net.sourceforge.cruisecontrol.builders.AntBuilder#setTempFile(String)
+   */
+  public void setTempFile(String tempFileName) {
+      delegate.setTempFile(tempFileName);
+  }
+
+  /**
+   * @see net.sourceforge.cruisecontrol.builders.AntBuilder#setTarget(String)
+   */
+  public void setTarget(String target) {
+      delegate.setTarget(target);
+  }
+
+  /**
+   * @see net.sourceforge.cruisecontrol.builders.AntBuilder#setBuildFile(String)
+   */
+  public void setBuildFile(String buildFile) {
+      delegate.setBuildFile(buildFile);
+  }
+
+  /**
+   * @see net.sourceforge.cruisecontrol.builders.AntBuilder#setUseLogger(boolean)
+   */
+  public void setUseLogger(boolean useLogger) {
+      delegate.setUseLogger(useLogger);
+  }
+
+  /**
+   * @see net.sourceforge.cruisecontrol.builders.AntBuilder#createJVMArg()
+   */
+  public Object createJVMArg() {
+      return delegate.createJVMArg();
+  }
+
+  /**
+   * @see net.sourceforge.cruisecontrol.builders.AntBuilder#createProperty()
+   */
+  public Property createProperty() {
+      return delegate.createProperty();
+  }
+
+  /**
+   * @see net.sourceforge.cruisecontrol.builders.AntBuilder#setUseDebug(boolean)
+   */
+  public void setUseDebug(boolean debug) {
+      delegate.setUseDebug(debug);
+  }
+
+  /**
+   * @see net.sourceforge.cruisecontrol.builders.AntBuilder#setUseQuiet(boolean)
+   */
+  public void setUseQuiet(boolean quiet) {
+      delegate.setUseQuiet(quiet);
+  }
+
+  /**
+   * @see net.sourceforge.cruisecontrol.builders.AntBuilder#getLoggerClassName()
+   */
+  public String getLoggerClassName() {
+      return delegate.getLoggerClassName();
+  }
+
+  /**
+   * @see net.sourceforge.cruisecontrol.builders.AntBuilder#setLoggerClassName(String)
+   */
+  public void setLoggerClassName(String string) {
+      delegate.setLoggerClassName(string);
+  }
+
+  /**
+   * @see net.sourceforge.cruisecontrol.builders.AntBuilder#setTimeout(long)
+   */
+  public void setTimeout(long timeout) {
+      delegate.setTimeout(timeout);
   }
 }
