ee1126c4904d5bc0aeeb082842922bfcd221ace1
Jeffrey Fredrick <jfredrick@users.sourceforge.net>
2006-01-15 15:05:33 +0000

    LockFileBootstrapper/Listener: Added a pair of plug-ins that coordinate  such that any two projects with the same lock file will not build  simultaneously.

---
+++ main/src/net/sourceforge/cruisecontrol/listeners/LockFileListener.java
@@ -0,0 +1,55 @@
+package net.sourceforge.cruisecontrol.listeners;
+
+import java.io.File;
+
+import net.sourceforge.cruisecontrol.CruiseControlException;
+import net.sourceforge.cruisecontrol.Listener;
+import net.sourceforge.cruisecontrol.ProjectEvent;
+import net.sourceforge.cruisecontrol.ProjectState;
+import net.sourceforge.cruisecontrol.util.ValidationHelper;
+
+public class LockFileListener implements Listener {
+
+    private String path;
+    private boolean needToDeleteLock = false;
+
+    public void handleEvent(ProjectEvent event) throws CruiseControlException {
+        if (!(event instanceof ProjectStateChangedEvent)) {
+            return;
+        }
+
+        ProjectState newState = ((ProjectStateChangedEvent) event).getNewState();
+        
+        if (projectGotPastBootstrapping(newState)) {
+            needToDeleteLock = true;
+        }
+        
+        if (newState.equals(ProjectState.IDLE) && needToDeleteLock) {
+            boolean deletedFile = attemptToDeleteLock();
+            if (!deletedFile) {
+                throw new CruiseControlException("project " + event.getProjectName()
+                                + " failed to delete lock file " + path);
+            }
+        }
+    }
+
+    boolean attemptToDeleteLock() {
+        return new File(path).delete();
+    }
+
+    private boolean projectGotPastBootstrapping(ProjectState newState) {
+        if (newState.equals(ProjectState.MODIFICATIONSET)) {
+            return true;
+        }
+        return false;
+    }
+
+    public void validate() throws CruiseControlException {
+        ValidationHelper.assertIsSet(path, "lockfile", LockFileListener.class);
+    }
+
+    public void setLockFile(String path) {
+        this.path = path;
+    }
+
+}
