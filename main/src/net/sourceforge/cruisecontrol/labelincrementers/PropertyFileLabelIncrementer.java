c406df9835a6ffebc085120fe4ed3a39e6feb3e2
Jeffrey Fredrick <jfredrick@users.sourceforge.net>
2006-02-06 01:13:19 +0000

    PropertyFileLabelIncrementer: New plug-in that returns the label from a                  properties file (CC-242). Submitted by Eugene Kuleshov

---
+++ main/src/net/sourceforge/cruisecontrol/labelincrementers/PropertyFileLabelIncrementer.java
@@ -0,0 +1,73 @@
+package net.sourceforge.cruisecontrol.labelincrementers;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import net.sourceforge.cruisecontrol.LabelIncrementer;
+
+import org.apache.log4j.Logger;
+import org.jdom.Element;
+
+public class PropertyFileLabelIncrementer implements LabelIncrementer {
+    private static final Logger LOG = Logger.getLogger(PropertyFileLabelIncrementer.class);
+
+    private String propertyFile;
+
+    private String propertyName;
+
+    private boolean preBuildIncrementer;
+
+    public String incrementLabel(String oldLabel, Element buildLog) {
+        return getLabel();
+    }
+
+    public boolean isValidLabel(String label) {
+        return true;
+    }
+
+    public boolean isPreBuildIncrementer() {
+        return this.preBuildIncrementer;
+    }
+
+    public String getDefaultLabel() {
+        return getLabel();
+    }
+
+    public void setPreBuildIncrementer(boolean preBuildIncrementer) {
+        this.preBuildIncrementer = preBuildIncrementer;
+    }
+
+    public void setPropertyFile(String propertyFile) {
+        this.propertyFile = propertyFile;
+    }
+
+    public void setPropertyName(String propertyName) {
+        this.propertyName = propertyName;
+    }
+
+    private String getLabel() {
+        InputStream is = null;
+        try {
+            is = new FileInputStream(propertyFile);
+            Properties p = new Properties();
+            p.load(is);
+
+            String label = p.getProperty(propertyName);
+            LOG.info("Retrieved label " + label);
+
+            return label;
+        } catch (IOException ex) {
+            String msg = "Unable to retrieve label " + ex.getMessage();
+            LOG.error(msg, ex);
+            throw new RuntimeException(msg);
+        } finally {
+            try {
+                is.close();
+            } catch (Exception ex) {
+            }
+        }
+    }
+
+}
