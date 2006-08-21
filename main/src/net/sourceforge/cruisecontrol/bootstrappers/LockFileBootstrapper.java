a64d5e682ffa1c6471b75318cbfd16306f60cef8
Jeffrey Fredrick <jfredrick@users.sourceforge.net>
2006-08-21 03:00:22 +0000

    add debug level message when lock file is created

--- main/src/net/sourceforge/cruisecontrol/bootstrappers/LockFileBootstrapper.java
+++ main/src/net/sourceforge/cruisecontrol/bootstrappers/LockFileBootstrapper.java
@@ -3,6 +3,8 @@ package net.sourceforge.cruisecontrol.bootstrappers;
 import java.io.File;
 import java.io.IOException;
 
+import org.apache.log4j.Logger;
+
 import net.sourceforge.cruisecontrol.Bootstrapper;
 import net.sourceforge.cruisecontrol.CruiseControlException;
 import net.sourceforge.cruisecontrol.util.ValidationHelper;
@@ -10,13 +12,15 @@ import net.sourceforge.cruisecontrol.util.ValidationHelper;
 public class LockFileBootstrapper implements Bootstrapper {
 
     private String path;
+    private static final Logger LOG = Logger.getLogger(LockFileBootstrapper.class);
 
     public void bootstrap() throws CruiseControlException {
         File lock = new File(path);
         try {
             if (!lock.createNewFile()) {
-                throw new CruiseControlException("Lock file already exists, aborting build attempt.");
+                throw new CruiseControlException("Lock file [" + path + "] already exists, aborting build attempt.");
             } else {
+                LOG.debug("Created lock file [" + path + "]");
                 lock.deleteOnExit();
             }
         } catch (IOException e) {
