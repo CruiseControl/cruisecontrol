6141aaaf62ef393a4a5035f7bc36284482b4d773
Jason Yip <jchyip@users.sourceforge.net>
2006-09-02 05:01:12 +0000

    Use double instead of single quotes for escaping dates
    git-svn-id: https://cruisecontrol.svn.sourceforge.net/svnroot/cruisecontrol/trunk/cruisecontrol@2659 878de816-9c12-0410-b5fe-b34e1928e20d

--- main/test/net/sourceforge/cruisecontrol/sourcecontrols/SVNTest.java
+++ main/test/net/sourceforge/cruisecontrol/sourcecontrols/SVNTest.java
@@ -135,7 +135,7 @@ public class SVNTest extends TestCase {
                 "--xml",
                 "-v",
                 "-r",
-                "'{" + SVN.formatSVNDate(lastBuild) + "}':'{" + SVN.formatSVNDate(checkTime) + "}'"};
+                "\"{" + SVN.formatSVNDate(lastBuild) + "}\":\"{" + SVN.formatSVNDate(checkTime) + "}\""};
         String[] actualCmd = svn.buildHistoryCommand(lastBuild, checkTime).getCommandline();
         assertArraysEquals(expectedCmd, actualCmd);
 
@@ -149,7 +149,7 @@ public class SVNTest extends TestCase {
                 "--xml",
                 "-v",
                 "-r",
-                "'{" + SVN.formatSVNDate(lastBuild) + "}':'{" + SVN.formatSVNDate(checkTime) + "}'",
+                "\"{" + SVN.formatSVNDate(lastBuild) + "}\":\"{" + SVN.formatSVNDate(checkTime) + "}\"",
                 "http://svn.collab.net/repos/svn" };
         actualCmd = svn.buildHistoryCommand(lastBuild, checkTime).getCommandline();
         assertArraysEquals(expectedCmd, actualCmd);
@@ -165,7 +165,7 @@ public class SVNTest extends TestCase {
                 "--xml",
                 "-v",
                 "-r",
-                "'{" + SVN.formatSVNDate(lastBuild) + "}':'{" + SVN.formatSVNDate(checkTime) + "}'",
+                "\"{" + SVN.formatSVNDate(lastBuild) + "}\":\"{" + SVN.formatSVNDate(checkTime) + "}\"",
                 "--username",
                 "lee",
                 "--password",
