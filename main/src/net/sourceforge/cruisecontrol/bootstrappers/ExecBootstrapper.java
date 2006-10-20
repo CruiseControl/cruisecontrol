4c3d3e19ff57b2858ec10412d9f8c9150399328b
Dan Rollo <bhamail@users.sourceforge.net>
2006-10-20 19:31:51 +0000

    commit (CC-538) New CompositeBuilder and ExecBootstrapper. Submitted by Frederic Mockel.
    git-svn-id: https://cruisecontrol.svn.sourceforge.net/svnroot/cruisecontrol/trunk/cruisecontrol@2697 878de816-9c12-0410-b5fe-b34e1928e20d

---
+++ main/src/net/sourceforge/cruisecontrol/bootstrappers/ExecBootstrapper.java
@@ -0,0 +1,134 @@
+/********************************************************************************
+ * CruiseControl, a Continuous Integration Toolkit
+ * Copyright (c) 2001, ThoughtWorks, Inc.
+ * 651 W Washington Ave. Suite 500
+ * Chicago, IL 60661 USA
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     + Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     + Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *     + Neither the name of ThoughtWorks, Inc., CruiseControl, nor the
+ *       names of its contributors may be used to endorse or promote
+ *       products derived from this software without specific prior
+ *       written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ********************************************************************************/
+package net.sourceforge.cruisecontrol.bootstrappers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import net.sourceforge.cruisecontrol.Bootstrapper;
+import net.sourceforge.cruisecontrol.CruiseControlException;
+import net.sourceforge.cruisecontrol.util.ValidationHelper;
+import net.sourceforge.cruisecontrol.builders.ExecBuilder;
+
+import org.apache.log4j.Logger;
+import org.jdom.Attribute;
+import org.jdom.Element;
+
+public class ExecBootstrapper implements Bootstrapper {
+
+    private static final Logger LOG = Logger.getLogger(ExecBootstrapper.class);
+    private ExecBuilder delegate = new ExecBuilder();
+
+    /**
+     * Called after the configuration is read to make sure that all the mandatory parameters
+     * were specified..
+     *
+     * @throws CruiseControlException if there was a configuration error.
+     */
+    public void validate() throws CruiseControlException {
+        // assert with this classname in error message, otherwise delegate classname is used
+        ValidationHelper.assertIsSet(delegate.getCommand(), "command", this.getClass());
+
+        delegate.validate();
+    }
+
+    public void bootstrap() throws CruiseControlException {
+
+        final Map properties = new HashMap();
+        // Run ExecuteBuilder
+        final Element result = delegate.build(properties);
+        if (result == null) {
+            LOG.error("ExecBootstrapper failed.\n\n");
+            throw new CruiseControlException("ExecBootstrapper failed with null result");
+        } else {
+            Attribute error = result.getAttribute("error");
+            if (error == null) {
+                LOG.info("ExecBootstrapper successful.");
+            } else {
+                LOG.error("ExecBootstrapper failed.\n\n"
+                        + error.getValue()
+                        + "\n");
+                throw new CruiseControlException("ExecBootstrapper failed: " + error.getValue());
+            }
+        }
+    }
+
+    /**
+     * Sets build timeout in seconds.
+     *
+     * @param timeout long build timeout
+     */
+    public void setTimeout(long timeout) {
+        delegate.setTimeout(timeout);
+    } // setTimeout
+
+    /**
+     * Sets the command to execute
+     *
+     * @param cmd the command to execute
+     */
+    public void setCommand(String cmd) {
+        delegate.setCommand(cmd);
+    } // setCommand
+
+    /**
+     * Sets the arguments for the command to execute
+     *
+     * @param args arguments for the command to execute
+     */
+    public void setArgs(String args) {
+        delegate.setArgs(args);
+    } // setArgs
+
+    /**
+     * Sets the error string to search for in the command output
+     *
+     * @param errStr the error string to search for in the command output
+     */
+    public void setErrorStr(String errStr) {
+        delegate.setErrorStr(errStr);
+    } // setErrorStr
+
+    /**
+     * Sets the working directory where the command is to be executed
+     *
+     * @param dir the directory where the command is to be executed
+     */
+    public void setWorkingDir(String dir) {
+        delegate.setWorkingDir(dir);
+    } // setWorkingDir
+}
---
+++ main/test/net/sourceforge/cruisecontrol/builders/CompositeBuilderTest.java
@@ -0,0 +1,224 @@
+/********************************************************************************
+ * CruiseControl, a Continuous Integration Toolkit
+ * Copyright (c) 2001, ThoughtWorks, Inc.
+ * 651 W Washington Ave. Suite 600
+ * Chicago, IL 60661 USA
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     + Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     + Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *     + Neither the name of ThoughtWorks, Inc., CruiseControl, nor the
+ *       names of its contributors may be used to endorse or promote
+ *       products derived from this software without specific prior
+ *       written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ********************************************************************************/
+package net.sourceforge.cruisecontrol.builders;
+
+import java.util.HashMap;
+
+import junit.framework.TestCase;
+import net.sourceforge.cruisecontrol.CruiseControlException;
+
+import org.jdom.Element;
+
+/**
+ * CompositeBuilder test class.
+ *
+ * @author <a href="mailto:frederic.mockel@gillardon.de">Frederic Mockel</a>
+ */
+public class CompositeBuilderTest extends TestCase {
+
+    private CompositeBuilder builder;
+
+    private String buildLog1txt = "<cruisecontrol>\n"
+            + "<modifications>\n"
+            + "<modification type=\"always\">\n"
+            + "<file action=\"change\">\n"
+            + "<filename>force build</filename>\n"
+            + "<project>force build</project>\n"
+            + "</file>\n"
+            + "<date>09/27/2006 00:00:00</date>\n"
+            + "<user>cruisecontroluser</user>\n"
+            + "<comment />\n"
+            + "</modification>\n"
+            + "</modifications>\n"
+            + "<info>\n"
+            + "<property name=\"projectname\" value=\"TestCruisecontrol\" />\n"
+            + "</info>\n"
+            + "<build time=\"1 second\" >\n"
+            + "<target name=\"exec\">\n"
+            + "<task name=\"cvs\">\n"
+            + "<message priority=\"info\"><do something></message>\n"
+            + "<message priority=\"info\"><do something more></message>\n"
+            + "<message priority=\"info\"><boring...></message>\n"
+            + "</task>\n"
+            + "</target>\n"
+            + "</build>\n"
+            + "</cruisecontrol>\n";
+
+    private String buildLog2txt = "<cruisecontrol>\n"
+            + "<modifications>\n"
+            + "<modification type=\"always\">\n"
+            + "<file action=\"change\">\n"
+            + "<filename>force build</filename>\n"
+            + "<project>force build</project>\n"
+            + "</file>\n"
+            + "<date>09/27/2006 00:00:00</date>\n"
+            + "<user>cruisecontroluser</user>\n"
+            + "<comment />\n"
+            + "</modification>\n"
+            + "</modifications>\n"
+            + "<info>\n"
+            + "<property name=\"projectname\" value=\"TestCruisecontrol\" />\n"
+            + "</info>\n"
+            + "<build time=\"1 second\" >\n"
+            + "<target name=\"exec\">\n"
+            + "<task name=\"cvs\">\n"
+            + "<message priority=\"info\"><do something></message>\n"
+            + "<message priority=\"info\"><do something more></message>\n"
+            + "<message priority=\"info\"><boring...></message>\n"
+            + "<message priority=\"error\"><error occured></message>\n"
+            + "</task>\n"
+            + "</target>\n"
+            + "</build>\n"
+            + "</cruisecontrol>\n";
+
+    public CompositeBuilderTest(String name) {
+        super(name);
+    }
+
+    public void testValidateShouldThrowExceptionWhenNoBuilderIsAdded() {
+        builder = new CompositeBuilder();
+        try {
+            builder.validate();
+            fail();
+        } catch (CruiseControlException expected) {
+        }
+    }
+
+    public void testBuildAllBuildersWhenNoErrorOccured() throws Exception {
+
+        Element buildLog1 = new Element("cruisecontrol");
+        buildLog1.addContent(buildLog1txt);
+
+        builder = new CompositeBuilder();
+        HashMap buildProperties = new HashMap();
+        MockBuilder mock1 = new MockBuilder("builder1");
+        MockBuilder mock2 = new MockBuilder("builder2");
+
+        builder.add(mock1);
+        builder.add(mock2);
+
+        mock1.setBuildLogXML(buildLog1);
+        mock2.setBuildLogXML(buildLog1);
+
+        final Element result = builder.build(buildProperties);
+        assertNotNull(result);
+        
+        assertTrue("builder1 didn't build", mock1.isBuildCalled());
+        assertTrue("builder2 didn't build", mock2.isBuildCalled());
+    }
+
+    public void testBuildWithTargetAllBuilders() throws Exception {
+
+        Element buildLog1 = new Element("cruisecontrol");
+        buildLog1.addContent(buildLog1txt);
+
+        builder = new CompositeBuilder();
+        HashMap buildProperties = new HashMap();
+        MockBuilder mock1 = new MockBuilder("builder1");
+        MockBuilder mock2 = new MockBuilder("builder2");
+
+        builder.add(mock1);
+        builder.add(mock2);
+
+        mock1.setBuildLogXML(buildLog1);
+        mock2.setBuildLogXML(buildLog1);
+
+        final String mockTarget = "mockTarget";
+        final Element result = builder.buildWithTarget(buildProperties, mockTarget);
+        assertNotNull(result);
+
+        assertTrue("builder1 didn't build", mock1.isBuildCalled());
+        assertEquals("builder1 didn't build with target", mockTarget, mock1.getTarget());
+        assertTrue("builder2 didn't build", mock2.isBuildCalled());
+        assertEquals("builder2 didn't build with target", mockTarget, mock1.getTarget());
+    }
+
+    public void testBuildAllBuildersWhenAnErrorOccured() throws Exception {
+
+        Element buildLog1 = new Element("cruisecontrol");
+        buildLog1.addContent(buildLog1txt);
+        Element buildLog2 = new Element("cruisecontrol");
+        buildLog1.addContent(buildLog2txt);
+
+        builder = new CompositeBuilder();
+        HashMap buildProperties = new HashMap();
+        MockBuilder mock1 = new MockBuilder("builder1");
+        MockBuilder mock2 = new MockBuilder("builder2");
+        MockBuilder mock3 = new MockBuilder("builder3");
+
+        builder.add(mock1);
+        builder.add(mock2);
+        builder.add(mock3);
+
+        mock1.setBuildLogXML(buildLog1);
+        mock2.setBuildLogXML(buildLog2);
+        mock3.setBuildLogXML(buildLog1);
+
+        builder.build(buildProperties);
+        assertTrue("builder3 didn't build", mock3.isBuildCalled());
+        assertEquals("builder3 should not have built with target", null, mock3.getTarget());
+    }
+
+    public void testBuildWithTargetWhenAnErrorOccured() throws Exception {
+
+        Element buildLog1 = new Element("cruisecontrol");
+        buildLog1.addContent(buildLog1txt);
+        Element buildLog2 = new Element("cruisecontrol");
+        buildLog1.addContent(buildLog2txt);
+
+        builder = new CompositeBuilder();
+        HashMap buildProperties = new HashMap();
+        MockBuilder mock1 = new MockBuilder("builder1");
+        MockBuilder mock2 = new MockBuilder("builder2");
+        MockBuilder mock3 = new MockBuilder("builder3");
+
+        builder.add(mock1);
+        builder.add(mock2);
+        builder.add(mock3);
+
+        mock1.setBuildLogXML(buildLog1);
+        mock2.setBuildLogXML(buildLog2);
+        mock3.setBuildLogXML(buildLog1);
+
+        final String mockTargetWError = "mockTargetWithError";
+        final Element result = builder.buildWithTarget(buildProperties, mockTargetWError);
+        assertNotNull(result);
+        
+        assertTrue("builder3 didn't build", mock3.isBuildCalled());
+        assertEquals("builder2 didn't build with target", mockTargetWError, mock3.getTarget());
+    }
+} // CompositeBuilderTest
