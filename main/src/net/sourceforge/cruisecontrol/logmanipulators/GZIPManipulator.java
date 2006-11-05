12feb6412e9e30fdfe3468483ac1df03daa677d8
Hack Kampbjørn <hack@users.sourceforge.net>
2006-11-05 12:53:28 +0000

    Ensure that the file streams are closed by using a finally clause and use a buffer to compress the log instead of doing it a byte at a time. This was lingering in my CVS checkout
    git-svn-id: https://cruisecontrol.svn.sourceforge.net/svnroot/cruisecontrol/trunk/cruisecontrol@2716 878de816-9c12-0410-b5fe-b34e1928e20d

--- main/src/net/sourceforge/cruisecontrol/logmanipulators/GZIPManipulator.java
+++ main/src/net/sourceforge/cruisecontrol/logmanipulators/GZIPManipulator.java
@@ -4,9 +4,15 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.zip.GZIPOutputStream;
+import net.sourceforge.cruisecontrol.util.IO;
+import org.apache.log4j.Logger;
 
 public class GZIPManipulator extends BaseManipulator {
+    private static final Logger LOG = Logger.getLogger(GZIPManipulator.class);
+    private static final int BUFFER_SIZE = 4 * 1024;
 
     public void execute(String logDir) {
         File[] filesToGZip = getRelevantFiles(logDir, false);
@@ -17,22 +23,26 @@ public class GZIPManipulator extends BaseManipulator {
     }
 
     private void gzipFile(File logfile, String logDir) {
+        OutputStream out = null;
+        InputStream in = null;
         try {
             String fileName = logfile.getName() + ".gz";
 
-            GZIPOutputStream out = new GZIPOutputStream(
+            out = new GZIPOutputStream(
                     new FileOutputStream(new File(logDir, fileName)));
-            FileInputStream in = new FileInputStream(logfile);
+            in = new FileInputStream(logfile);
             int len;
-            while ((len = in.read()) > 0) {
-                out.write(len);
+            byte[] buffer = new byte[BUFFER_SIZE];
+            while ((len = in.read(buffer)) > 0) {
+                out.write(buffer, 0 ,len);
             }
-            in.close();
             out.flush();
-            out.close();
             logfile.delete();
         } catch (IOException e) {
-            e.printStackTrace();
+            LOG.warn("could not gzip " + logfile.getName() + ": " +e.getMessage(), e);
+        } finally {
+            IO.close(out);
+            IO.close(in);
         }
     }
 
