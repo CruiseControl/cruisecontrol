dbadc864d09f800b8285048a3d83e4898e7f2cdf
Dan Rollo <bhamail@users.sourceforge.net>
2007-04-05 15:42:55 +0000

    fix unit test to run in headless environment
    git-svn-id: https://cruisecontrol.svn.sourceforge.net/svnroot/cruisecontrol/trunk/cruisecontrol@3007 878de816-9c12-0410-b5fe-b34e1928e20d

--- contrib/distributed/test/net/sourceforge/cruisecontrol/distributed/util/BuildAgentUtilityTest.java
+++ contrib/distributed/test/net/sourceforge/cruisecontrol/distributed/util/BuildAgentUtilityTest.java
@@ -4,8 +4,10 @@ import junit.framework.TestCase;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.awt.GraphicsEnvironment;
 
 import net.sourceforge.cruisecontrol.builders.DistributedMasterBuilderTest;
+import org.apache.log4j.Logger;
 
 /**
  * @author Dan Rollo
@@ -13,6 +15,8 @@ import net.sourceforge.cruisecontrol.builders.DistributedMasterBuilderTest;
  * Time: 9:23:58 PM
  */
 public class BuildAgentUtilityTest extends TestCase {
+
+    private static final Logger LOG = Logger.getLogger(BuildAgentUtilityTest.class);
 
     private static final class MockUI extends BuildAgentUtility.UI {
         private MockUI() {
@@ -24,16 +28,29 @@ public class BuildAgentUtilityTest extends TestCase {
 
     protected void setUp() throws Exception {
         DistributedMasterBuilderTest.setupInsecurePolicy();
-        mockUI = new MockUI();
+
+        if (GraphicsEnvironment.isHeadless()) {
+            LOG.warn("WARNING: DETECTED HEADLESS ENVIRONMENT. Skipping test: "
+                    + getClass().getName() + ".setUp() MockUI creation");
+        } else {
+            mockUI = new MockUI();
+        }
     }
     protected void tearDown() throws Exception {
-        // clear all agent util prefs        
-        mockUI.getPrefsRoot().removeNode();
-        mockUI.getPrefsRoot().flush();
+        if (mockUI != null) {
+            // clear all agent util prefs
+            mockUI.getPrefsRoot().removeNode();
+            mockUI.getPrefsRoot().flush();
+        }
     }
 
 
     public void testDiscoveryInstanceReuse() throws Exception {
+        if (GraphicsEnvironment.isHeadless()) {
+            LOG.warn("WARNING: DETECTED HEADLESS ENVIRONMENT. Skipping test: "
+                    + getClass().getName() + ".testDiscoveryInstanceReuse()");
+            return;
+        }
 
         final BuildAgentUtility buildAgentUtility = new BuildAgentUtility(mockUI);
         final List agents = new ArrayList();
