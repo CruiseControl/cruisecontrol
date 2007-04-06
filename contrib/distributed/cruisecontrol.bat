c622013b6d88c029ababaa81eefb485d1782732e
Dan Rollo <bhamail@users.sourceforge.net>
2007-04-06 15:58:41 +0000

    add missing CC_OPTS to bat script
    git-svn-id: https://cruisecontrol.svn.sourceforge.net/svnroot/cruisecontrol/trunk/cruisecontrol@3012 878de816-9c12-0410-b5fe-b34e1928e20d

--- contrib/distributed/cruisecontrol.bat
+++ contrib/distributed/cruisecontrol.bat
@@ -72,7 +72,7 @@ set CCDIST_CORE=%CCDIST%\dist\core\
 set CCDIST_JINICORE=%CCDIST%\jini-core\
 set CCDIST_CONF=%CCDIST%\conf
 
-set EXEC=%JAVA_PATH% -Djavax.management.builder.initial=mx4j.server.MX4JMBeanServerBuilder -Djava.security.policy=%CCDIST_CONF%\insecure.policy -Dcc.library.dir=%LIBDIR% -Dcc.dist.dir=%DISTDIR% -jar %DISTDIR%\cruisecontrol-launcher.jar -lib "%JAVA_HOME%\lib\tools.jar" -lib %CCDIST_BUILDER%;%CCDIST_CORE%;%CCDIST_JINICORE%;%CCDIST_CONF% %*
+set EXEC=%JAVA_PATH% %CC_OPTS% -Djavax.management.builder.initial=mx4j.server.MX4JMBeanServerBuilder -Djava.security.policy=%CCDIST_CONF%\insecure.policy -Dcc.library.dir=%LIBDIR% -Dcc.dist.dir=%DISTDIR% -jar %DISTDIR%\cruisecontrol-launcher.jar -lib "%JAVA_HOME%\lib\tools.jar" -lib %CCDIST_BUILDER%;%CCDIST_CORE%;%CCDIST_JINICORE%;%CCDIST_CONF% %*
 echo %EXEC%
 %EXEC%
 
