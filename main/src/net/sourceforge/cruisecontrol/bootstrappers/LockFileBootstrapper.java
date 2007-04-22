f7b20a5bc3baa99d58b5f9c72fde5f41820f39f2
Jeffrey Fredrick <jfredrick@users.sourceforge.net>
2007-04-22 04:19:58 +0000

    LockFileBootstrapper/LockFileListener: Fix bug where projects with multiple locks could leave dangling files (CC-568).
    git-svn-id: https://cruisecontrol.svn.sourceforge.net/svnroot/cruisecontrol/trunk/cruisecontrol@3050 878de816-9c12-0410-b5fe-b34e1928e20d

--- main/src/net/sourceforge/cruisecontrol/bootstrappers/LockFileBootstrapper.java
+++ main/src/net/sourceforge/cruisecontrol/bootstrappers/LockFileBootstrapper.java
@@ -3,23 +3,33 @@ package net.sourceforge.cruisecontrol.bootstrappers;
 import java.io.File;
 import java.io.IOException;
 
-import org.apache.log4j.Logger;
-
 import net.sourceforge.cruisecontrol.Bootstrapper;
 import net.sourceforge.cruisecontrol.CruiseControlException;
+import net.sourceforge.cruisecontrol.util.Util;
 import net.sourceforge.cruisecontrol.util.ValidationHelper;
+
+import org.apache.log4j.Logger;
 
 public class LockFileBootstrapper implements Bootstrapper {
 
     private String path;
+    private String projectName;
     private static final Logger LOG = Logger.getLogger(LockFileBootstrapper.class);
 
     public void bootstrap() throws CruiseControlException {
         File lock = new File(path);
         try {
             if (!lock.createNewFile()) {
-                throw new CruiseControlException("Lock file [" + path + "] already exists, aborting build attempt.");
+                String projectNameInFile = Util.readFileToString(lock);
+                if (!projectName.equalsIgnoreCase(projectNameInFile)) {
+                    String message = "Lock file [" + path + "] already exists from project " + projectNameInFile
+                            + " , aborting build attempt.";
+                    throw new CruiseControlException(message);
+                } else {
+                    LOG.debug("Lock file [" + path + "] already exists but project names match");
+                }
             } else {
+                Util.writeStringToFile(projectName, lock);
                 LOG.debug("Created lock file [" + path + "]");
                 lock.deleteOnExit();
             }
@@ -30,9 +40,14 @@ public class LockFileBootstrapper implements Bootstrapper {
 
     public void validate() throws CruiseControlException {
         ValidationHelper.assertIsSet(path, "lockfile", LockFileBootstrapper.class);
+        ValidationHelper.assertIsSet(projectName, "projectName", LockFileBootstrapper.class);
     }
 
     public void setLockFile(String path) {
         this.path = path;
+    }
+
+    public void setProjectName(String name) {
+        projectName = name;
     }
 }
--- main/test/net/sourceforge/cruisecontrol/bootstrappers/LockFileBootstrapperTest.java
+++ main/test/net/sourceforge/cruisecontrol/bootstrappers/LockFileBootstrapperTest.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import junit.framework.TestCase;
 import net.sourceforge.cruisecontrol.CruiseControlException;
 import net.sourceforge.cruisecontrol.testutil.TestUtil.FilesToDelete;
+import net.sourceforge.cruisecontrol.util.Util;
 
 public class LockFileBootstrapperTest extends TestCase {
 
@@ -28,25 +29,57 @@ public class LockFileBootstrapperTest extends TestCase {
         assertFalse(lock.exists());
         
         bootstrapper.setLockFile(lock.getAbsolutePath());
+        bootstrapper.setProjectName("project.name");
         
         bootstrapper.bootstrap();
         assertTrue(lock.exists());
+        assertProjectName("project.name", lock);
+        
+        bootstrapper.bootstrap();
+
+        bootstrapper.setProjectName("different.name");
         
         try {
             bootstrapper.bootstrap();
-            fail("should throw exception when lock already exists");
+            fail("should throw exception when lock already exists with different name");
         } catch (CruiseControlException expected) {            
         }
     }
     
-    public void testValidate() throws CruiseControlException {
+    private void assertProjectName(String expected, File lock) throws IOException {
+        String actual = Util.readFileToString(lock);
+        assertEquals("project name in file didn't match", expected, actual);
+    }
+
+    public void testValidateShouldThrowExceptionWhenRequiredAttributesNotSet() throws CruiseControlException {
+        try {
+            bootstrapper.validate();
+            fail("should throw exception when lock file and project name not set");
+        } catch (CruiseControlException expected) {
+        }
+    }
+    
+    public void testValidateShouldThrowExceptionWhenProjectNameNotSet() {
+        bootstrapper.setLockFile("delete.me");
+        try {
+            bootstrapper.validate();
+            fail("should throw exception when project name not set");
+        } catch (CruiseControlException expected) {
+        }
+    }
+    
+    public void testValidateShouldThrowExceptionWhenLockFileNotSet() {
+        bootstrapper.setProjectName("project.name");
         try {
             bootstrapper.validate();
             fail("should throw exception when lock file not set");
-        } catch (CruiseControlException expected) {
+        } catch (CruiseControlException e) {
         }
-        
+    }
+    
+    public void testValidateShouldntThrowExceptionWhenRequiredAttributesAreSet() throws CruiseControlException {
         bootstrapper.setLockFile("delete.me");
+        bootstrapper.setProjectName("project.name");
         bootstrapper.validate();
     }
     
