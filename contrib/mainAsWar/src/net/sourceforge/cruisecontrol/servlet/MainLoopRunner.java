723f18b4df63dbb826492d47cc84cd47fe25746f
Dan Rollo <bhamail@users.sourceforge.net>
2007-04-24 02:31:51 +0000

    Add ability to set the rmi port from the "cruisecontrol-main" web application (CC-637)
    git-svn-id: https://cruisecontrol.svn.sourceforge.net/svnroot/cruisecontrol/trunk/cruisecontrol@3058 878de816-9c12-0410-b5fe-b34e1928e20d

--- contrib/mainAsWar/src/net/sourceforge/cruisecontrol/servlet/MainLoopRunner.java
+++ contrib/mainAsWar/src/net/sourceforge/cruisecontrol/servlet/MainLoopRunner.java
@@ -5,13 +5,15 @@ import net.sourceforge.cruisecontrol.Main;
 public class MainLoopRunner implements Runnable {
 
     private Integer jmxport;
+    private Integer rmiport;
 
-    public MainLoopRunner(Integer jmxport) {
+    public MainLoopRunner(Integer jmxport, Integer rmiport) {
         this.jmxport = jmxport;
+        this.rmiport = rmiport;
     }
 
     public void run() {
-        String[] args = {"-jmxport", jmxport.toString()};
+        String[] args = {"-jmxport", jmxport.toString(), "-rmiport", rmiport.toString()};
         new Main().start(args);
     }
 
