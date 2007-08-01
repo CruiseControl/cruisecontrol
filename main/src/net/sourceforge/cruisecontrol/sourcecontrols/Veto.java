5eeb7b5a8edc90bf89e5f2b088ee02c388139f79
Paul Julius <pauljulius@users.sourceforge.net>
2007-08-01 08:53:11 +0000

    Improvement to provide detail in log as to what changes caused the veto (CC-676). Patch by Barrett Nuzum.
    git-svn-id: https://cruisecontrol.svn.sourceforge.net/svnroot/cruisecontrol/trunk/cruisecontrol@3170 878de816-9c12-0410-b5fe-b34e1928e20d

--- main/src/net/sourceforge/cruisecontrol/sourcecontrols/Veto.java
+++ main/src/net/sourceforge/cruisecontrol/sourcecontrols/Veto.java
@@ -1,39 +1,47 @@
 package net.sourceforge.cruisecontrol.sourcecontrols;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
+
 import net.sourceforge.cruisecontrol.CruiseControlException;
 import net.sourceforge.cruisecontrol.Modification;
 import net.sourceforge.cruisecontrol.SourceControl;
 
 public class Veto implements SourceControl {
+    /**
+     * enable logging for this class
+     */
+    private static Logger log = Logger.getLogger(Veto.class);
 
     private Triggers triggers;
+
     private BuildStatus buildStatus;
 
     public List getModifications(Date lastBuild, Date now) {
-        
+
         List triggerMods = triggers.getModifications(lastBuild, now);
         if (triggerMods.isEmpty()) {
             return Collections.EMPTY_LIST;
         }
-        
+
         List buildStatusMods = buildStatus.getModifications(lastBuild, now);
         if (buildStatusMods.isEmpty()) {
             throw new OutOfDateException("trigger changes with no buildstatus changes");
         }
-        
+
         Modification latestBuildStatusMod = getLatestModification(buildStatusMods);
         Modification latestTriggerMod = getLatestModification(triggerMods);
-        
-        if (latestBuildStatusMod.modifiedTime.before(latestTriggerMod.modifiedTime)) {
+
+        if (!getNewerModifications(triggerMods, latestBuildStatusMod).isEmpty()) {
             throw new OutOfDateException("buildstatus out of date compared to trigger changes");
         }
-        
+
         return Collections.EMPTY_LIST;
     }
 
@@ -46,6 +54,19 @@ public class Veto implements SourceControl {
             }
         }
         return latest;
+    }
+
+    private List getNewerModifications(List mods, Modification buildStatusMod) {
+        List newerMods = new ArrayList();
+        log.info("Comparing all trigger mods against buildStatusMod with date [" + buildStatusMod.modifiedTime + "]");
+        for (Iterator iter = mods.iterator(); iter.hasNext();) {
+            Modification mod = (Modification) iter.next();
+            if (mod.modifiedTime.after(buildStatusMod.modifiedTime)) {
+                newerMods.add(mod);
+                log.info("Newer file : " + mod.getFullPath() + " at [" + mod.modifiedTime + "]");
+            }
+        }
+        return newerMods;
     }
 
     public Map getProperties() {
@@ -89,5 +110,5 @@ public class Veto implements SourceControl {
             super(string);
         }
     }
-    
+
 }
