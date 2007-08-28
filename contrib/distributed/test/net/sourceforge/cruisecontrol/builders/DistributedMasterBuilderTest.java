99d85fd2f30ff28c0c75f4bf820efc44790c9d59
Dan Rollo <bhamail@users.sourceforge.net>
2007-08-28 17:42:11 +0000

    increase unit test Agent discovery timeout

--- contrib/distributed/test/net/sourceforge/cruisecontrol/builders/DistributedMasterBuilderTest.java
+++ contrib/distributed/test/net/sourceforge/cruisecontrol/builders/DistributedMasterBuilderTest.java
@@ -586,7 +586,7 @@ public class DistributedMasterBuilderTest extends TestCase {
         try {
             synchronized (agent) {
                 if (!BuildAgentTest.isServiceIDAssigned(agent)) {
-                    agent.wait(10 * 1000);
+                    agent.wait(20 * 1000);
                 }
             }
         } finally {
