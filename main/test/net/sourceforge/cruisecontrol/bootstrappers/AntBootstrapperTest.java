d00471fee48eaeeff5dce7f2b15ba274f5ecc4c4
Jeffrey Fredrick <jfredrick@users.sourceforge.net>
2007-08-29 19:53:20 +0000

    remove dependency on file system
    git-svn-id: https://cruisecontrol.svn.sourceforge.net/svnroot/cruisecontrol/trunk/cruisecontrol@3346 878de816-9c12-0410-b5fe-b34e1928e20d

--- main/test/net/sourceforge/cruisecontrol/bootstrappers/AntBootstrapperTest.java
+++ main/test/net/sourceforge/cruisecontrol/bootstrappers/AntBootstrapperTest.java
@@ -1,9 +1,20 @@
 package net.sourceforge.cruisecontrol.bootstrappers;
 
+import java.io.File;
+
 import junit.framework.TestCase;
 import net.sourceforge.cruisecontrol.CruiseControlException;
+import net.sourceforge.cruisecontrol.testutil.TestUtil.FilesToDelete;
+import net.sourceforge.cruisecontrol.util.IO;
 
 public class AntBootstrapperTest extends TestCase {
+    
+    private final FilesToDelete filesToDelete = new FilesToDelete();
+
+    protected void tearDown() throws Exception {
+        filesToDelete.delete();
+    }
+    
     public void testValidate() {
         AntBootstrapper bootstrapper = new AntBootstrapper();
 
@@ -32,11 +43,22 @@ public class AntBootstrapperTest extends TestCase {
 
     public void testBootstrap() throws Exception {
         AntBootstrapper bootstrapper = new AntBootstrapper();
+        
+        File buildFile = File.createTempFile("testbuild", ".xml");
+        writeBuildFile(buildFile);
 
-        bootstrapper.setBuildFile("testbuild.xml");
+        bootstrapper.setBuildFile(buildFile.getAbsolutePath());
         bootstrapper.setTempFile("notLog.xml");
         bootstrapper.setTarget("init");
         bootstrapper.validate();
         bootstrapper.bootstrap();
+    }
+
+    private void writeBuildFile(File buildFile) throws CruiseControlException {
+        StringBuffer contents = new StringBuffer();
+        contents.append("<project name='testbuild' default='init'>");
+        contents.append("<target name='init'><echo message='called testbulid.xml init target'/></target>");
+        contents.append("</project>");
+        IO.write(buildFile, contents.toString());
     }
 }
