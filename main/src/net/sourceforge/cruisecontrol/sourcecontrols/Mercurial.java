18986d211e5c9371d411fdf5df019d9075988d1d
Paul Julius <pauljulius@users.sourceforge.net>
2007-09-02 08:50:02 +0000

    Sourcecontrol & Bootstrapper implementation for Mercurial (hg) http://www.selenic.com/mercurial/wiki/ (CC-638). Submitted by Jerome Lacoste.
    git-svn-id: https://cruisecontrol.svn.sourceforge.net/svnroot/cruisecontrol/trunk/cruisecontrol@3360 878de816-9c12-0410-b5fe-b34e1928e20d

--- main/src/net/sourceforge/cruisecontrol/sourcecontrols/Mercurial.java
+++ main/src/net/sourceforge/cruisecontrol/sourcecontrols/Mercurial.java
@@ -1,6 +1,6 @@
 /********************************************************************************
  * CruiseControl, a Continuous Integration Toolkit
- * Copyright (c) 2001, ThoughtWorks, Inc.
+ * Copyright (c) 2007, ThoughtWorks, Inc.
  * 200 E. Randolph, 25th Floor
  * Chicago, IL 60601 USA
  * All rights reserved.
@@ -56,9 +56,7 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
-import java.text.DateFormat;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
@@ -74,7 +72,7 @@ import java.util.regex.Pattern;
  * This class implements the SourceControl methods for a Mercurial repository.
  *
  * @author <a href="jerome@coffeebreaks.org">Jerome Lacoste</a>
- * @see <a href="http://www.selenic.com/mercurial/wiki//">Mercurial web site</a>
+ * @see <a href="http://www.selenic.com/mercurial">Mercurial web site</a>
  */
 public class Mercurial implements SourceControl {
 
@@ -85,8 +83,8 @@ public class Mercurial implements SourceControl {
     /**
      * Configuration parameters
      */
-    private String repositoryLocation;
     private String localWorkingCopy;
+
     static final String INCOMING_XML_TEMPLATE = "<hgChange>\n\t<author>{author}</author>\n\t<rev>{rev}</rev>\n\t"
             + "<node>{node}</node>\n\t<description>{desc|escape}</description>\n\t<date>{date|hgdate}</date>\n\t"
             + "<addedFiles>{file_adds}</addedFiles>\n\t<removedFiles>{file_dels}</removedFiles>\n\t"
@@ -105,20 +103,10 @@ public class Mercurial implements SourceControl {
     }
 
     /**
-     * Sets the repository location to use when making calls to Mercurial.
-     *
-     * @param repositoryLocation String indicating the url to the Mercurial
-     *                           repository on which to find the log history.
-     */
-    public void setRepositoryLocation(String repositoryLocation) {
-        this.repositoryLocation = repositoryLocation;
-    }
-
-    /**
-     * Sets the local working copy to use when making calls to Mercurial.
+     * Sets the local working copy to use when making calls to mercurial.
      *
      * @param localWorkingCopy String indicating the relative or absolute path
-     *                         to the local working copy of the Mercurial
+     *                         to the local working copy of the mercurial
      *                         repository of which to find the log history.
      */
     public void setLocalWorkingCopy(String localWorkingCopy) {
@@ -135,9 +123,6 @@ public class Mercurial implements SourceControl {
      *          null
      */
     public void validate() throws CruiseControlException {
-        ValidationHelper.assertTrue(repositoryLocation != null || localWorkingCopy != null,
-                "At least 'repositoryLocation'or 'localWorkingCopy' is a required attribute on the Mercurial task ");
-
         if (localWorkingCopy != null) {
             File workingDir = new File(localWorkingCopy);
             ValidationHelper.assertTrue(workingDir.exists() && workingDir.isDirectory(),
@@ -153,18 +138,36 @@ public class Mercurial implements SourceControl {
      * @return the list of modifications, or an empty list if we failed
      *         to retrieve the changes.
      */
-    public List getModifications(Date lastBuild, Date now) {
+    public List getModifications(Date lastBuildDate, Date now) {
+        String version = getMercurialVersion();
+        LOG.info("Using Mercurial: '" + version + "'");
         Commandline command = null;
+        List modifications = Collections.EMPTY_LIST;
         try {
             command = buildHistoryCommand();
 
-            return execHistoryCommand(command);
+            modifications = execHistoryCommand(command);
+
+            // TODO: should we filter out the results ?
+            // modifications = filterModifications(modifications, lastBuildDate, now);
+
         } catch (Exception e) {
-            if (command != null) {
-                LOG.error("Error executing mercurial log command " + command, e);
-            }
+            LOG.error("Error executing mercurial history command " + command, e);
         }
-        return Collections.EMPTY_LIST;
+        fillPropertiesIfNeeded(modifications);
+        return modifications;
+    }
+
+    private String getMercurialVersion() {
+        Commandline command = null;
+        try {
+            command = buildVersionCommand();
+
+            return execVersionCommand(command);
+        } catch (Exception e) {
+            LOG.error("Error executing mercurial version command " + command, e);
+            return "version unresolved...";
+        }
     }
 
     /**
@@ -189,21 +192,10 @@ public class Mercurial implements SourceControl {
         }
 
         command.createArgument("incoming");
-        // required to get file_adds and file_dels to work in this version of mercurial 0.9.5-SNAPSHOT
+        // --debug required to get file_adds and file_dels to work in this version of mercurial (0.9.4+20070830)
         command.createArgument("--debug");
         command.createArgument("--template");
         command.createArgument(INCOMING_XML_TEMPLATE);
-
-        // command.createArgument("-r");
-        // command.createArgument(lastBuildRevision + ":qtip");
-
-        // if (path != null) {
-        //    command.createArgument(path);
-        // } else if (repositoryLocation != null) {
-        //    command.createArgument(repositoryLocation);
-        // }
-
-        LOG.debug("Executing command: " + command);
 
         return command;
     }
@@ -211,17 +203,73 @@ public class Mercurial implements SourceControl {
     private static List execHistoryCommand(Commandline command)
             throws InterruptedException, IOException, ParseException, JDOMException {
 
+        LOG.debug("Executing command: " + command);
+
         Process p = command.execute();
 
         Thread stderr = logErrorStream(p);
-        InputStream svnStream = p.getInputStream();
-        List modifications = parseStream(svnStream);
+        InputStream commandOutputStream = p.getInputStream();
+        List modifications = parseStream(commandOutputStream);
 
         p.waitFor();
         stderr.join();
         IO.close(p);
 
         return modifications;
+    }
+
+    /**
+     * Generates the command line for the hg version command.
+     * <p/>
+     * 'hg version'
+     *
+     * @return version command
+     * @throws net.sourceforge.cruisecontrol.CruiseControlException
+     *          exception
+     */
+    Commandline buildVersionCommand()
+            throws CruiseControlException {
+
+        Commandline command = new Commandline();
+        command.setExecutable("hg");
+
+        if (localWorkingCopy != null) {
+            command.setWorkingDirectory(localWorkingCopy);
+        }
+
+        command.createArgument("version");
+
+        return command;
+    }
+
+    private String execVersionCommand(Commandline command) throws CruiseControlException {
+
+        LOG.debug("Executing command: " + command);
+
+        try {
+            Process p = command.execute();
+
+            Thread stderr = logErrorStream(p);
+            InputStream svnStream = p.getInputStream();
+            String revision = parseVersionStream(svnStream);
+
+            p.waitFor();
+            stderr.join();
+            IO.close(p);
+
+            return revision;
+        } catch (IOException e) {
+            throw new CruiseControlException(e);
+        } catch (ParseException e) {
+            throw new CruiseControlException(e);
+        } catch (InterruptedException e) {
+            throw new CruiseControlException(e);
+        }
+    }
+
+    static String parseVersionStream(InputStream svnStream) throws ParseException, IOException {
+        InputStreamReader reader = new InputStreamReader(svnStream, "UTF-8");
+        return HgVersionParser.parse(reader);
     }
 
     private static Thread logErrorStream(Process p) {
@@ -252,9 +300,29 @@ public class Mercurial implements SourceControl {
         }
     }
 
+    void fillPropertiesIfNeeded(List modifications) {
+        if (!modifications.isEmpty()) {
+            properties.modificationFound();
+
+            String maxRevision = "";
+            for (int i = 0; i < modifications.size(); i++) {
+                Modification modification = (Modification) modifications.get(i);
+                Modification.ModifiedFile file = (Modification.ModifiedFile) modification.files.get(0);
+                if (i == modifications.size() - 1) {
+                    maxRevision = modification.revision;
+                }
+                if (file.action.equals("deleted")) {
+                    properties.deletionFound();
+                }
+            }
+            properties.put("hgrevision", maxRevision);
+        }
+    }
+    /*
     public static DateFormat getOutDateFormatter() {
         return Iso8601DateParser.ISO8601_DATE_PARSER;
     }
+    */
 
     static final class HgLogParser {
 
@@ -344,13 +412,15 @@ public class Mercurial implements SourceControl {
          * @return converted date
          * @throws java.text.ParseException if specified date doesn't match the expected format
          */
-        static Date convertIso8601Date(String date) throws ParseException {
+        /*
+        private static Date convertIso8601Date(String date) throws ParseException {
             try {
                 return Iso8601DateParser.parse(date);
             } catch (IllegalArgumentException e) {
                 throw new ParseException(e.getMessage(), 0);
             }
         }
+        */
 
         /**
          * Converts the specified SVN date string into a Date.
@@ -368,34 +438,27 @@ public class Mercurial implements SourceControl {
                 throw parseException;
             }
         }
-
-        static List filterModifications(Modification[] modifications, Date lastBuild) {
-            List filtered = new ArrayList();
-            for (int i = 0; i < modifications.length; i++) {
-                Modification modification = modifications[i];
-                if (modification.modifiedTime.getTime() > lastBuild.getTime()) {
-                    filtered.add(modification);
-                }
-            }
-            return filtered;
-        }
     }
 
 
+    // not used as Mercurial don't display the seconds. Too bad. Will this be fixed in a further revision ?
+    // would be nice as is a more readable thatn the hgdate format... 
     // 2007-08-29 21:44 +0200
-    static final class Iso8601DateParser {
+    /*
+    private static final class Iso8601DateParser {
         private Iso8601DateParser() {
         }
 
-        public static final SimpleDateFormat ISO8601_DATE_PARSER = new SimpleDateFormat("yyyy-MM-d HH:mm Z");
+        private static final SimpleDateFormat ISO8601_DATE_PARSER = new SimpleDateFormat("yyyy-MM-d HH:mm Z");
 
         private static Date parse(String date) throws ParseException {
             return ISO8601_DATE_PARSER.parse(date);
         }
     }
+    */
 
     // 1188223879 -7200
-    static final class HgDateParser {
+    private static final class HgDateParser {
         private HgDateParser() {
         }
 
@@ -403,7 +466,7 @@ public class Mercurial implements SourceControl {
             Pattern p = Pattern.compile("([0-9]*) (.*)");
             Matcher m = p.matcher(date);
             if (!m.matches()) {
-                throw new ParseException("No match of " + date, 0);
+                throw new ParseException("HgDateParser: no match of " + date, 0);
             }
             Calendar c = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
             c.setTimeInMillis(Long.parseLong(m.group(1)) * 1000);
@@ -411,14 +474,23 @@ public class Mercurial implements SourceControl {
         }
     }
 
-    static final class SVNInfoXMLParser {
-        private SVNInfoXMLParser() {
+    static final class HgVersionParser {
+        private HgVersionParser() {
         }
 
-        public static String parse(Reader reader) throws JDOMException, IOException {
-            SAXBuilder builder = new SAXBuilder(false);
-            Document document = builder.build(reader);
-            return document.getRootElement().getChild("entry").getAttribute("revision").getValue();
+        public static String parse(Reader reader) throws ParseException, IOException {
+            BufferedReader myReader = new BufferedReader(reader);
+            String versionLine = myReader.readLine();
+            if (versionLine == null) {
+                throw new IllegalStateException("hg version returned nothing");
+            }
+
+            Pattern p = Pattern.compile("Mercurial Distributed SCM \\((.*)\\)");
+            Matcher m = p.matcher(versionLine);
+            if (!m.matches()) {
+                throw new ParseException("HgVersionParser: no match of " + versionLine, 0);
+            }
+            return m.group(1);
         }
     }
 }
