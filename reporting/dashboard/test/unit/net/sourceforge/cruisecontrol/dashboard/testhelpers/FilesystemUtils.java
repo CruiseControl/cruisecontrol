ee5a38f60f10e14f9a267aabd08bac5b6709b2a2
Dan Rollo <bhamail@users.sourceforge.net>
2007-10-16 03:15:28 +0000

    attempt another workaround for intermitent w2k error: java.io.IOException: Access is denied
    git-svn-id: https://cruisecontrol.svn.sourceforge.net/svnroot/cruisecontrol/trunk/cruisecontrol@3612 878de816-9c12-0410-b5fe-b34e1928e20d

--- reporting/dashboard/test/unit/net/sourceforge/cruisecontrol/dashboard/testhelpers/FilesystemUtils.java
+++ reporting/dashboard/test/unit/net/sourceforge/cruisecontrol/dashboard/testhelpers/FilesystemUtils.java
@@ -86,33 +86,30 @@ public final class FilesystemUtils {
         // java.io.IOException: Access is denied
         // at java.io.WinNTFileSystem.createFileExclusively(Native Method)
         // at java.io.File.createNewFile(File.java:883)
-        // at net.sourceforge.cruisecontrol.dashboard.testhelpers.FilesystemUtils.createFile(FilesystemUtils.java:99)
+        // at net.sourceforge.cruisecontrol.dashboard.testhelpers.FilesystemUtils.createFile(FilesystemUtils.java:98)
         // ...
         // see: http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6198547
         // see: http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6325169
-        if (!file.exists()) {
-            boolean created = false;
-            int count = 0;
-            IOException lastIOException = null;
-            do {
-                try {
-                    created = file.createNewFile();
-                } catch (IOException e) {
-                    lastIOException = e;
-                    Thread.yield();
-                }
-                count++;
-            } while (!created && count < 3);
+        int count = 0;
+        IOException lastIOException = null;
+        do {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                lastIOException = e;
+                Thread.yield();
+            }
+            count++;
+        } while (!file.exists() && count < 3);
 
-            if (!file.exists()) {
-                if (lastIOException != null) {
-                    throw lastIOException;
-                } else {
-                    throw new RuntimeException("Error creating file: " + file.getAbsolutePath());
-                }
+        if (!file.exists()) {
+            if (lastIOException != null) {
+                throw lastIOException;
+            } else {
+                throw new RuntimeException("Error creating file: " + file.getAbsolutePath());
             }
         }
-        
+
         file.deleteOnExit();
         return file;
     }
