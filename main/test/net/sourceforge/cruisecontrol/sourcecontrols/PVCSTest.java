c8be0ceec06ad96358194f1114e59ab6869ca265
Jason Yip <jchyip@users.sourceforge.net>
2007-10-28 05:23:16 +0000

    PVCS changes Add support to check for changes against promotion group (CC-717); make thread-safe (CC-583); fix quoting (CC-655).  Patch by Shane Genschaw.

--- main/test/net/sourceforge/cruisecontrol/sourcecontrols/PVCSTest.java
+++ main/test/net/sourceforge/cruisecontrol/sourcecontrols/PVCSTest.java
@@ -4,11 +4,11 @@
  * 200 E. Randolph, 25th Floor
  * Chicago, IL 60601 USA
  * All rights reserved.
- *
+ * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- *
+ * 
  *     + Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
  *
@@ -36,10 +36,13 @@
  ********************************************************************************/
 package net.sourceforge.cruisecontrol.sourcecontrols;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -49,9 +52,14 @@ import java.util.Map;
 import junit.framework.TestCase;
 import net.sourceforge.cruisecontrol.CruiseControlException;
 import net.sourceforge.cruisecontrol.Modification;
+import net.sourceforge.cruisecontrol.sourcecontrols.PVCS.PvcsStreamConsumer;
 import net.sourceforge.cruisecontrol.util.Commandline;
 
+import org.apache.log4j.Logger;
+
 public class PVCSTest extends TestCase {
+
+    private static final Logger LOG = Logger.getLogger(PVCSTest.class);
 
     private PVCS pvcs;
 
@@ -84,16 +92,11 @@ public class PVCSTest extends TestCase {
 
     public void testGetExecutable() {
         String testExe = "testexe";
-        assertEquals(
-            "Wrong pvcs bin setting w/out bin set.",
-            testExe,
-            pvcs.getExecutable(testExe));
+        assertEquals("Wrong pvcs bin setting w/out bin set.", testExe, pvcs.getExecutable(testExe));
 
         pvcs.setPvcsbin("mybindir");
-        assertEquals(
-            "Wrong pvcs bin setting w/ bin set.",
-            "mybindir" + File.separator + testExe,
-            pvcs.getExecutable(testExe));
+        assertEquals("Wrong pvcs bin setting w/ bin set.", "mybindir" + File.separator + testExe,
+                pvcs.getExecutable(testExe));
     }
 
     public void testBuildExecCommandWithVersionLabel() {
@@ -101,15 +104,12 @@ public class PVCSTest extends TestCase {
         pvcs.setPvcssubproject("/TestProject");
         pvcs.setPvcsversionlabel("Test Version Label");
 
-        Commandline ccCommand = pvcs.buildExecCommand("11/23/2004 8:00AM", "11/23/2004 1:00PM");
-        String expectedCommand = pvcs.getExecutable("pcli")
-                + " "
-                + "run -ns -q -xo\"vlog.txt\" -xe\"vlog.txt\" vlog "
-                + "-ds\"11/23/2004 8:00AM\" -de\"11/23/2004 1:00PM\" "
-                + "-pr\"C:/PVCS-Repos/TestProject/pvcs\" -v\"Test Version Label\" "
-                + "-z /TestProject";
+        Commandline ccCommand = pvcs.buildExecCommand("11/23/2004 08:00AM", "11/23/2004 01:00PM");
+        String expectedCommand = pvcs.getExecutable("pcli") + " " + "run -ns -q vlog "
+                + "\"-ds11/23/2004 08:00AM\" \"-de11/23/2004 01:00PM\" "
+                + "-prC:/PVCS-Repos/TestProject/pvcs \"-vTest Version Label\" " + "-z /TestProject";
 
-        assertEquals("Wrong PVCS command generated!", expectedCommand, ccCommand.toStringNoQuoting());
+        assertEquals("Wrong PVCS command generated!", expectedCommand, ccCommand.toString());
     }
 
     public void testBuildExecCommandWithNullVersionLabel() {
@@ -117,52 +117,48 @@ public class PVCSTest extends TestCase {
         pvcs.setPvcssubproject("/TestProject");
         pvcs.setPvcsversionlabel("");
 
-        Commandline ccCommand = pvcs.buildExecCommand("11/23/2004 8:00AM", "11/23/2004 1:00PM");
-        String expectedCommand = pvcs.getExecutable("pcli") + " "
-                + "run -ns -q -xo\"vlog.txt\" -xe\"vlog.txt\" vlog "
-                + "-ds\"11/23/2004 8:00AM\" -de\"11/23/2004 1:00PM\" "
-                + "-pr\"C:/PVCS-Repos/TestProject/pvcs\" " + "-z /TestProject";
-        assertEquals("Wrong PVCS command generated!", expectedCommand, ccCommand.toStringNoQuoting());
+        Commandline ccCommand = pvcs.buildExecCommand("11/23/2004 08:00AM", "11/23/2004 01:00PM");
+        String expectedCommand = pvcs.getExecutable("pcli") + " " + "run -ns -q vlog "
+                + "\"-ds11/23/2004 08:00AM\" \"-de11/23/2004 01:00PM\" "
+                + "-prC:/PVCS-Repos/TestProject/pvcs " + "-z /TestProject";
+        assertEquals("Wrong PVCS command generated!", expectedCommand, ccCommand.toString());
     }
 
     public void testBuildExecCommandWithoutVersionLabel() {
         pvcs.setPvcsproject("C:/PVCS-Repos/TestProject/pvcs");
         pvcs.setPvcssubproject("/TestProject");
 
-        Commandline ccCommand = pvcs.buildExecCommand("11/23/2004 8:00AM", "11/23/2004 1:00PM");
-        String expectedCommand = pvcs.getExecutable("pcli") + " "
-                + "run -ns -q -xo\"vlog.txt\" -xe\"vlog.txt\" vlog "
-                + "-ds\"11/23/2004 8:00AM\" -de\"11/23/2004 1:00PM\" "
-                + "-pr\"C:/PVCS-Repos/TestProject/pvcs\" " + "-z /TestProject";
+        Commandline ccCommand = pvcs.buildExecCommand("11/23/2004 08:00AM", "11/23/2004 01:00PM");
+        String expectedCommand = pvcs.getExecutable("pcli") + " " + "run -ns -q vlog "
+                + "\"-ds11/23/2004 08:00AM\" \"-de11/23/2004 01:00PM\" "
+                + "-prC:/PVCS-Repos/TestProject/pvcs " + "-z /TestProject";
 
-        assertEquals("Wrong PVCS command generated!", expectedCommand, ccCommand.toStringNoQuoting());
-  }
+        assertEquals("Wrong PVCS command generated!", expectedCommand, ccCommand.toString());
+    }
 
     public void testBuildExecCommandWithoutLoginId() {
         pvcs.setPvcsproject("C:/PVCS-Repos/TestProject/pvcs");
         pvcs.setPvcssubproject("/TestProject");
 
-        Commandline ccCommand = pvcs.buildExecCommand("11/23/2004 8:00AM", "11/23/2004 1:00PM");
-        String expectedCommand = pvcs.getExecutable("pcli") + " "
-                + "run -ns -q -xo\"vlog.txt\" -xe\"vlog.txt\" vlog "
-                + "-ds\"11/23/2004 8:00AM\" -de\"11/23/2004 1:00PM\" "
-                + "-pr\"C:/PVCS-Repos/TestProject/pvcs\" " + "-z /TestProject";
+        Commandline ccCommand = pvcs.buildExecCommand("11/23/2004 08:00AM", "11/23/2004 01:00PM");
+        String expectedCommand = pvcs.getExecutable("pcli") + " " + "run -ns -q vlog "
+                + "\"-ds11/23/2004 08:00AM\" \"-de11/23/2004 01:00PM\" "
+                + "-prC:/PVCS-Repos/TestProject/pvcs " + "-z /TestProject";
 
-        assertEquals("Wrong PVCS command generated!", expectedCommand, ccCommand.toStringNoQuoting());
+        assertEquals("Wrong PVCS command generated!", expectedCommand, ccCommand.toString());
     }
 
     public void testBuildExecCommandWithEmptyLoginId() {
-        pvcs.setPvcsproject("C:/PVCS-Repos/TestProject/pvcs");
+        pvcs.setPvcsproject("C:/PVCS-Repos/Test Project/pvcs");
         pvcs.setPvcssubproject("/TestProject");
         pvcs.setLoginid("");
 
-        Commandline ccCommand = pvcs.buildExecCommand("11/23/2004 8:00AM", "11/23/2004 1:00PM");
-        String expectedCommand = pvcs.getExecutable("pcli") + " "
-                + "run -ns -q -xo\"vlog.txt\" -xe\"vlog.txt\" vlog "
-                + "-ds\"11/23/2004 8:00AM\" -de\"11/23/2004 1:00PM\" "
-                + "-pr\"C:/PVCS-Repos/TestProject/pvcs\" " + "-z /TestProject";
+        Commandline ccCommand = pvcs.buildExecCommand("11/23/2004 08:00AM", "11/23/2004 01:00PM");
+        String expectedCommand = pvcs.getExecutable("pcli") + " " + "run -ns -q vlog "
+                + "\"-ds11/23/2004 08:00AM\" \"-de11/23/2004 01:00PM\" "
+                + "\"-prC:/PVCS-Repos/Test Project/pvcs\" " + "-z /TestProject";
 
-        assertEquals("Wrong PVCS command generated!", expectedCommand, ccCommand.toStringNoQuoting());
+        assertEquals("Wrong PVCS command generated!", expectedCommand, ccCommand.toString());
     }
 
     public void testBuildExecCommandWithLoginId() {
@@ -170,50 +166,67 @@ public class PVCSTest extends TestCase {
         pvcs.setPvcssubproject("/TestProject");
         pvcs.setLoginid("TestUser");
 
-        Commandline ccCommand = pvcs.buildExecCommand("11/23/2004 8:00AM", "11/23/2004 1:00PM");
-        String expectedCommand = pvcs.getExecutable("pcli") + " "
-                + "run -ns -q -xo\"vlog.txt\" -xe\"vlog.txt\" vlog "
-                + "-id\"TestUser\" -ds\"11/23/2004 8:00AM\" "
-                + "-de\"11/23/2004 1:00PM\" -pr\"C:/PVCS-Repos/TestProject/pvcs\" "
+        Commandline ccCommand = pvcs.buildExecCommand("11/23/2004 08:00AM", "11/23/2004 01:00PM");
+        String expectedCommand = pvcs.getExecutable("pcli") + " " + "run -ns -q vlog "
+                + "-idTestUser \"-ds11/23/2004 08:00AM\" "
+                + "\"-de11/23/2004 01:00PM\" -prC:/PVCS-Repos/TestProject/pvcs "
                 + "-z /TestProject";
 
-        assertEquals("Wrong PVCS command generated!", expectedCommand, ccCommand.toStringNoQuoting());
+        assertEquals("Wrong PVCS command generated!", expectedCommand, ccCommand.toString());
     }
 
-    public void testMakeModifications() throws URISyntaxException {
+    public void testPvcsStreamConsumer() throws URISyntaxException {
         Calendar cal = Calendar.getInstance();
         cal.set(2004, 11, 23);
         Date date = cal.getTime();
-        pvcs.setLastBuild(date);
-        pvcs.setPvcsproject("Services");
-        List mods = pvcs.makeModificationsList(new File(new URI(getClass().getResource("vlog.txt").toExternalForm())));
+        PvcsStreamConsumer consumer = new PvcsStreamConsumer(date, new SimpleDateFormat(
+                "MMM dd yyyy HH:mm:ss"), "Services", "-arc");
+
+        BufferedReader brIn;
+        try {
+            brIn = new BufferedReader(new FileReader(new File(new URI(getClass().getResource(
+                    "vlog.txt").toExternalForm()))));
+            String line;
+            while ((line = brIn.readLine()) != null) {
+                consumer.consumeLine(line);
+            }
+            brIn.close();
+        } catch (IOException e) {
+            LOG.error("Error in reading vlog file of PVCS modifications : ", e);
+        }
+
+        List mods = consumer.getModificationList();
         assertEquals(7, mods.size());
         Modification mod1 = (Modification) mods.get(0);
         assertEquals("Initial revision", mod1.comment);
         Modification mod2 = (Modification) mods.get(1);
-        assertEquals("Add code for " + System.getProperty("line.separator") + "Sections", mod2.comment);
+        assertEquals("Add code for " + System.getProperty("line.separator") + "Sections",
+                mod2.comment);
 
     }
-    
-    public void testProperty() {
+
+    public void testProperty() throws IOException, InterruptedException {
         pvcs = new PVCS() {
-
-            void executeCommandLine(String lastBuildDate, String nowDate) throws IOException, InterruptedException {
+            protected void executeCommandline(Commandline command, PvcsStreamConsumer consumer)
+                    throws IOException, InterruptedException {
+                // do nothing
             }
-
-            List makeModificationsList(File inputFile) {
+        };
+        Date lastBuild = new Date();
+        PvcsStreamConsumer consumer = new PvcsStreamConsumer(lastBuild, new SimpleDateFormat(
+                "MMM dd yyyy HH:mm:ss"), "C:/PVCS-Repos/TestProject/pvcs", "-arc") {
+            public List getModificationList() {
                 List mods = new ArrayList();
                 mods.add("modification");
                 return mods;
             }
-            
         };
-        
-        pvcs.getModifications(new Date(), new Date());
+        pvcs.getModifications(lastBuild, new Date(), consumer);
+        pvcs.getModifications(lastBuild, new Date());
         assertEquals(0, pvcs.getProperties().size());
-        
+
         pvcs.setProperty("property");
-        pvcs.getModifications(new Date(), new Date());
+        pvcs.getModifications(lastBuild, new Date(), consumer);
         Map properties = pvcs.getProperties();
         assertEquals(1, properties.size());
         assertTrue(properties.containsKey("property"));
