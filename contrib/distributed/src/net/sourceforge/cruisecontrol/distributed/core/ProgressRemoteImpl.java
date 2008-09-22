085e171e42ec85d46ad0d9b3e9d9d373f2131c64
Dan Rollo <bhamail@users.sourceforge.net>
2008-09-22 02:47:10 +0000

    implement remote versions of getLastUpdated() and getText() for Progress interface.

--- contrib/distributed/src/net/sourceforge/cruisecontrol/distributed/core/ProgressRemoteImpl.java
+++ contrib/distributed/src/net/sourceforge/cruisecontrol/distributed/core/ProgressRemoteImpl.java
@@ -3,6 +3,7 @@ package net.sourceforge.cruisecontrol.distributed.core;
 import net.sourceforge.cruisecontrol.Progress;
 
 import java.rmi.RemoteException;
+import java.util.Date;
 
 /**
  * Allow progress updates from distributed agents.
@@ -26,5 +27,13 @@ public class ProgressRemoteImpl implements ProgressRemote {
 
     public String getValueRemote() throws RemoteException {
         return  progress.getValue();
+    }
+
+    public Date getLastUpdatedRemote() throws RemoteException {
+        return progress.getLastUpdated();
+    }
+
+    public String getTextRemote() throws RemoteException {
+        return progress.getText();
     }
 }
