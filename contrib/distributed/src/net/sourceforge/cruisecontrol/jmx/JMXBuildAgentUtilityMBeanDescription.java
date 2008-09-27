3178a22ef0a1e3cce244d213fc42fd2b4bd897d4
Dan Rollo <bhamail@users.sourceforge.net>
2008-09-27 17:57:40 +0000

    correct JMXBuildAgentUtility descriptions.

--- contrib/distributed/src/net/sourceforge/cruisecontrol/jmx/JMXBuildAgentUtilityMBeanDescription.java
+++ contrib/distributed/src/net/sourceforge/cruisecontrol/jmx/JMXBuildAgentUtilityMBeanDescription.java
@@ -31,15 +31,14 @@ public class JMXBuildAgentUtilityMBeanDescription extends MBeanDescriptionAdapte
 
     static {
         ATTR_DESCRIPTIONS = new HashMap<String, String>();
-        ATTR_DESCRIPTIONS.put("getLookupServiceCount", "The number of Lookup Services (Registrars) found.");
-        ATTR_DESCRIPTIONS.put("getBuildAgents", "A big knarly string representation of all Build Agents found.");
-        ATTR_DESCRIPTIONS.put("getBuildAgentServiceIds", "Use the ServiceId (the part after '<hostname>: ') as the "
+        ATTR_DESCRIPTIONS.put("LookupServiceCount", "The number of Lookup Services (Registrars) found.");
+        ATTR_DESCRIPTIONS.put("BuildAgents", "A big knarly string representation of all Build Agents found.");
+        ATTR_DESCRIPTIONS.put("BuildAgentServiceIds", "Use the ServiceId (the part after '<hostname>: ') as the "
                 + "parameter value to kill() or restart() calls. The ServiceId uniquely identifies a Build Agent.");
-        final String descAfterBuildFinished = "If true, any invocation of kill or restart on a busy agent will wait "
-                + "until the currently running build finishes. If false, invocation of kill or restart will "
-                + "occur immediately, even if the agent is currently busy building a project.";
-        ATTR_DESCRIPTIONS.put("isKillOrRestartAfterBuildFinished", descAfterBuildFinished);
-        ATTR_DESCRIPTIONS.put("setKillOrRestartAfterBuildFinished", descAfterBuildFinished);
+        ATTR_DESCRIPTIONS.put("KillOrRestartAfterBuildFinished",
+                "If true, any invocation of kill or restart on a busy agent will wait until the currently running "
+                + "build finishes. If false, invocation of kill or restart will occur immediately, even if the agent "
+                + "is currently busy building a project.");
     }
 
     public String getOperationDescription(Method method) {
