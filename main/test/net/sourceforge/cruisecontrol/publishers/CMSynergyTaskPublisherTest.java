a57bb19d75011176bc0ed6e7312c2f90dab57840
Dan Rollo <bhamail@users.sourceforge.net>
2009-02-18 19:19:55 +0000

    researching CC-876, adding some unit tests
    git-svn-id: https://cruisecontrol.svn.sourceforge.net/svnroot/cruisecontrol/trunk/cruisecontrol@4301 878de816-9c12-0410-b5fe-b34e1928e20d

--- main/test/net/sourceforge/cruisecontrol/publishers/CMSynergyTaskPublisherTest.java
+++ main/test/net/sourceforge/cruisecontrol/publishers/CMSynergyTaskPublisherTest.java
@@ -11,6 +11,15 @@ import junit.framework.TestCase;
  */
 public class CMSynergyTaskPublisherTest extends TestCase {
 
+    public static void addElementWithContent(final Element parent,
+                                             final String elementName, final String elementContent) {
+
+        final Element elem = new Element(elementName);
+        elem.addContent(elementContent);
+
+        parent.addContent(elem);
+    }
+
     private static void addTaskElement(final Element mod) {
         if (!mod.getName().equals("modification")) {
             throw new IllegalArgumentException("mod parameter must be a modification element, was: " + mod.getContent());
@@ -19,37 +28,43 @@ public class CMSynergyTaskPublisherTest extends TestCase {
         // set type attrib on parent mod element
         mod.setAttribute("type", "ccmtask");
 
-        final Element task = new Element("task");
-        task.addContent(22491 + "");
-
-        mod.addContent(task);
+        addElementWithContent(mod, "task", 22491 + "");
     }
 
-    private static void replaceModsWithTaskMod(final Element log) {
-        final Element mods = TestUtil.createModsElement(1);
-        addTaskElement((Element) mods.getContent().get(0));
+    public static void replaceMods(final Element log, final Element mods) {
         log.removeChild("modifications");
         log.addContent(0, mods);
     }
 
 
+
     public void testShouldPublishSuccess() throws Exception {
         final CMSynergyTaskPublisher publisher = new CMSynergyTaskPublisher();
 
-        final Element log = TestUtil.createElement(true, true, "2 minutes 20 seconds", 0, null);
+        assertFalse("No mods, should not publish", publisher.shouldPublish(
+                TestUtil.createElement(true, true, "2 minutes 20 seconds", 0, null)));
+
+        final Element log = TestUtil.createElement(true, true, "2 minutes 20 seconds", 1, null);
         assertFalse("No task mods, should not publish", publisher.shouldPublish(log));
 
-        replaceModsWithTaskMod(log);
+        final Element mods = TestUtil.createModsElement(1);
+        addTaskElement((Element) mods.getContent().get(0));
+        replaceMods(log, mods);
         assertTrue("Build Success with task mod, should publish", publisher.shouldPublish(log));
     }
 
     public void testShouldNotPublishFail() throws Exception {
         final CMSynergyTaskPublisher publisher = new CMSynergyTaskPublisher();
 
-        final Element log = TestUtil.createElement(false, true, "2 minutes 20 seconds", 0, "it broke");
-        assertFalse("Build Fail, should not publish", publisher.shouldPublish(log));
+        assertFalse("Build Fail no mods, should not publish", publisher.shouldPublish(
+                TestUtil.createElement(false, true, "2 minutes 20 seconds", 0, "it broke")));
 
-        replaceModsWithTaskMod(log);
+        final Element log = TestUtil.createElement(false, true, "2 minutes 20 seconds", 1, "it broke");
+        assertFalse("Build Fail with no task mod, should not publish", publisher.shouldPublish(log));
+
+        final Element mods = TestUtil.createModsElement(1);
+        addTaskElement((Element) mods.getContent().get(0));
+        replaceMods(log, mods);
         assertFalse("Build Fail with task mod, should not publish", publisher.shouldPublish(log));
     }
 
