64f53092cba67f4b8a9d3a6e3040fc2fbb9798d5
Dan Rollo <bhamail@users.sourceforge.net>
2010-08-16 18:30:29 +0000

    increase test thread sleep time in attempt to fix intermittent test failure on Windows.

--- main/test/net/sourceforge/cruisecontrol/builders/CompositeBuilderTest.java
+++ main/test/net/sourceforge/cruisecontrol/builders/CompositeBuilderTest.java
@@ -323,7 +323,7 @@ public class CompositeBuilderTest extends TestCase {
 
                 // wait for longer than timeout
                 try {                               // + 1 works on linux, not on winz 
-                    Thread.sleep((timeoutSecs * 1000L) + 100);
+                    Thread.sleep((timeoutSecs * 1000L) + 500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
